fdc17e08a83326315f5f32bdd39de96dc2bd5980
Miguel París Díaz <mparisdiaz@gmail.com>
2013-02-27 16:01:00 +0100

    Do not use static methods in MediaFactory.

--- src/main/java/com/kurento/kms/media/MediaFactory.java
+++ src/main/java/com/kurento/kms/media/MediaFactory.java
@@ -2,28 +2,24 @@ package com.kurento.kms.media;
 
 import java.io.Serializable;
 
-public class MediaFactory  implements Serializable {
+public class MediaFactory implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-	public static MediaPlayer getMediaPlayer(String uri){
+	public MediaPlayer getMediaPlayer(String uri) {
 		return null;
 	}
-	
-	public static MediaRecorder getMediaRecorder (String uri){
+
+	public MediaRecorder getMediaRecorder(String uri) {
 		return null;
 	}
-	
-	public static Stream getStream() {
+
+	public Stream getStream() {
 		return null;
 	}
-	
-	public  static <T extends Mixer> T getMixer(Class<T> clazz){
+
+	public <T extends Mixer> T getMixer(Class<T> clazz) {
 		return null;
 	}
-	
 
 }
