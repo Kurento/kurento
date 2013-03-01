a2c6f3b1d4c01a310c64cc8e679285d44c34e3ad
Miguel París Díaz <mparisdiaz@gmail.com>
2013-03-01 13:27:18 +0100

    Update tests.

--- src/test/java/com/kurento/kms/media/MediaServerTest.java
+++ src/test/java/com/kurento/kms/media/MediaServerTest.java
@@ -10,6 +10,7 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.kurento.kms.api.MediaType;
 import com.kurento.kms.media.Stream.Continuation;
 import com.kurento.kms.media.internal.KmsConstants;
 
@@ -24,11 +25,13 @@ public class MediaServerTest {
 		properties.setProperty(KmsConstants.SERVER_PORT, ""
 				+ KmsConstants.DEFAULT_SERVER_PORT);
 
-		mediaFactory = new MediaFactory(properties);
+		MediaFactory.init(properties);
+		mediaFactory = MediaFactory.getMediaFactory();
 	}
 
 	@AfterClass
-	public static void afterClass() {
+	public static void afterClass() throws IOException {
+		mediaFactory.release();
 	}
 
 	@Test
@@ -50,11 +53,50 @@ public class MediaServerTest {
 		});
 
 		Assert.assertTrue(sem.tryAcquire(10, TimeUnit.SECONDS));
+		stream.release();
 	}
 
 	@Test
 	public void testPlayer() throws MediaException, IOException {
 		MediaPlayer player = mediaFactory.getMediaPlayer("");
 		player.play();
+		player.pause();
+		player.stop();
+		player.release();
 	}
+
+	@Test
+	public void testRecorder() throws MediaException, IOException {
+		MediaRecorder recorder = mediaFactory.getMediaRecorder("");
+		recorder.record();
+		recorder.pause();
+		recorder.stop();
+		recorder.release();
+	}
+
+	@Test
+	public void testJoinable() throws MediaException, IOException {
+		Stream streamA = mediaFactory.getStream();
+		Stream streamB = mediaFactory.getStream();
+
+		streamA.join(streamB);
+		streamA.unjoin(streamB);
+
+		System.out.println("MediaSrcs: " + streamA.getMediaSrcs());
+		System.out.println("MediaSinks: " + streamA.getMediaSinks());
+
+		System.out.println("MediaSrcs audio: "
+				+ streamA.getMediaSrcs(MediaType.AUDIO));
+		System.out.println("MediaSrcs video: "
+				+ streamA.getMediaSrcs(MediaType.VIDEO));
+
+		System.out.println("MediaSinks audio: "
+				+ streamA.getMediaSinks(MediaType.AUDIO));
+		System.out.println("MediaSinks video: "
+				+ streamA.getMediaSinks(MediaType.VIDEO));
+
+		streamA.release();
+		streamB.release();
+	}
+
 }
