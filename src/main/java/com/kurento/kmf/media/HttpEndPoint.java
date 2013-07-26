5998895ce8bc134b7c902616eb8986b68741e0c9
Miguel París Díaz <mparisdiaz@gmail.com>
2013-07-26 10:37:40 +0200

    Implement HttpEndPoint
    Change-Id: Ic33742d764d6e8b6370ce813748b6f081ec13037

--- src/main/java/com/kurento/kmf/media/HttpEndPoint.java
+++ src/main/java/com/kurento/kmf/media/HttpEndPoint.java
@@ -1,8 +1,16 @@
 package com.kurento.kmf.media;
 
-import org.apache.commons.lang.NotImplementedException;
+import java.io.IOException;
 
+import org.apache.thrift.TException;
+import org.apache.thrift.async.AsyncMethodCallback;
+
+import com.kurento.kmf.media.internal.MediaServerServiceManager;
 import com.kurento.kms.api.MediaObject;
+import com.kurento.kms.api.MediaObjectNotFoundException;
+import com.kurento.kms.api.MediaServerException;
+import com.kurento.kms.api.MediaServerService;
+import com.kurento.kms.api.MediaServerService.AsyncClient.getUrl_call;
 
 /**
  * Represents a http address where a single get or post can be done
@@ -20,15 +28,58 @@ public class HttpEndPoint extends EndPoint {
 
 	/* SYNC */
 
-	public String getUri() {
-		// TODO: Implement this method
-		throw new NotImplementedException();
+	public String getUrl() throws IOException {
+		MediaServerService.Client service = MediaServerServiceManager
+				.getMediaServerService();
+
+		try {
+			return service.getUrl(mediaObject);
+		} catch (MediaObjectNotFoundException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		} catch (MediaServerException e) {
+			throw new RuntimeException(e.getMessage(), e);
+		} catch (TException e) {
+			throw new IOException(e.getMessage(), e);
+		} finally {
+			MediaServerServiceManager.releaseMediaServerService(service);
+		}
 	}
 
 	/* ASYNC */
 
-	public void getUri(Continuation<String> cont) {
-		// TODO: Implement this method
-		cont.onError(new NotImplementedException());
+	public void getUrl(final Continuation<String> cont) throws IOException {
+		MediaServerService.AsyncClient service = MediaServerServiceManager
+				.getMediaServerServiceAsync();
+
+		try {
+			service.getUrl(
+					mediaObject,
+					new AsyncMethodCallback<MediaServerService.AsyncClient.getUrl_call>() {
+						@Override
+						public void onComplete(getUrl_call response) {
+							try {
+								cont.onSuccess(response.getResult());
+							} catch (MediaObjectNotFoundException e) {
+								cont.onError(new RuntimeException(e
+										.getMessage(), e));
+							} catch (MediaServerException e) {
+								cont.onError(new RuntimeException(e
+										.getMessage(), e));
+							} catch (TException e) {
+								cont.onError(new IOException(e.getMessage(), e));
+							}
+						}
+
+						@Override
+						public void onError(Exception exception) {
+							cont.onError(exception);
+						}
+					});
+		} catch (TException e) {
+			throw new IOException(e.getMessage(), e);
+		} finally {
+			MediaServerServiceManager.releaseMediaServerServiceAsync(service);
+		}
 	}
+
 }
