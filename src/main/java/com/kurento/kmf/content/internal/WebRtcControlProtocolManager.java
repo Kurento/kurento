e3b5bb268db85a9e9eb3e7133b3cae3c0932d986
Boni Garcia <bgarcia@gsyc.es>
2013-08-07 18:18:36 +0200

    Checked that received JSON are UTF-8
    Change-Id: Ic28483a3880360a56000f614621ed1f5987f5e9f

--- src/main/java/com/kurento/kmf/content/internal/WebRtcControlProtocolManager.java
+++ src/main/java/com/kurento/kmf/content/internal/WebRtcControlProtocolManager.java
@@ -1,6 +1,9 @@
 package com.kurento.kmf.content.internal;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
@@ -13,12 +16,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
 import com.google.gson.Gson;
-import com.kurento.kmf.content.internal.jsonrpc.WebRtcJsonResponse;
 import com.kurento.kmf.content.internal.jsonrpc.WebRtcJsonRequest;
+import com.kurento.kmf.content.internal.jsonrpc.WebRtcJsonResponse;
 
 public class WebRtcControlProtocolManager {
 	private static final Logger log = LoggerFactory
 			.getLogger(WebRtcControlProtocolManager.class);
+
+	private static final int BUFF = 4096;
+
+	private static final String UTF8 = "UTF-8";
 
 	private Gson gson;
 
@@ -29,15 +36,67 @@ public class WebRtcControlProtocolManager {
 	public WebRtcJsonRequest receiveJsonRequest(AsyncContext asyncCtx)
 			throws IOException {
 		HttpServletRequest request = (HttpServletRequest) asyncCtx.getRequest();
-		// TODO: we assume UTF-8 as character encoding, we should try to support
-		// others.
-		InputStreamReader isr = new InputStreamReader(request.getInputStream(),
-				"UTF-8");
+
+		// Received character encoding should be UTF-8. In order to check this,
+		// the method detectJsonEncoding will be used. Before that, the
+		// InputStream read from request.getInputStream() should be cloned
+		// (using a ByteArrayOutputStream) to be used on detectJsonEncoding and
+		// then for reading the JSON message
+		InputStream inputStream = request.getInputStream();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] buffer = new byte[BUFF];
+		int len;
+		while ((len = inputStream.read(buffer)) > -1) {
+			baos.write(buffer, 0, len);
+		}
+		baos.flush();
+
+		String encoding = detectJsonEncoding(new ByteArrayInputStream(
+				baos.toByteArray()));
+		log.debug("Detected JSON encoding: " + encoding);
+		if (encoding == null || !encoding.equalsIgnoreCase(UTF8)) {
+			throw new IOException(
+					"Invalid charset encondig in received JSON request");
+		}
+
+		InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(
+				baos.toByteArray()), UTF8);
 		WebRtcJsonRequest jsonRequest = gson.fromJson(isr,
 				WebRtcJsonRequest.class);
 		Assert.notNull(jsonRequest.getMethod());
 		return jsonRequest;
+	}
 
+	private String detectJsonEncoding(InputStream inputStream)
+			throws IOException {
+		inputStream.mark(4);
+		int mask = 0;
+		for (int count = 0; count < 4; count++) {
+			int r = inputStream.read();
+			if (r == -1) {
+				break;
+			}
+			mask = mask << 1;
+			mask |= (r == 0) ? 0 : 1;
+		}
+		inputStream.reset();
+		return match(mask);
+	}
+
+	private String match(int mask) {
+		switch (mask) {
+		case 1:
+			// UTF-32BE
+		case 5:
+			// UTF-16BE
+		case 8:
+			// UTF-32LE
+		case 10:
+			// UTF-16LE
+			return null;
+		default:
+			return UTF8;
+		}
 	}
 
 	public void sendJsonAnswer(AsyncContext asyncCtx, WebRtcJsonResponse message)
@@ -71,9 +130,9 @@ public class WebRtcControlProtocolManager {
 		synchronized (asyncCtx) {
 			HttpServletResponse response = (HttpServletResponse) asyncCtx
 					.getResponse();
-			response.setContentType("application/json; charset=UTF-8");
+			response.setContentType("application/json; charset=" + UTF8);
 			OutputStreamWriter osw = new OutputStreamWriter(
-					response.getOutputStream(), "UTF-8");
+					response.getOutputStream(), UTF8);
 			osw.write(gson.toJson(message));
 			osw.flush();
 			asyncCtx.complete();
