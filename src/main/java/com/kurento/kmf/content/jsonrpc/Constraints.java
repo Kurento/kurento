9ef1ed1d51c932a54c25a4ca60a1d2b939fd4603
Luis López <lulop@kurento.com>
2013-08-23 17:42:48 +0200

    Adapted contraints to http://tools.ietf.org/html/rfc4566#page-27
    Change-Id: If681ca1f781b7c4db222d5650aad66353262445b

--- src/main/java/com/kurento/kmf/content/jsonrpc/Constraints.java
+++ src/main/java/com/kurento/kmf/content/jsonrpc/Constraints.java
@@ -1,6 +1,5 @@
 package com.kurento.kmf.content.jsonrpc;
 
 public enum Constraints {
-	// TODO: change to SENDONLY, RECVONLY, SENDRECV and INACTIVE, respectly
-	SEND, RECV, SENDRECV, NONE
+	SENDONLY, RECVONLY, SENDRECV, INACTIVE
 }
