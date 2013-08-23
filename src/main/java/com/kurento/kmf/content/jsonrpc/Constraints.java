4fe2dfa886d4473017f0d97853d11f2a492a5693
Miguel París Díaz <mparisdiaz@gmail.com>
2013-08-23 16:20:39 +0200

    Add base implementation for JSON-RPC
    Change-Id: I87ddd5a9b48fbf601dee527e72e1c82149ad4016

---
+++ src/main/java/com/kurento/kmf/content/jsonrpc/Constraints.java
@@ -0,0 +1,6 @@
+package com.kurento.kmf.content.jsonrpc;
+
+public enum Constraints {
+	// TODO: change to SENDONLY, RECVONLY, SENDRECV and INACTIVE, respectly
+	SEND, RECV, SENDRECV, NONE
+}
---
+++ src/main/java/com/kurento/kmf/content/jsonrpc/JsonRpcEvent.java
@@ -0,0 +1,26 @@
+package com.kurento.kmf.content.jsonrpc;
+
+public class JsonRpcEvent {
+	private String type;
+	private String data;
+
+	public static JsonRpcEvent newEvent(String type, String data) {
+		return new JsonRpcEvent(type, data);
+	}
+
+	JsonRpcEvent() {
+	}
+
+	JsonRpcEvent(String type, String data) {
+		this.type = type;
+		this.data = data;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getData() {
+		return data;
+	}
+}
