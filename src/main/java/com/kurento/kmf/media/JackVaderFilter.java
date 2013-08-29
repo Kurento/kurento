b9d063b1ef569527763606958a63b79651fa6600
Luis López <lulop@kurento.com>
2013-08-30 00:25:21 +0200

    Add filterType on JackVaderFilter
    Change-Id: I59b74d920ed5f1f638b60c124a58c1a0f0225903

--- src/main/java/com/kurento/kmf/media/JackVaderFilter.java
+++ src/main/java/com/kurento/kmf/media/JackVaderFilter.java
@@ -3,10 +3,13 @@ package com.kurento.kmf.media;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.kurento.kms.api.FilterType;
 import com.kurento.kms.api.MediaObjectId;
 
 public class JackVaderFilter extends Filter {
 	private static final long serialVersionUID = 1L;
+
+	static final FilterType filterType = FilterType.JACK_VADER_FILTER;
 
 	public static final Logger log = LoggerFactory
 			.getLogger(JackVaderFilter.class);
