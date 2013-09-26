b3d9ab2e1a947c45843da0c8efa66d8f04a1820a
Santiago Gala <sgala@apache.org>
2013-09-26 18:04:09 +0200

    Add Source headers to files
    Change-Id: I2747f47f65889d6d77951210cb378aee204de04d

--- src/main/java/com/kurento/kmf/content/package-info.java
+++ src/main/java/com/kurento/kmf/content/package-info.java
@@ -1,14 +1,16 @@
-/**
- * 
- * Provides the Handlers (entities implementing services, such as
- * play, record, and so on), Requests interfaces (Entities declaring services 
- * interfaces), Annotations (Java annotations to declare services), 
- * Configuration, and a custom Exception within Content Management API. 
- * 
- * @author Luis López (llopez@gsyc.es)
- * @author Miguel París (mparisdiaz@gsyc.es)
- * @author Boni García (bgarcia@gsyc.es)
- * @version 1.0.0
+/*
+ * (C) Copyright 2013 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
  */
 package com.kurento.kmf.content;
 
