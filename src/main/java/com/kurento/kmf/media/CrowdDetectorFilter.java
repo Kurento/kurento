02da3915d5cc62f577d2d4c8c667ca2cedeac83d
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-03-25 15:56:54 +0100

    CrowDetectorFilter: Add event listeners
    Change-Id: I4db0bc9bb3f03eb0648000844d4b2bcd68934cff

--- src/main/java/com/kurento/kmf/media/CrowdDetectorFilter.java
+++ src/main/java/com/kurento/kmf/media/CrowdDetectorFilter.java
@@ -2,11 +2,36 @@ package com.kurento.kmf.media;
 
 import java.util.List;
 
+import com.kurento.kmf.media.events.CrowdDetectorDirectionEvent;
+import com.kurento.kmf.media.events.CrowdDetectorFluidityEvent;
+import com.kurento.kmf.media.events.CrowdDetectorOccupancyEvent;
+import com.kurento.kmf.media.events.MediaEventListener;
 import com.kurento.tool.rom.RemoteClass;
 import com.kurento.tool.rom.server.Param;
 
 @RemoteClass
 public interface CrowdDetectorFilter extends Filter {
+
+	ListenerRegistration addCrowdDetectorFluidityListener(
+			MediaEventListener<CrowdDetectorFluidityEvent> listener);
+
+	void addCrowdDetectorFluidityListener(
+			MediaEventListener<CrowdDetectorFluidityEvent> listener,
+			Continuation<ListenerRegistration> cont);
+
+	ListenerRegistration addCrowdDetectorOccupancyListener(
+			MediaEventListener<CrowdDetectorOccupancyEvent> listener);
+
+	void addCrowdDetectorOccupancyListener(
+			MediaEventListener<CrowdDetectorOccupancyEvent> listener,
+			Continuation<ListenerRegistration> cont);
+
+	ListenerRegistration addCrowdDetectorDirectionListener(
+			MediaEventListener<CrowdDetectorDirectionEvent> listener);
+
+	void addCrowdDetectorDirectionListener(
+			MediaEventListener<CrowdDetectorDirectionEvent> listener,
+			Continuation<ListenerRegistration> cont);
 
 	public interface Factory {
 
