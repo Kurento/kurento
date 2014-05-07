ae8cd1128ce9d3237cea669c7b09fff91dde8133
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-05-07 21:56:47 +0200

    cmake: Enable valgrind tests
    Change-Id: I69cbca70600e9be47961cf139735edc155b64b3c

--- TestHelper.cmake
+++ TestHelper.cmake
@@ -6,6 +6,13 @@ function(create_check_target)
   endif()
 endfunction()
 
+function(create_valgrind_target)
+  if (NOT TARGET valgrind)
+    MESSAGE (STATUS "Enabling valgrind target")
+    add_custom_target(valgrind)
+  endif()
+endfunction()
+
 ## This function simplifies tests creation
 ## Name : add_test_program
 ## Params: test_name, extra_source (optional)
@@ -23,4 +30,32 @@ function(add_test_program test_name)
   foreach (p ${TEST_PROPERTIES})
     set_property(TEST ${test_name} APPEND PROPERTY ENVIRONMENT ${p})
   endforeach()
+
+  add_custom_target (${test_name}.check
+    COMMAND ${TEST_PROPERTIES} ${CMAKE_CURRENT_BINARY_DIR}/${test_name}
+    DEPENDS ${test_name})
+
+  if (EXISTS ${VALGRIND})
+    foreach(SUP ${SUPPRESSIONS})
+      set (SUPPS "${SUPPS} --suppressions=${SUP}")
+    endforeach()
+
+    add_custom_target (${test_name}.valgrind
+      DEPENDS ${test_name})
+
+    add_custom_command (TARGET ${test_name}.valgrind
+      COMMENT "Running valgrind for ${test_name}"
+      COMMAND ${TEST_PROPERTIES} CK_DEFAULT_TIMEOUT=360
+        G_SLICE=always-malloc ${VALGRIND} -q
+        $(SUPPS)
+        --tool=memcheck --leak-check=yes --trace-children=yes
+        --leak-resolution=high --show-possibly-lost=no
+        --num-callers=20
+        ${CMAKE_CURRENT_BINARY_DIR}/${test_name} 2>&1 |
+          tee ${test_name}.valgrind.log
+        )
+
+     create_valgrind_target()
+     add_dependencies(valgrind ${test_name}.valgrind)
+  endif ()
 endfunction()
