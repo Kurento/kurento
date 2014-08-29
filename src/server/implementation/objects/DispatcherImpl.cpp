0aa821a8b028e29a011d590a7275fe887a01d52f
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2014-08-29 14:03:08 +0200

    DispatcherImpl: Add implementation
    Change-Id: I3987471cc5b1e5955d13cf63d34ed1f062c1513c

--- src/server/implementation/objects/DispatcherImpl.cpp
+++ src/server/implementation/objects/DispatcherImpl.cpp
@@ -1,6 +1,6 @@
 #include <gst/gst.h>
 #include "MediaPipeline.hpp"
-#include "HubPort.hpp"
+#include "HubPortImpl.hpp"
 #include <DispatcherImplFactory.hpp>
 #include "DispatcherImpl.hpp"
 #include <jsonrpc/JsonSerializer.hpp>
@@ -20,15 +20,25 @@ DispatcherImpl::DispatcherImpl (const boost::property_tree::ptree &conf,
                                 std::shared_ptr<MediaPipeline> mediaPipeline) : HubImpl (conf,
                                       std::dynamic_pointer_cast<MediaObjectImpl> (mediaPipeline), FACTORY_NAME)
 {
-  // FIXME: Implement this
 }
 
 void DispatcherImpl::connect (std::shared_ptr<HubPort> source,
                               std::shared_ptr<HubPort> sink)
 {
-  // FIXME: Implement this
-  throw KurentoException (NOT_IMPLEMENTED,
-                          "DispatcherImpl::connect: Not implemented");
+  std::shared_ptr<HubPortImpl> sourcePort =
+    std::dynamic_pointer_cast<HubPortImpl> (source);
+  std::shared_ptr<HubPortImpl> sinkPort = std::dynamic_pointer_cast<HubPortImpl>
+                                          (sink);
+  bool connected;
+
+  g_signal_emit_by_name (G_OBJECT (element), "connect",
+                         sourcePort->getHandlerId(),
+                         sinkPort->getHandlerId(),
+                         &connected);
+
+  if (!connected) {
+    throw KurentoException (CONNECT_ERROR, "Can not connect ports");
+  }
 }
 
 MediaObjectImpl *
