6fac88dfa74070aca459eb18bf8b31bbc6513bfa
mgallego <micael.gallego@gmail.com>
2014-08-30 13:13:57 +0200

    Generate JS projects in KurentoCodeGeneration pseudo-test
    Change-Id: Ibc0a42907c2e2601c7193afa862c2e2c784e8ae7

--- src/test/java/org/kurento/modulecreator/test/KurentoCodeGeneration.java
+++ src/test/java/org/kurento/modulecreator/test/KurentoCodeGeneration.java
@@ -29,21 +29,40 @@ public class KurentoCodeGeneration {
 
 		// updateGitProjects();
 
-		generateCodeFor("kms-core");
-		generateCodeFor("kms-elements");
-		generateCodeFor("kms-filters");
+		// generateJavaCode();
+
+		generateJavaScriptCode();
+
+	}
+
+	private static void generateJavaScriptCode() throws JsonIOException,
+			IOException {
+
+		generateJavaScriptCodeFor("kms-core");
+		generateJavaScriptCodeFor("kms-elements");
+		generateJavaScriptCodeFor("kms-filters");
+		generateJavaScriptCodeFor("kms-platedetector");
+		generateJavaScriptCodeFor("kms-pointerdetector");
+		generateJavaScriptCodeFor("kms-crowddetector");
+		generateJavaScriptCodeFor("kms-chroma");
+		generateJavaScriptCodeFor("kms-example");
+	}
+
+	private static void generateJavaCode() throws IOException {
+		generateJavaCodeFor("kms-core");
+		generateJavaCodeFor("kms-elements");
+		generateJavaCodeFor("kms-filters");
 
 		mvnInstall("/home/mica/Data/Kurento/git/kurento-module-creator");
 		mvnInstall("/home/mica/Data/Kurento/git/kurento-maven-plugin");
 
 		mvnInstall("/home/mica/Data/Kurento/git/kurento-java/kurento-client");
 
-		generateCodeFor("kms-platedetector");
-		generateCodeFor("kms-pointerdetector");
-		generateCodeFor("kms-crowddetector");
-		generateCodeFor("kms-chroma");
-		generateCodeFor("kms-example");
-
+		generateJavaCodeFor("kms-platedetector");
+		generateJavaCodeFor("kms-pointerdetector");
+		generateJavaCodeFor("kms-crowddetector");
+		generateJavaCodeFor("kms-chroma");
+		generateJavaCodeFor("kms-example");
 	}
 
 	private static void updateGitProjects() throws IOException {
@@ -69,16 +88,79 @@ public class KurentoCodeGeneration {
 		return KMS_PROJECTS_PATH + "/" + project;
 	}
 
-	public static void generateCodeFor(String project) throws JsonIOException,
-			IOException {
+	private static void generateJavaScriptCodeFor(String project)
+			throws IOException {
+		log.info("----------------------------------------------------");
+		log.info("  Start Generating JavaScript code for " + project);
+		log.info("----------------------------------------------------");
+
+		generateNpmProject(project);
+	}
+
+	private static void generateNpmProject(String project) throws IOException {
+
+		String npmProjectFolder = getProjectPath(project) + "/build/js";
+
+		execAndGetResult("rm -R " + npmProjectFolder + "/package.json");
+		execAndGetResult("rm -R " + npmProjectFolder + "/src");
+
+		KurentoModuleCreator modCreator = new KurentoModuleCreator();
+
+		modCreator.setKmdFilesToGen(PathUtils.getPaths(
+				new String[] { getProjectPath(project)
+						+ "/src/server/interface" }, "*.kmd.json"));
+
+		modCreator.setDependencyKmdFiles(PathUtils.searchFiles(
+				Paths.get(GLOBAL_FUSIONED_KMDS_PATH), "*.kmd.json"));
+
+		modCreator.setInternalTemplates("npm");
+		modCreator.setCodeGenDir(Paths.get(npmProjectFolder));
+		modCreator.generateCode();
+
+		modCreator.setInternalTemplates("js");
+		modCreator.setCodeGenDir(Paths.get(npmProjectFolder + "/lib/"));
+		modCreator.generateCode();
+
+		// Copy fusioned kmd file to /lib/
+		Path fusionedKmdFile = Paths.get(npmProjectFolder + "/lib/");
+		modCreator.setOutputFile(fusionedKmdFile);
+		modCreator.generateCode();
+
+		// Copy fusioned kmd file to /tmp/kurento-module-creator/kmds
+		copyFusionedKmdToKmdsFolder(fusionedKmdFile);
+	}
+
+	private static void copyFusionedKmdToKmdsFolder(Path fusionedKmdFile)
+			throws IOException {
+
+		Path kmdsPath = Paths.get(GLOBAL_FUSIONED_KMDS_PATH);
+
+		if (!Files.exists(kmdsPath)) {
+			Files.createDirectories(kmdsPath);
+		}
+
+		try (DirectoryStream<Path> stream = Files
+				.newDirectoryStream(fusionedKmdFile)) {
+
+			for (Path kmdFile : stream) {
+
+				Files.copy(kmdFile, kmdsPath.resolve(kmdFile.getFileName()),
+						java.nio.file.StandardCopyOption.REPLACE_EXISTING,
+						java.nio.file.StandardCopyOption.COPY_ATTRIBUTES,
+						java.nio.file.LinkOption.NOFOLLOW_LINKS);
+			}
+		}
+	}
+
+	public static void generateJavaCodeFor(String project)
+			throws JsonIOException, IOException {
 
 		log.info("----------------------------------------------------");
-		log.info("  Generate code for " + project);
+		log.info("  Start Generating Java code for " + project);
 		log.info("----------------------------------------------------");
 
 		generateMavenProject(project);
 		mvnInstallProject(project);
-
 	}
 
 	private static void mvnInstallProject(String project) throws IOException {
@@ -111,18 +193,14 @@ public class KurentoCodeGeneration {
 				Paths.get(GLOBAL_FUSIONED_KMDS_PATH), "*.kmd.json"));
 
 		modCreator.setInternalTemplates("maven");
-
 		modCreator.setCodeGenDir(Paths.get(mavenProjectFolder));
-
 		modCreator.generateCode();
 
 		if (modCreator.hasToGenerateCode()) {
 
 			modCreator.setInternalTemplates("java");
-
 			modCreator.setCodeGenDir(Paths.get(mavenProjectFolder
 					+ "/src/main/java"));
-
 			modCreator.generateCode();
 
 		} else {
@@ -130,30 +208,10 @@ public class KurentoCodeGeneration {
 			// Copy fusioned kmd file to /META-INF/kurento/
 			Path fusionedKmdFile = Paths.get(mavenProjectFolder
 					+ "/src/main/resources/META-INF/kurento/");
-
 			modCreator.setOutputFile(fusionedKmdFile);
-
 			modCreator.generateCode();
 
-			// Copy fusioned kmd file to /tmp/kurento-module-creator/kmds
-			Path kmdsPath = Paths.get(GLOBAL_FUSIONED_KMDS_PATH);
-
-			if (!Files.exists(kmdsPath)) {
-				Files.createDirectories(kmdsPath);
-			}
-
-			try (DirectoryStream<Path> stream = Files
-					.newDirectoryStream(fusionedKmdFile)) {
-
-				for (Path kmdFile : stream) {
-
-					Files.copy(kmdFile,
-							kmdsPath.resolve(kmdFile.getFileName()),
-							java.nio.file.StandardCopyOption.REPLACE_EXISTING,
-							java.nio.file.StandardCopyOption.COPY_ATTRIBUTES,
-							java.nio.file.LinkOption.NOFOLLOW_LINKS);
-				}
-			}
+			copyFusionedKmdToKmdsFolder(fusionedKmdFile);
 		}
 	}
 
