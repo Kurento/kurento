8fd32b081e0e5b9e42bf366bf39308297a3823ef
Santiago Carot-Nemesio <sancane@gmail.com>
2014-09-04 15:06:02 +0200

    Add multichannelcontroller skelleton class
    Change-Id: I3059571810c722c67c12c48ece1bc135c736e93d

---
+++ src/gst-plugins/kmsmultichannelcontroller.c
@@ -0,0 +1,71 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "kmsmultichannelcontroller.h"
+
+GST_DEBUG_CATEGORY_STATIC (kms_multi_channel_controller_debug);
+#define GST_CAT_DEFAULT kms_multi_channel_controller_debug
+
+GType _kms_multi_channel_controller_type = 0;
+
+struct _KmsMultiChannelController
+{
+  GstMiniObject obj;
+
+  gchar *host;
+  guint16 port;
+};
+
+GST_DEFINE_MINI_OBJECT_TYPE (KmsMultiChannelController,
+    kms_multi_channel_controller);
+
+static void
+_priv_kms_multi_channel_controller_initialize (void)
+{
+  _kms_multi_channel_controller_type = kms_multi_channel_controller_get_type ();
+
+  GST_DEBUG_CATEGORY_INIT (kms_multi_channel_controller_debug,
+      "multichannelcontroller", 0, "multi-channel controller protocol");
+}
+
+static void
+_kms_multi_channel_controller_free (KmsMultiChannelController * mcc)
+{
+  g_return_if_fail (mcc != NULL);
+
+  GST_DEBUG ("free");
+
+  if (mcc->host != NULL)
+    g_free (mcc->host);
+
+  g_slice_free1 (sizeof (KmsMultiChannelController), mcc);
+}
+
+KmsMultiChannelController *
+kms_multi_channel_controller_new ()
+{
+  KmsMultiChannelController *mcc;
+
+  mcc = g_slice_new0 (KmsMultiChannelController);
+
+  gst_mini_object_init (GST_MINI_OBJECT_CAST (mcc), 0,
+      _kms_multi_channel_controller_type, NULL, NULL,
+      (GstMiniObjectFreeFunction) _kms_multi_channel_controller_free);
+
+  return KMS_MULTI_CHANNEL_CONTROLLER (mcc);
+}
+
+static void _priv_kms_multi_channel_controller_initialize (void)
+    __attribute__ ((constructor));
---
+++ src/gst-plugins/kmsmultichannelcontroller.h
@@ -0,0 +1,95 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef __KMS_MULTI_CHANNEL_CONTROLLER_H__
+#define __KMS_MULTI_CHANNEL_CONTROLLER_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+typedef struct _KmsMultiChannelController KmsMultiChannelController;
+
+#define KMS_TYPE_MULTI_CHANNEL_CONTROLLER	(kms_multi_channel_controller_get_type())
+#define KMS_IS_MULTI_CHANNEL_CONTROLLER(obj)	(GST_IS_MINI_OBJECT_TYPE (obj, KMS_TYPE_MULTI_CHANNEL_CONTROLLER))
+#define KMS_MULTI_CHANNEL_CONTROLLER_CAST(obj)	((KmsMultiChannelController*)(obj))
+#define KMS_MULTI_CHANNEL_CONTROLLER(obj)	(KMS_MULTI_CHANNEL_CONTROLLER_CAST(obj))
+
+GType kms_multi_channel_controller_get_type (void);
+
+#ifdef _FOOL_GTK_DOC_
+G_INLINE_FUNC KmsMultiChannelController * kms_multi_channel_controller_ref (KmsMultiChannelController * m);
+#endif
+
+static inline KmsMultiChannelController *
+kms_multi_channel_controller_ref (KmsMultiChannelController * m)
+{
+  return KMS_MULTI_CHANNEL_CONTROLLER_CAST (gst_mini_object_ref (GST_MINI_OBJECT_CAST (m)));
+}
+
+#ifdef _FOOL_GTK_DOC_
+G_INLINE_FUNC void kms_multi_channel_controller_unref (KmsMultiChannelController * m);
+#endif
+
+static inline void
+kms_multi_channel_controller_unref (KmsMultiChannelController * m)
+{
+  gst_mini_object_unref (GST_MINI_OBJECT_CAST (m));
+}
+
+#ifdef _FOOL_GTK_DOC_
+G_INLINE_FUNC KmsMultiChannelController * kms_multi_channel_controller_copy (const KmsMultiChannelController * m);
+#endif
+
+static inline KmsMultiChannelController *
+kms_multi_channel_controller_copy (const KmsMultiChannelController * m)
+{
+  return KMS_MULTI_CHANNEL_CONTROLLER_CAST (gst_mini_object_copy (GST_MINI_OBJECT_CONST_CAST (m)));
+}
+
+#define kms_multi_channel_controller_is_writable(m) \
+  gst_mini_object_is_writable (GST_MINI_OBJECT_CAST (m))
+
+#define kms_multi_channel_controller_make_writable(m) \
+  KMS_MULTI_CHANNEL_CONTROLLER_CAST (gst_mini_object_make_writable (GST_MINI_OBJECT_CAST (m)))
+
+#ifdef _FOOL_GTK_DOC_
+G_INLINE_FUNC gboolean kms_multi_channel_controller_replace (KmsMultiChannelController **old_multi_channel_controller, KmsMultiChannelController *new_multi_channel_controller);
+#endif
+
+static inline gboolean
+kms_multi_channel_controller_replace (KmsMultiChannelController **old_multi_channel_controller,
+  KmsMultiChannelController *new_multi_channel_controller)
+{
+  return gst_mini_object_replace ((GstMiniObject **) old_multi_channel_controller,
+    (GstMiniObject *) new_multi_channel_controller);
+}
+
+/* application specific */
+
+/**
+ * kms_multi_channel_controller_new:
+ *
+ * Creates a newly allocated multi_channel_controller without any data.
+ *
+ * MT safe.
+ *
+ * Returns: (transfer full): the new #KmsMultiChannelController.
+ */
+KmsMultiChannelController * kms_multi_channel_controller_new ();
+
+G_END_DECLS
+
+#endif /* __KMS_MULTI_CHANNEL_CONTROLLER_H__ */
