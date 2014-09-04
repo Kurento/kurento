11961060d50288a384eb42cabdb6c8210914102e
Luis López <lulop@kurento.com>
2014-09-04 07:10:45 +0200

    First version of kurento-tutorial-node for node
    It contains the final version of kurento-hello-world and preliminary versions of kurento-magic-mirror and kurento-one2many
    
    Change-Id: I429b473d4314ba91dc5ef78ce7d6507c0a370603
    
    Improved kurento-magic-mirror tutorial
    
    Change-Id: I6557476657ad1b515190efb005d4102e7fa3c0e1

--- kurento-magic-mirror/app.js
+++ kurento-magic-mirror/app.js
@@ -1,122 +1,254 @@
-var KwsMedia = require('kws-media-api'); //TODO: use kurento-client-js
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+var kurento = require('kurento-client');
 var express = require('express');
 var app = express();
 var path = require('path');
 var wsm = require('ws');
+var session = require('express-session')
+
+/*
+ * Management of sessions
+ */
+app.use(express.cookieParser());
+
+var sessionHandler = session({
+	secret : 'none',
+	rolling : true,
+	resave : true,
+	saveUninitialized : true
+});
+
+app.use(sessionHandler);
 
 app.set('port', process.env.PORT || 3000);
 
 /*
-* Defintion of constants
-**/
+ * Defintion of constants
+ */
 
-const ws_uri = "ws://192.168.56.101:8888/kurento"; //TODO: set to localhost.
+const
+ws_uri = "ws://192.168.56.101:8888/kurento"; // TODO: set to localhost.
 
 /*
  * Definition of global variables.
- * */
+ */
 
-var pipeline = null;
-
+var pipelines = {};
+var kurentoClient = null;
 
 /*
-* Server startup
-**/
+ * Server startup
+ */
 
 var port = app.get('port');
-var server = app.listen(port, function(){
-    console.log('Express server started ');
-    console.log('Connect to http://localhost:' + port + '/');
+var server = app.listen(port, function() {
+	console.log('Express server started ');
+	console.log('Connect to http://<host_name>:' + port + '/');
 });
 
-var WebSocketServer = wsm.Server
-, wss = new WebSocketServer({server: server, path: '/magicmirror'});
+var WebSocketServer = wsm.Server, wss = new WebSocketServer({
+	server : server,
+	path : '/magicmirror'
+});
 
+/*
+ * Management of WebSocket messages
+ */
 wss.on('connection', function(ws) {
-    
-    ws.on('message', function(_message) {
-	var message = JSON.parse(_message);
-	console.log('received: ', message);
-	
-	switch(message.id){
-	    case 'start':
-		start(message.sdpOffer, function(error, sdpAnswer){
-		    if(error){
-			ws.send(JSON.stringify({id:'error', message : error}));
-		    }
-		    ws.send(JSON.stringify({id:'startResponse', sdpAnswer:sdpAnswer}));
-		});
-		break;
-	    
-	    case 'stop':
-		stop();
-		break;
-	    
-	    default:
-		ws.send(JSON.stringify({id:'error', message : 'Invalid message ' + message}));
-		break;
-	}
-	
-    });
+	var sessionId = null;
+	var request = ws.upgradeReq;
+	var response = {
+		writeHead : {}
+	}; // black magic here
+
+	sessionHandler(request, response, function(err) {
+		sessionId = request.session.id;
+		console.log("Connection received with sessionId " + sessionId);
+	});
+
+	ws.on('error', function(error) {
+		console.log('Connection ' + sessionId + ' error');
+		stop(sessionId);
+	});
+
+	ws.on('close', function() {
+		console.log('Connection ' + sessionId + ' closed');
+		stop(sessionId);
+	});
+
+	ws.on('message', function(_message) {
+		var message = JSON.parse(_message);
+		console.log('Connection ' + sessionId + ' received message ', message);
+
+		switch (message.id) {
+		case 'start':
+			start(sessionId, message.sdpOffer, function(error, sdpAnswer) {
+				if (error) {
+					return ws.send(JSON.stringify({
+						id : 'error',
+						message : error
+					}));
+				}
+				ws.send(JSON.stringify({
+					id : 'startResponse',
+					sdpAnswer : sdpAnswer
+				}));
+			});
+			break;
+
+		case 'stop':
+			stop(sessionId);
+			break;
+
+		default:
+			ws.send(JSON.stringify({
+				id : 'error',
+				message : 'Invalid message ' + message
+			}));
+			break;
+		}
+
+	});
 });
 
 /*
  * Definition of functions
- * */
+ */
 
-function getPipeline(callback){
-	if(pipeline !== null){
-		return callback(null, pipeline);
+// Recover kurentoClient for the first time.
+function getKurentoClient(callback) {
+	if (kurentoClient !== null) {
+		return callback(null, kurentoClient);
+	}
+
+	kurento(ws_uri, function(error, _kurentoClient) {
+		if (error) {
+			console.log("Coult not find media server at address " + ws_uri);
+			return callback("Could not find media server at address" + ws_uri
+					+ ". Exiting with error " + error);
+		}
+
+		kurentoClient = _kurentoClient;
+		callback(null, kurentoClient);
+	});
+}
+
+function start(sessionId, sdpOffer, callback) {
+
+	if (!sessionId) {
+		return callback("Cannot use undefined sessionId");
 	}
 	
-	KwsMedia(ws_uri, function(error, kurentoClient){
-		if(error){
+	//Check if session is already transmitting
+	if(pipelines[sessionId]){
+		pipelines[sessionId].release();
+		delete pipelines[sessionId];
+	}
+
+	getKurentoClient(function(error, kurentoClient) {
+		if (error) {
 			return callback(error);
 		}
-		
-		kurentoClient.create('MediaPipeline', function(error, _pipeline){
-			if(error){
+
+		kurentoClient.create('MediaPipeline', function(error, pipeline) {
+			if (error) {
 				return callback(error);
 			}
-			
-			pipeline = _pipeline;
-			return callback(null, pipeline);
+
+			createMediaElements(pipeline, function(error, webRtcEndpoint,
+					faceOverlayFilter) {
+				if (error) {
+					pipeline.release();
+					return callback(error);
+				}
+
+				connectMediaElements(webRtcEndpoint, faceOverlayFilter,
+						function(error) {
+							if (error) {
+								pipeline.release();
+								return callback(error);
+							}
+
+							webRtcEndpoint.processOffer(sdpOffer, function(
+									error, sdpAnswer) {
+								if (error) {
+									pipeline.release();
+									return callback(error);
+								}
+
+								pipelines[sessionId] = pipeline;
+								return callback(null, sdpAnswer);
+							});
+						});
+			});
 		});
 	});
 }
 
+function createMediaElements(pipeline, callback) {
 
-function start(sdpOffer, callback){
-    
-    getPipeline(function(error, pipeline){
-        if(error){
-            return callback(error);
-        }
-        
-        pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint){
-            if(error){
-                return callback(error);
-            }
-            
-            webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer){
-                if(error){
-                    webRtcEndpoint.release();
-                    return callback(error);
-                }
-                
-                webRtcEndpoint.connect(webRtcEndpoint, function(error){
-		    callback(null, sdpAnswer);
-                });
-            });            
-	});        
-    });
+	pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint) {
+		if (error) {
+			return callback(error);
+		}
+
+		pipeline.create('FaceOverlayFilter',
+				function(error, faceOverlayFilter) {
+					if (error) {
+						return callback(error);
+					}
+
+					faceOverlayFilter.setOverlayedImage(
+							"http://files.kurento.org/imgs/mario-wings.png",
+							-0.35, -1.2, 1.6, 1.6, function(error) {
+								if (error) {
+									return callback(error);
+								}
+
+								return callback(null, webRtcEndpoint,
+										faceOverlayFilter);
+
+							});
+				});
+	});
 }
 
-function stop() {
-    if(pipeline !== null){
-	pipeline.release();
-    }
+function connectMediaElements(webRtcEndpoint, faceOverlayFilter, callback) {
+	webRtcEndpoint.connect(faceOverlayFilter, function(error) {
+		if (error) {
+			return callback(error);
+		}
+
+		faceOverlayFilter.connect(webRtcEndpoint, function(error) {
+			if (error) {
+				return callback(error);
+			}
+
+			return callback(null);
+		});
+	});
 }
 
+function stop(sessionId) {
+	if (pipelines[sessionId]) {
+		var pipeline = pipelines[sessionId];
+		pipeline.release();
+		delete pipelines[sessionId];
+	}
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
