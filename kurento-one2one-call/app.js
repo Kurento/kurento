6a84da2345a9ab59b40c901161e31dc1a06f9d66
Luis López <lulop@kurento.com>
2014-09-06 00:30:08 +0200

    Frist fraft version of kurento-one2one-call
    Change-Id: I6c5f0dff068933d79f045f051e46bb5591acbd3b

---
+++ kurento-one2one-call/app.js
@@ -0,0 +1,396 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+var kurento = require('kurento-client');
+var express = require('express');
+var app = express();
+var path = require('path');
+var wsm = require('ws');
+
+app.set('port', process.env.PORT || 3000);
+
+/*
+ * Definition of constants
+ */
+
+const
+//ws_uri = "ws://localhost:8888/kurento";
+ws_uri = "ws://192.168.56.101:8888/kurento";
+/*
+ * Definition of global variables.
+ */
+
+var kurentoClient = null;
+var userRegistry = new UserRegistry();
+var pipelines = {};
+var idCounter = 0;
+
+
+function nextUniqueId() {
+	idCounter++;
+	return idCounter.toString();
+}
+
+
+/*
+ * Definition of helper classes
+ * */
+
+//Represents caller and callee sessions
+function UserSession(id, name, ws){
+	this.id = id;
+	this.name = name;
+	this.ws = ws;
+	this.peer = null;
+	this.sdpOffer = null;
+}
+
+UserSession.prototype.sendMessage = function(message){
+	this.ws.send(JSON.stringify(message));
+}
+
+//Represents registrar of users
+function UserRegistry(){
+	this.usersById = {};
+	this.usersByName = {};
+}
+
+UserRegistry.prototype.register = function(user){
+	this.usersById[user.id] = user;
+	this.usersByName[user.name] = user;
+}
+
+UserRegistry.prototype.unregister = function(id){
+	var user = this.getById(id);
+	if(user) delete this.usersById[id]
+	if(this.getByName(user.name)) delete this.usersByName[user.name];
+}
+
+UserRegistry.prototype.getById = function(id){
+	return this.usersById[id];
+}
+
+UserRegistry.prototype.getByName = function(name){
+	return this.usersByName[name];
+}
+
+UserRegistry.prototype.removeById = function(id){
+	var userSession = this.usersById[id];
+	if(!userSession) return;
+	delete this.usersById[id];
+	delete this.usersByName[userSession.name];
+}
+
+//Represents a B2B active call
+function CallMediaPipeline(){
+	this._pipeline = null;
+	this._callerWebRtcEndpoint = null;
+	this._calleeWebRtcEndpoint = null;
+}
+
+CallMediaPipeline.prototype.createPipeline = function(callback){
+	var self = this;
+	getKurentoClient(function(error, kurentoClient){
+		if(error){
+			return callback(error);
+		}
+		
+		kurentoClient.create('MediaPipeline', function(error, pipeline){
+			if(error){
+				return callback(error);
+			}
+			
+			pipeline.create('WebRtcEndpoint', function(error, callerWebRtcEndpoint){
+				if(error){
+					pipeline.release();
+					return callback(error);
+				}
+				
+				pipeline.create('WebRtcEndpoint', function(error, calleeWebRtcEndpoint){
+					if(error){
+						pipeline.release();
+						return callback(error);
+					}
+					
+					callerWebRtcEndpoint.connect(calleeWebRtcEndpoint, function(error){
+						if(error){
+							pipeline.release();
+							return callback(error);
+						}
+						
+						calleeWebRtcEndpoint.connect(callerWebRtcEndpoint, function(error){
+							if(error){
+								pipeline.release();
+								return callback(error);
+							}
+						});
+						
+						self._pipeline = pipeline;
+						self._callerWebRtcEndpoint = callerWebRtcEndpoint;
+						self._calleeWebRtcEndpoint = calleeWebRtcEndpoint;
+						
+						callback(null);
+					});					
+				});
+			});			
+		});		
+	})
+}
+
+CallMediaPipeline.prototype.generateSdpAnswerForCaller = function(sdpOffer, callback){
+	this._callerWebRtcEndpoint.processOffer(sdpOffer, callback);
+}
+
+CallMediaPipeline.prototype.generateSdpAnswerForCallee = function(sdpOffer, callback){
+	this._calleeWebRtcEndpoint.processOffer(sdpOffer, callback);
+}
+
+CallMediaPipeline.prototype.release = function(){
+	if(this._pipeline) this._pipeline.release();
+	this._pipeline = null;
+}
+
+
+/*
+ * Server startup
+ */
+
+var port = app.get('port');
+var server = app.listen(port, function() {
+	console.log('Express server started ');
+	console.log('Connect to http://<host_name>:' + port + '/');
+});
+
+var WebSocketServer = wsm.Server, wss = new WebSocketServer({
+	server : server,
+	path : '/call'
+});
+
+
+wss.on('connection', function(ws) {
+
+	var sessionId = nextUniqueId();
+
+	console.log('Connection received with sessionId ' + sessionId);
+
+	ws.on('error', function(error) {
+		console.log('Connection ' + sessionId + ' error');
+		stop(sessionId);
+	});
+
+	ws.on('close', function() {
+		console.log('Connection ' + sessionId + ' closed');		
+		stop(sessionId);
+		userRegistry.unregister(sessionId);
+	});
+
+	ws.on('message', function(_message) {
+		var message = JSON.parse(_message);
+		console.log('Connection ' + sessionId + ' received message ', message);
+
+		switch (message.id) {
+		case 'register':
+			register(sessionId, message.name, ws);				
+			break;
+
+		case 'call':
+			call(sessionId, message.to, message.from, message.sdpOffer);
+			break;
+
+		case 'incomingCallResponse':
+			incomingCallResponse(sessionId, message.from, message.callResponse, message.sdpOffer);
+			break;
+		
+		case 'stop':
+			stop(sessionId);
+			break;
+
+		default:
+			ws.send(JSON.stringify({
+				id : 'error',
+				message : 'Invalid message ' + message
+			}));
+			break;
+		}
+
+	});
+});
+
+function stop(sessionId){
+	if(!pipelines[sessionId]){
+		return;
+	}
+	
+	var pipeline = pipelines[sessionId];
+	delete pipelines[sessionId];
+	pipeline.release();
+	var stopperUser = userRegistry.getById(sessionId);
+	var stoppedUser = userRegistry.getByName(stopperUser.peer);
+	stopperUser.peer = null;
+	if(stoppedUser){
+		stoppedUser.peer = null;
+		delete pipelines[stoppedUser.id];
+		var message = {
+			id: 'stopCommunication',
+			message: 'remote user hanged out'
+		}
+		stoppedUser.sendMessage(message)
+	}
+}
+
+function incomingCallResponse(calleeId, from, callResponse, calleeSdp){
+	
+	function onError(callerReason, calleeReason){
+		if(pipeline) pipeline.release();
+		if(caller){
+			var callerMessage = {
+				id: 'callResponse',
+				response: 'rejected'
+			};
+			if(callerReason) callerMessage.message = callerReason;
+			caller.sendMessage(callerMessage);
+		}
+			
+		var calleeMessage = {
+			id: 'stopCommunication'
+		};
+		if(calleeReason) calleeMessage.message = calleeReason;
+		callee.sendMessage(calleeMessage);
+	}
+	
+	var callee = userRegistry.getById(calleeId);
+	if(!from || !userRegistry.getByName(from)){
+		return onError(null, 'unknown from = ' + from);
+	}		
+	var caller = userRegistry.getByName(from);
+	
+	if(callResponse === 'accept'){	
+		var pipeline = new CallMediaPipeline();	
+		
+		pipeline.createPipeline(function(error){
+			if(error){
+				return onError(error, error);
+			}
+			
+			pipeline.generateSdpAnswerForCaller(caller.sdpOffer, function(error, callerSdpAnswer){
+				if(error){
+					return onError(error, error);
+				}
+				
+				pipeline.generateSdpAnswerForCallee(calleeSdp, function(error, calleeSdpAnswer){
+					if(error){
+						return onError(error, error);
+					}
+					
+					pipelines[caller.id] = pipeline;
+					pipelines[callee.id] = pipeline;
+					
+					var message = {
+						id: 'startCommunication',
+						sdpAnswer: calleeSdpAnswer
+					};
+					
+					callee.sendMessage(message);
+					
+					message = {
+						id: 'callResponse',
+						response : 'accepted',
+						sdpAnswer: callerSdpAnswer
+					};
+					
+					caller.sendMessage(message);					
+				});				
+			});			
+		});
+	} else {
+		
+		var decline = {
+			id: 'callResponse',
+			response: 'rejected',
+			message: 'user declined'
+		};
+		
+		caller.sendMessage(decline);
+	}
+}
+
+function call(callerId, to, from, sdpOffer){
+	var caller = userRegistry.getById(callerId);
+	var rejectCause = 'user ' + to + ' is not registered';
+	if(userRegistry.getByName(to)){
+		var callee = userRegistry.getByName(to);
+		caller.sdpOffer = sdpOffer
+		callee.peer = from;
+		caller.peer = to;
+		var message = {
+				id: 'incomingCall',
+				from: from
+				};
+		try{
+			return callee.sendMessage(message);
+		} catch(exception){
+			rejectCause = "Error " + exception;
+		}
+	} 
+	var message  = {
+		id: 'callResponse',
+		response: 'rejected: ',
+		message: rejectCause
+	};
+	caller.sendMessage(message);
+	
+}
+
+function register(id, name, ws, callback){
+	function onError(error){
+		console.log("Error processing register: " + error);
+		ws.send(JSON.stringify({id:'registerResponse', response : 'rejected ', message: error}));
+	}
+	
+	if(!name){
+		return onError("empty user name");
+	}
+	
+	if(userRegistry.getByName(name)){
+		return onError("already registered");
+	}
+	
+	userRegistry.register(new UserSession(id, name, ws));
+	try {
+		ws.send(JSON.stringify({id: 'registerResponse', response: 'accepted'}));
+	} catch(exception){
+		onError(exception);
+	}
+}
+
+//Recover kurentoClient for the first time.
+function getKurentoClient(callback) {
+	if (kurentoClient !== null) {
+		return callback(null, kurentoClient);
+	}
+
+	kurento(ws_uri, function(error, _kurentoClient) {
+		if (error) {
+			var message = 'Coult not find media server at address ' + ws_uri;
+			console.log(message);
+			return callback(message + ". Exiting with error " + error);
+		}
+
+		kurentoClient = _kurentoClient;
+		callback(null, kurentoClient);
+	});
+}
+
+app.use(express.static(path.join(__dirname, 'static')));
---
+++ kurento-one2one-call/static/js/index.js
@@ -0,0 +1,273 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+var ws = new WebSocket('ws://' + location.host + '/call');
+var videoInput;
+var videoOutput;
+var webRtcPeer;
+
+
+var registerName = null;
+const NOT_REGISTERED = 0;
+const REGISTERING = 1;
+const REGISTERED = 2;
+var registerState = null
+
+function setRegisterState(nextState){
+	switch (nextState) {
+	case NOT_REGISTERED:
+		$('#register').attr('disabled', false);
+		$('#call').attr('disabled', true);
+		$('#terminate').attr('disabled', true);
+		break;
+		
+	case REGISTERING:
+		$('#register').attr('disabled', true);
+		break;
+		
+	case REGISTERED:
+		$('#register').attr('disabled', true);
+		setCallState(NO_CALL);
+		break;
+		
+	default:
+		return;
+	}
+	registerState = nextState;
+}
+
+const NO_CALL = 0;
+const PROCESSING_CALL = 1;
+const IN_CALL =2;
+var callState = null
+
+function setCallState(nextState){
+	switch (nextState) {
+	case NO_CALL:
+		$('#call').attr('disabled', false);
+		$('#terminate').attr('disabled', true);
+		break;
+
+	case PROCESSING_CALL:
+		$('#call').attr('disabled', true);
+		$('#terminate').attr('disabled', true);
+		break;
+	case IN_CALL:
+		$('#call').attr('disabled', true);
+		$('#terminate').attr('disabled', false);
+		break;
+	default:
+		return;
+	}
+	callState = nextState;
+}
+
+
+window.onload = function() {
+	setRegisterState(NOT_REGISTERED);
+	console = new Console('console', console);
+	dragDrop.initElement('videoSmall');
+	videoInput = document.getElementById('videoInput');
+	videoOutput = document.getElementById('videoOutput');
+	document.getElementById('name').focus();
+}
+
+window.onbeforeunload = function() {
+	ws.close();
+}
+
+ws.onmessage = function(message) {
+	var parsedMessage = JSON.parse(message.data);
+	console.info('Received message: ' + message.data);
+
+	switch (parsedMessage.id) {
+	case 'registerResponse':
+		resgisterResponse(parsedMessage);
+		break;
+	case 'callResponse':
+		callResponse(parsedMessage);
+		break;
+	case 'incomingCall':
+		incomingCall(parsedMessage);
+		break;
+	case 'startCommunication':
+		startCommunication(parsedMessage);
+		break;
+	case 'stopCommunication':
+		console.info("Communication ended by remote peer");
+		stop(true);
+		break;
+	default:
+		console.error('Unrecognized message', parsedMessage);
+	}
+}
+
+function resgisterResponse(message) {
+	if(message.response == 'accepted'){
+		setRegisterState(REGISTERED);
+	} else {
+		setRegisterState(NOT_REGISTERED);
+		var errorMessage = message.message ? message.message : 'Unknown reason for register rejection.';
+		console.log(errorMessage);
+		alert('Error registering user. See console for further information.');
+	}
+	
+}
+
+function callResponse(message) {
+	if (message.response != 'accepted') {
+		console.info('Call not accepted by peer. Closing call');
+		var errorMessage = message.message ? message.message : 'Unknown reason for call rejection.';
+		console.log(errorMessage);
+		stop(true);
+	} else {
+		setCallState(IN_CALL);
+		webRtcPeer.processSdpAnswer(message.sdpAnswer);
+	}
+}
+
+function startCommunication(message) {
+	setCallState(IN_CALL);
+	webRtcPeer.processSdpAnswer(message.sdpAnswer);
+}
+
+function incomingCall(message) {
+	
+	//If bussy just reject without disturbing user
+	if(callState != NO_CALL){
+		var response = {
+				id : 'incomingCallResponse',
+				from : message.from,
+				callResponse : 'reject',
+				message : 'bussy'
+				
+			};
+			return sendMessage(response);
+	}
+	
+	setCallState(PROCESSING_CALL);
+	if (confirm('User ' + message.from
+			+ ' is calling you. Do you accept the call?')) {
+		showSpinner(videoInput, videoOutput);
+		webRtcPeer = kurentoUtils.WebRtcPeer.startSendRecv(videoInput,
+				videoOutput, function(sdp, wp) {
+					var response = {
+						id : 'incomingCallResponse',
+						from : message.from,
+						callResponse : 'accept',
+						sdpOffer : sdp
+					};
+					sendMessage(response);
+				}, function(error){
+					setCallState(NO_CALL);
+				});
+	} else {
+		var response = {
+			id : 'incomingCallResponse',
+			from : message.from,
+			callResponse : 'reject',
+			message : 'user declined'
+		};
+		sendMessage(response);
+		stop(true);
+	}
+}
+
+function register() {	
+	var name = document.getElementById('name').value;
+	if(name == '') {
+		window.alert("You must insert your user name");
+		return;
+	}
+	
+	setRegisterState(REGISTERING);
+	
+	var message = {
+		id : 'register',
+		name : name
+	};
+	sendMessage(message);
+	document.getElementById('peer').focus();
+}
+
+function call() {
+	
+	if(document.getElementById('peer').value == ''){
+		window.alert("You must specify the peer name");
+		return;
+	}
+	
+	setCallState(PROCESSING_CALL);
+	
+	showSpinner(videoInput, videoOutput);
+
+	kurentoUtils.WebRtcPeer.startSendRecv(videoInput, videoOutput, function(
+			offerSdp, wp) {
+		webRtcPeer = wp;
+		console.log('Invoking SDP offer callback function');
+		var message = {
+			id : 'call',
+			from : document.getElementById('name').value,
+			to : document.getElementById('peer').value,
+			sdpOffer : offerSdp
+		};
+		sendMessage(message);
+	}, function(error){
+		console.log(error);
+		setCallState(NO_CALL);
+	});
+}
+
+function stop(message) {
+	setCallState(NO_CALL);
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+
+		if (!message) {
+			var message = {
+				id : 'stop'
+			}
+			sendMessage(message);
+		}
+	}
+	videoInput.src = '';
+	videoOutput.src = '';
+	hideSpinner(videoInput, videoOutput);
+}
+
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	console.log('Senging message: ' + jsonMessage);
+	ws.send(jsonMessage);
+}
+
+function showSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = './img/transparent-1px.png';
+		arguments[i].style.background = 'center transparent url("./img/spinner.gif") no-repeat';
+	}
+}
+
+function hideSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = './img/webrtc.png';
+		arguments[i].style.background = '';
+	}
+}
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
