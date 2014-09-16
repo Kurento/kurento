210d1e6cfa5f55bb50261acd0ae24102f41a55db
Boni Garcia <bgarcia@gsyc.es>
2014-09-16 15:17:12 +0200

    Removed examples since all of them uses KwsMedia
    Change-Id: Ifdf96fc0c02403a43f24bf1f1cf2fba919d27bdf

--- example/PlayerEndpoint-HttpGetEndpoint_OAuth2/config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.idmURL = 'https://account.lab.fi-ware.org';
-config.client_id = '530';
-config.client_secret = 'a770a15c351e82bc31aa0f904d224326662a055c456b423dd200fd223c515bedc950b1f15df24512e60337b2003dbb08e873008f00e1ddd8b2997fb1071ad744';
-config.callbackURL = 'http://localhost/login';
-
-module.exports = config;
