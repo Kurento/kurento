1275c74096f7269e0cf13e57ca83637585107685
Santiago Carot-Nemesio <sancane@gmail.com>
2014-10-07 15:30:09 +0200

    kmsrecorderendpoint: Configure caps basing on the recording profile
    Change-Id: I179b5db8db1f7a4af7d425c254ab623f0b679032

--- src/gst-plugins/kmsrecorderendpoint.c
+++ src/gst-plugins/kmsrecorderendpoint.c
@@ -87,6 +87,7 @@ struct state_controller
 
 struct _KmsRecorderEndpointPrivate
 {
+  KmsRecordingProfile profile;
   GstElement *pipeline;
   GstClockTime paused_time;
   GstClockTime paused_start;
@@ -729,11 +730,9 @@ kms_recorder_endpoint_set_property (GObject * object, guint property_id,
           self->priv->use_dvr, NULL);
       break;
     case PROP_PROFILE:{
-      KmsRecordingProfile profile;
-
-      profile = g_value_get_enum (value);
-      g_object_set (G_OBJECT (self->priv->controller), "profile", profile,
-          NULL);
+      self->priv->profile = g_value_get_enum (value);
+      g_object_set (G_OBJECT (self->priv->controller), "profile",
+          self->priv->profile, NULL);
       break;
     }
     default:
@@ -755,11 +754,7 @@ kms_recorder_endpoint_get_property (GObject * object, guint property_id,
       g_value_set_boolean (value, self->priv->use_dvr);
       break;
     case PROP_PROFILE:{
-      KmsRecordingProfile profile;
-
-      g_object_get (G_OBJECT (self->priv->controller), "profile", &profile,
-          NULL);
-      g_value_set_enum (value, profile);
+      g_value_set_enum (value, self->priv->profile);
       break;
     }
     default:
@@ -767,6 +762,210 @@ kms_recorder_endpoint_get_property (GObject * object, guint property_id,
       break;
   }
   KMS_ELEMENT_UNLOCK (KMS_ELEMENT (self));
+}
+
+static GstCaps *
+kms_recorder_endpoint_get_caps_from_profile (KmsRecorderEndpoint * self,
+    KmsElementPadType type)
+{
+  GstEncodingContainerProfile *cprof;
+  const GList *profiles, *l;
+  GstCaps *caps = NULL;
+
+  switch (type) {
+    case KMS_ELEMENT_PAD_TYPE_VIDEO:
+      cprof =
+          kms_recording_profile_create_profile (self->priv->profile, FALSE,
+          TRUE);
+      break;
+    case KMS_ELEMENT_PAD_TYPE_AUDIO:
+      cprof =
+          kms_recording_profile_create_profile (self->priv->profile, TRUE,
+          FALSE);
+      break;
+    default:
+      return NULL;
+  }
+
+  profiles = gst_encoding_container_profile_get_profiles (cprof);
+
+  for (l = profiles; l != NULL; l = l->next) {
+    GstEncodingProfile *prof = l->data;
+
+    if ((GST_IS_ENCODING_AUDIO_PROFILE (prof) &&
+            type == KMS_ELEMENT_PAD_TYPE_AUDIO) ||
+        (GST_IS_ENCODING_VIDEO_PROFILE (prof) &&
+            type == KMS_ELEMENT_PAD_TYPE_VIDEO)) {
+      caps = gst_encoding_profile_get_input_caps (prof);
+      break;
+    }
+  }
+
+  return caps;
+}
+
+static GstCaps *
+kms_recorder_endpoint_allowed_caps (KmsElement * self, KmsElementPadType type)
+{
+  GstElement *valve;
+  GstPad *srcpad;
+  GstCaps *caps;
+
+  switch (type) {
+    case KMS_ELEMENT_PAD_TYPE_VIDEO:
+      valve = kms_element_get_video_valve (self);
+      break;
+    case KMS_ELEMENT_PAD_TYPE_AUDIO:
+      valve = kms_element_get_audio_valve (self);
+      break;
+    default:
+      return NULL;
+  }
+
+  srcpad = gst_element_get_static_pad (valve, "src");
+  caps = gst_pad_get_allowed_caps (srcpad);
+  gst_object_unref (srcpad);
+
+  return caps;
+}
+
+static gboolean
+kms_recorder_endpoint_query_caps (KmsElement * element, GstPad * pad,
+    GstQuery * query)
+{
+  KmsRecorderEndpoint *self = KMS_RECORDER_ENDPOINT (element);
+  GstCaps *allowed = NULL, *caps = NULL;
+  GstCaps *filter, *result = NULL, *tcaps;
+
+  gst_query_parse_caps (query, &filter);
+
+  switch (kms_element_get_pad_type (element, pad)) {
+    case KMS_ELEMENT_PAD_TYPE_VIDEO:
+      allowed =
+          kms_recorder_endpoint_allowed_caps (element,
+          KMS_ELEMENT_PAD_TYPE_VIDEO);
+      caps =
+          kms_recorder_endpoint_get_caps_from_profile (self,
+          KMS_ELEMENT_PAD_TYPE_VIDEO);
+      break;
+    case KMS_ELEMENT_PAD_TYPE_AUDIO:{
+      allowed =
+          kms_recorder_endpoint_allowed_caps (element,
+          KMS_ELEMENT_PAD_TYPE_AUDIO);
+      caps =
+          kms_recorder_endpoint_get_caps_from_profile (self,
+          KMS_ELEMENT_PAD_TYPE_AUDIO);
+      break;
+    }
+    default:
+      GST_DEBUG ("unknown pad");
+      return FALSE;
+  }
+
+  /* make sure we only return results that intersect our padtemplate */
+  tcaps = gst_pad_get_pad_template_caps (pad);
+  if (tcaps != NULL) {
+    if (allowed == NULL) {
+      result = tcaps;
+    } else {
+      result = gst_caps_intersect (allowed, tcaps);
+    }
+    gst_caps_unref (tcaps);
+  }
+
+  if (caps != NULL) {
+    /* Filter against profile */
+    result = gst_caps_intersect (caps, result);
+  }
+
+  /* filter against the query filter when needed */
+  if (filter != NULL) {
+    result = gst_caps_intersect (result, filter);
+  }
+
+  gst_query_set_caps_result (query, result);
+  gst_caps_unref (result);
+
+  if (allowed != NULL)
+    gst_caps_unref (allowed);
+
+  if (caps != NULL)
+    gst_caps_unref (caps);
+
+  return TRUE;
+}
+
+static gboolean
+kms_recorder_endpoint_query_accept_caps (KmsElement * element, GstPad * pad,
+    GstQuery * query)
+{
+  KmsRecorderEndpoint *self = KMS_RECORDER_ENDPOINT (element);
+  GstCaps *caps, *accept;
+  GstElement *valve;
+  gboolean ret = TRUE;;
+
+  switch (kms_element_get_pad_type (element, pad)) {
+    case KMS_ELEMENT_PAD_TYPE_VIDEO:
+      valve = kms_element_get_video_valve (element);
+      caps = kms_recorder_endpoint_get_caps_from_profile (self,
+          KMS_ELEMENT_PAD_TYPE_VIDEO);
+      break;
+    case KMS_ELEMENT_PAD_TYPE_AUDIO:{
+      valve = kms_element_get_audio_valve (element);
+      caps = kms_recorder_endpoint_get_caps_from_profile (self,
+          KMS_ELEMENT_PAD_TYPE_AUDIO);
+      break;
+    }
+    default:
+      GST_DEBUG ("unknown pad");
+      return FALSE;
+  }
+
+  if (caps == NULL) {
+    return
+        KMS_ELEMENT_CLASS (kms_recorder_endpoint_parent_class)->sink_query
+        (element, pad, query);
+  }
+
+  gst_query_parse_accept_caps (query, &accept);
+
+  ret = gst_caps_can_intersect (accept, caps);
+
+  if (ret) {
+    GstPad *srcpad;
+
+    srcpad = gst_element_get_static_pad (valve, "src");
+    ret = gst_pad_peer_query_accept_caps (srcpad, caps);
+    gst_object_unref (srcpad);
+  }
+
+  gst_caps_unref (caps);
+
+  gst_query_set_accept_caps_result (query, ret);
+
+  return TRUE;
+}
+
+static gboolean
+kms_recorder_endpoint_sink_query (KmsElement * self, GstPad * pad,
+    GstQuery * query)
+{
+  gboolean ret;
+
+  switch (GST_QUERY_TYPE (query)) {
+    case GST_QUERY_CAPS:
+      ret = kms_recorder_endpoint_query_caps (self, pad, query);
+      break;
+    case GST_QUERY_ACCEPT_CAPS:
+      ret = kms_recorder_endpoint_query_accept_caps (self, pad, query);
+      break;
+    default:
+      ret =
+          KMS_ELEMENT_CLASS (kms_recorder_endpoint_parent_class)->sink_query
+          (self, pad, query);
+  }
+
+  return ret;
 }
 
 static void
@@ -797,6 +996,8 @@ kms_recorder_endpoint_class_init (KmsRecorderEndpointClass * klass)
       GST_DEBUG_FUNCPTR (kms_recorder_endpoint_audio_valve_removed);
   kms_element_class->video_valve_removed =
       GST_DEBUG_FUNCPTR (kms_recorder_endpoint_video_valve_removed);
+  kms_element_class->sink_query =
+      GST_DEBUG_FUNCPTR (kms_recorder_endpoint_sink_query);
 
   gobject_class->set_property =
       GST_DEBUG_FUNCPTR (kms_recorder_endpoint_set_property);
