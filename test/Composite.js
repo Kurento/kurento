bf001b89fd9d1b42ca203f738ab5b956fdffd507
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2014-11-05 16:27:53 +0100

    Test for Composite (Hub & HubPort)
    Change-Id: Ieb652d9c3f64c38335a2f9f25a4051f3e1584eda

---
+++ test/Composite.js
@@ -0,0 +1,75 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+/**
+ * {@link Composite} test suite.
+ *
+ * <p>
+ * Methods tested:
+ * <ul>
+ * <li>{@link Composite#getLocalSessionDescriptor()}
+ * </ul>
+ * <p>
+ * Events tested:
+ * <ul>
+ * <li>{@link WebRtcEndpoint#addMediaSessionStartListener(MediaEventListener)}
+ * <li>
+ * {@link HttpEndpoint#addMediaSessionTerminatedListener(MediaEventListener)}
+ * </ul>
+ *
+ *
+ * @author Jesús Leganés Combarro "piranna" (piranna@gmail.com)
+ * @since 4.2.4
+ *
+ */
+
+if(typeof QUnit == 'undefined')
+{
+  QUnit = require('qunit-cli');
+  QUnit.load();
+
+  kurentoClient = require('..');
+
+  require('./_common');
+  require('./_proxy');
+};
+
+
+QUnit.module('Composite', lifecycle);
+
+QUnit.asyncTest('create', function()
+{
+  QUnit.expect(4);
+
+  this.pipeline.create('Composite', function(error, composite)
+  {
+    QUnit.equal(error, undefined, 'create composite');
+
+    if(error) return onerror(error);
+
+    QUnit.notEqual(composite, undefined, 'composite');
+
+    composite.createHubPort(function(error, hubPort)
+    {
+      QUnit.equal(error, undefined, 'createHubPort');
+
+      if(error) return onerror(error);
+
+      QUnit.notEqual(hubPort, undefined, 'hubPort');
+
+      QUnit.start();
+    });
+  });
+});
