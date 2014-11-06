514eeb420751fdb41a963a0331ab7eac26b4afa2
David Fernandez <d.fernandezlop@gmail.com>
2014-11-06 21:51:44 +0100

    Add demo kurento-alvar
    Bowerized some external dependencies. Set common directory for resources.
    Fixed release when application is stopped and when onError is executed.
    
    Change-Id: I62d4a425924f369845de8fdbea2638e3600a1ac2

---
+++ kurento-alvar/js/index.js
@@ -0,0 +1,131 @@
+/*
+* (C) Copyright 2014 Kurento (http://kurento.org/)
+*
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the GNU Lesser General Public License
+* (LGPL) version 2.1 which accompanies this distribution, and is available at
+* http://www.gnu.org/licenses/lgpl-2.1.html
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+*/
+
+
+const ws_uri = 'ws://' + location.hostname + ':8888/kurento';
+const logo_uri = '/var/kurento/img/kurento-logo.png'; //Edit to fill the location of your logo
+
+kurentoClient.register(kurentoModuleMarkerdetector)
+
+var videoInput;
+var videoOutput;
+var webRtcPeer;
+var pipeline;
+
+window.onload = function() {
+	console = new Console('console', console);
+	videoInput = document.getElementById('videoInput');
+	videoOutput = document.getElementById('videoOutput');
+
+	$('#stop').attr('disabled', true);
+	$('#start').attr('disabled', false);
+}
+
+function start() {
+	showSpinner(videoInput, videoOutput);
+	webRtcPeer = kurentoUtils.WebRtcPeer.startSendRecv(videoInput, videoOutput, onOffer, onError);
+
+	$('#stop').attr('disabled', false);
+	$('#start').attr('disabled', true);
+}
+
+function stop() {
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+		webRtcPeer = null;
+	}
+	
+	if(pipeline){
+		pipeline.release();
+		pipeline = null;
+	}
+	
+	videoInput.src = '';
+	videoOutput.src = '';
+	hideSpinner(videoInput, videoOutput);
+	
+
+	$('#stop').attr('disabled', true);
+	$('#start').attr('disabled', false);
+}
+
+function onOffer(sdpOffer) {
+    kurentoClient(ws_uri, function(error, client) {
+		if (error) return onError(error);
+
+		client.create('MediaPipeline', function(error, p) {
+			if (error) return onError(error);
+
+			pipeline = p;
+			pipeline.create('WebRtcEndpoint', function(error, webRtc) {
+				if (error) return onError(error);
+
+				pipeline.create('ArMarkerdetector', function(error, filter) {
+					if (error) return onError(error);
+
+						webRtc.connect(filter, function(error) {
+						if (error) return onError(error);
+
+						console.log("WebRtcEndpoint --> filter");
+
+						filter.setOverlayImage(logo_uri, function(error) {
+							if (error) return onError(error);
+
+							console.log("Set Image");
+			    		});
+
+						filter.connect(webRtc, function(error) {
+							if (error) return onError(error);
+
+							console.log("Filter --> WebRtcEndpoint");
+						});
+					});
+
+					webRtc.processOffer(sdpOffer, function(error, sdpAnswer) {
+						if (error) return onError(error);
+
+						console.log("SDP answer obtained. Processing ...");
+
+						webRtcPeer.processSdpAnswer(sdpAnswer);
+					});
+				});
+			});
+		});
+	});
+}
+
+function onError(error) {
+	console.error(error);
+	stop();
+}
+
+function showSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = 'http://files.kurento.org/imgs/transparent-1px.png';
+		arguments[i].style.background = "center transparent url('http://files.kurento.org/imgs/spinner.gif') no-repeat";
+	}
+}
+
+function hideSpinner() {
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = './img/webrtc.png';
+		arguments[i].style.background = '';
+	}
+}
+
+$(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
+	event.preventDefault();
+	$(this).ekkoLightbox();
+});
