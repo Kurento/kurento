7bd6174cc5409f374eac03ce8acf4353215c380d
mgallego <micael.gallego@gmail.com>
2014-11-13 14:48:51 +0100

    [kurento-jsonrpc-client] Improve logs
    Change-Id: I5d09b253bbe5d7d35dc9452241232711b5dbd842

--- kurento-jsonrpc/kurento-jsonrpc-client/src/main/java/org/kurento/jsonrpc/client/JsonRpcClientWebSocket.java
+++ kurento-jsonrpc/kurento-jsonrpc-client/src/main/java/org/kurento/jsonrpc/client/JsonRpcClientWebSocket.java
@@ -177,7 +177,7 @@ public class JsonRpcClientWebSocket extends JsonRpcClient {
 
 			} catch (DeploymentException | URISyntaxException e) {
 				throw new KurentoException(
-						"Exception connecting to KMS server", e);
+						"Exception connecting to WebSocket server", e);
 			}
 
 			try {
