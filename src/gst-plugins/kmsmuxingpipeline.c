34173b337de7fc60d6c7ead2571a677fcfff268d
Santiago Carot-Nemesio <sancane@gmail.com>
2014-11-21 12:43:25 +0100

    kmsmuxingpipeline: Initialize pipeline
    Change-Id: Icb1419d5ee69357b9918b1b31a232a619826666f

--- src/gst-plugins/kmsmuxingpipeline.c
+++ src/gst-plugins/kmsmuxingpipeline.c
@@ -24,6 +24,7 @@
 #include "kmsmuxingpipeline.h"
 
 #define OBJECT_NAME "muxingpipeline"
+#define KMS_MUXING_PIPELINE_NAME OBJECT_NAME
 
 GST_DEBUG_CATEGORY_STATIC (kms_muxing_pipeline_debug_category);
 #define GST_CAT_DEFAULT kms_muxing_pipeline_debug_category
@@ -150,6 +151,95 @@ kms_muxing_pipeline_init (KmsMuxingPipeline * self)
   self->priv = KMS_MUXING_PIPELINE_GET_PRIVATE (self);
 }
 
+static void
+kms_muxing_pipeline_configure (KmsMuxingPipeline * self)
+{
+  GstEncodingContainerProfile *cprof;
+  const GList *profiles, *l;
+  GstElement *appsrc;
+
+  cprof =
+      kms_recording_profile_create_profile (self->priv->profile, TRUE, TRUE);
+
+  profiles = gst_encoding_container_profile_get_profiles (cprof);
+
+  for (l = profiles; l != NULL; l = l->next) {
+    GstEncodingProfile *prof = l->data;
+    GstCaps *caps;
+
+    if (GST_IS_ENCODING_AUDIO_PROFILE (prof)) {
+      appsrc = self->priv->audiosrc;
+    } else if (GST_IS_ENCODING_VIDEO_PROFILE (prof)) {
+      appsrc = self->priv->videosrc;
+    } else
+      continue;
+
+    caps = gst_encoding_profile_get_input_caps (prof);
+
+    g_object_set (G_OBJECT (appsrc), "is-live", TRUE, "do-timestamp", FALSE,
+        "min-latency", G_GUINT64_CONSTANT (0), "max-latency",
+        G_GUINT64_CONSTANT (0), "format", GST_FORMAT_TIME, "caps", caps, NULL);
+
+    gst_caps_unref (caps);
+  }
+
+  g_object_set (G_OBJECT (self->priv->encodebin), "profile", cprof,
+      "audio-jitter-tolerance", 100 * GST_MSECOND,
+      "avoid-reencoding", TRUE, NULL);
+  gst_encoding_profile_unref (cprof);
+
+  if (self->priv->profile == KMS_RECORDING_PROFILE_MP4) {
+    GstElement *mux =
+        gst_bin_get_by_name (GST_BIN (self->priv->encodebin), "muxer");
+
+    g_object_set (G_OBJECT (mux), "fragment-duration", 2000, "streamable", TRUE,
+        NULL);
+
+    g_object_unref (mux);
+  } else if (self->priv->profile == KMS_RECORDING_PROFILE_WEBM) {
+    GstElement *mux =
+        gst_bin_get_by_name (GST_BIN (self->priv->encodebin), "muxer");
+
+    g_object_set (G_OBJECT (mux), "streamable", TRUE, NULL);
+
+    g_object_unref (mux);
+  }
+}
+
+static void
+kms_muxing_pipeline_prepare_pipeline (KmsMuxingPipeline * self)
+{
+  self->priv->pipeline = gst_pipeline_new (KMS_MUXING_PIPELINE_NAME);
+  self->priv->videosrc = gst_element_factory_make ("appsrc", NULL);
+  self->priv->audiosrc = gst_element_factory_make ("appsrc", NULL);
+  self->priv->encodebin = gst_element_factory_make ("encodebin", NULL);
+
+  kms_muxing_pipeline_configure (self);
+
+  gst_bin_add_many (GST_BIN (self->priv->pipeline), self->priv->videosrc,
+      self->priv->audiosrc, self->priv->encodebin, self->priv->sink, NULL);
+
+  if (!gst_element_link (self->priv->encodebin, self->priv->sink)) {
+    GST_ERROR_OBJECT (self, "Could not link elements: %"
+        GST_PTR_FORMAT ", %" GST_PTR_FORMAT,
+        self->priv->encodebin, self->priv->sink);
+  }
+
+  if (!gst_element_link_pads (self->priv->videosrc, "src",
+          self->priv->encodebin, "video_%u")) {
+    GST_ERROR_OBJECT (self, "Could not link elements: %"
+        GST_PTR_FORMAT ", %" GST_PTR_FORMAT,
+        self->priv->videosrc, self->priv->encodebin);
+  }
+
+  if (!gst_element_link_pads (self->priv->audiosrc, "src",
+          self->priv->encodebin, "audio_%u")) {
+    GST_ERROR_OBJECT (self, "Could not link elements: %"
+        GST_PTR_FORMAT ", %" GST_PTR_FORMAT,
+        self->priv->audiosrc, self->priv->encodebin);
+  }
+}
+
 KmsMuxingPipeline *
 kms_muxing_pipeline_new (const char *optname1, ...)
 {
@@ -161,6 +251,8 @@ kms_muxing_pipeline_new (const char *optname1, ...)
   obj = KMS_MUXING_PIPELINE (g_object_new_valist (KMS_TYPE_MUXING_PIPELINE,
           optname1, ap));
   va_end (ap);
+
+  kms_muxing_pipeline_prepare_pipeline (obj);
 
   return obj;
 }
