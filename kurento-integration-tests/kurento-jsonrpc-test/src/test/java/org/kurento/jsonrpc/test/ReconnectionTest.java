a19d7a3414fddfff56d9aa552e759f5103ecd6bb
Ivan Gracia <izanmail@gmail.com>
2014-12-04 18:17:00 +0100

    [kurento-jsonrpc-test] added thread sleep afer disconnection
    Change-Id: I915fa7641f792e32fbb585c78d8429e7871f4a19

--- kurento-integration-tests/kurento-jsonrpc-test/src/test/java/org/kurento/jsonrpc/test/ReconnectionTest.java
+++ kurento-integration-tests/kurento-jsonrpc-test/src/test/java/org/kurento/jsonrpc/test/ReconnectionTest.java
@@ -16,94 +16,98 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReconnectionTest extends JsonRpcConnectorBaseTest {
-
+	
 	private static final Logger log = LoggerFactory
 			.getLogger(ReconnectionTest.class);
-
+	
 	public static class Handler extends DefaultJsonRpcHandler<String> {
-
+		
 		@Override
 		public void handleRequest(final Transaction transaction,
 				Request<String> request) throws Exception {
-
+			
 			Session session = transaction.getSession();
-
+			
 			if (session.isNew()) {
 				transaction.sendResponse("new");
 			} else {
 				transaction.sendResponse("old");
 			}
 		}
-
+		
 		@Override
 		public void afterConnectionEstablished(Session session)
 				throws Exception {
 			session.setReconnectionTimeout(5000);
 		}
 	}
-
+	
 	@Test
-	public void test() throws IOException {
-
+	public void test() throws IOException, InterruptedException {
+		
 		JsonRpcClient client = createJsonRpcClient("/reconnection");
-
+		
 		if (client instanceof JsonRpcClientWebSocket) {
-
+			
 			Assert.assertEquals("new",
 					client.sendRequest("sessiontest", String.class));
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
-
+			
 			log.info("SessionId: " + client.getSession().getSessionId());
-
-			JsonRpcClientWebSocket webSocketClient = (JsonRpcClientWebSocket) client;
+			
+			JsonRpcClientWebSocket webSocketClient =
+					(JsonRpcClientWebSocket) client;
 			webSocketClient.closeNativeSession();
-
+			
+			Thread.sleep(100);
+			
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
-
+			
 			client.setSessionId(null);
-
+			
 			// With this we test if the transportId is used to recognize the
 			// session
-
+			
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
-
+			
 			log.info("SessionId: " + client.getSession().getSessionId());
-
+			
 		}
-
+		
 		client.close();
-
+		
 	}
-
+	
 	@Test
 	public void timeoutReconnectionTest() throws IOException,
 			InterruptedException {
-
+		
 		JsonRpcClient client = createJsonRpcClient("/reconnection");
-
+		
 		if (client instanceof JsonRpcClientWebSocket) {
-
+			
 			Assert.assertEquals("new",
 					client.sendRequest("sessiontest", String.class));
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
 			Assert.assertEquals("old",
 					client.sendRequest("sessiontest", String.class));
-
-			JsonRpcClientWebSocket webSocketClient = (JsonRpcClientWebSocket) client;
+			
+			JsonRpcClientWebSocket webSocketClient =
+					(JsonRpcClientWebSocket) client;
 			webSocketClient.closeNativeSession();
-
+			
 			Thread.sleep(1000);
-
+			
 			try {
 				Assert.assertEquals("old",
 						client.sendRequest("sessiontest", String.class));
@@ -111,9 +115,9 @@ public class ReconnectionTest extends JsonRpcConnectorBaseTest {
 				Assert.fail("Reconnection should be happend behind the covers");
 			}
 		}
-
+		
 		client.close();
-
+		
 	}
-
+	
 }
