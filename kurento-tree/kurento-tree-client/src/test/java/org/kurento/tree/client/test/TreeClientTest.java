69749d00c463228c4a4acb4fecb0168502d95848
Ivan Gracia <izanmail@gmail.com>
2014-12-05 20:09:16 +0100

    [kurento-tree-client] added dummy test for tree client
    Change-Id: I2630c0ab266f4adb593ce35e958dcd8510d8e7f3

---
+++ kurento-tree/kurento-tree-client/src/test/java/org/kurento/tree/client/test/TreeClientTest.java
@@ -0,0 +1,13 @@
+package org.kurento.tree.client.test;
+
+import static org.hamcrest.CoreMatchers.anything;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class TreeClientTest {
+	@Test
+	public void emptyTest() {
+		assertThat(0, anything());
+	}
+}
