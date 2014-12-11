a81ecb8c8c6da772da99bf43c8a37531ead25f06
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2014-12-11 11:47:19 +0100

    Method to get server info
    Change-Id: I55989b63d842722256d13f450961d03bdb122319

---
+++ test/ServerManager.js
@@ -0,0 +1,76 @@
+/*
+ * (C) Copyright 2013-2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+/**
+ * {@link HttpEndpoint} test suite.
+ *
+ * <p>
+ * Methods tested:
+ * <ul>
+ * <li>{@link HttpEndpoint#getUrl()}
+ * </ul>
+ * <p>
+ * Events tested:
+ * <ul>
+ * <li>{@link HttpEndpoint#addMediaSessionStartListener(MediaEventListener)}
+ * <li>
+ * {@link HttpEndpoint#addMediaSessionTerminatedListener(MediaEventListener)}
+ * </ul>
+ *
+ *
+ * @author Jesús Leganés Combarro "piranna" (piranna@gmail.com)
+ * @since 5.0.5
+ *
+ */
+
+if (typeof QUnit == 'undefined') {
+  QUnit = require('qunit-cli');
+  QUnit.load();
+
+  kurentoClient = require('..');
+
+  require('./_common');
+  require('./_proxy');
+};
+
+QUnit.module('ServerManager', lifecycle);
+
+QUnit.asyncTest('Server manager', function () {
+  var self = this;
+
+  QUnit.expect(1);
+
+  self.kurento.getServerManager(function (error, server) {
+    if (error) return onerror(error);
+
+    var mediaPipeline_id;
+
+    server.on('ObjectCreated', function (event) {
+      mediaPipeline_id = event.object;
+    })
+
+    setTimeout(function () {
+      self.kurento.create('MediaPipeline', function (error,
+        pipeline) {
+        if (error) return onerror(error);
+
+        QUnit.equal(pipeline.id, mediaPipeline_id, 'ID: ' +
+          pipeline.id);
+
+        QUnit.start();
+      })
+    }, 500)
+  })
+});
