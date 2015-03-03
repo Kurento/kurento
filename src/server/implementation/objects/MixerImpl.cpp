96ba62a28f0c614551632e43b8bacb063ff0eb96
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-03-03 10:07:16 +0100

    MixerImpl: Restore functionality deleted during migration from version 4
    Disclaimer: This patch is not tested is just to port the functionality.
    
    Change-Id: Ie84ea1a92c65d9f0890071a82fe3b44b6ad00e4d

--- src/server/implementation/objects/MixerImpl.cpp
+++ src/server/implementation/objects/MixerImpl.cpp
@@ -1,7 +1,7 @@
 #include <gst/gst.h>
 #include "MediaPipeline.hpp"
 #include "MediaType.hpp"
-#include "HubPort.hpp"
+#include "HubPortImpl.hpp"
 #include <MixerImplFactory.hpp>
 #include "MixerImpl.hpp"
 #include <jsonrpc/JsonSerializer.hpp>
@@ -21,22 +21,73 @@ MixerImpl::MixerImpl (const boost::property_tree::ptree &conf,
                       std::shared_ptr<MediaPipeline> mediaPipeline) : HubImpl (conf,
                             std::dynamic_pointer_cast<MediaObjectImpl> (mediaPipeline), FACTORY_NAME)
 {
-  // FIXME: Implement this
 }
 
 void MixerImpl::connect (std::shared_ptr<MediaType> media,
                          std::shared_ptr<HubPort> source, std::shared_ptr<HubPort> sink)
 {
-  // FIXME: Implement this
-  throw KurentoException (NOT_IMPLEMENTED, "MixerImpl::connect: Not implemented");
+  std::shared_ptr<HubPortImpl> sourcePort =
+    std::dynamic_pointer_cast<HubPortImpl> (source);
+  std::shared_ptr<HubPortImpl> sinkPort =
+    std::dynamic_pointer_cast<HubPortImpl> (sink);
+  std::string  action;
+  bool connected;
+
+  switch (media->getValue() ) {
+  case MediaType::AUDIO:
+    action = "connect-audio";
+    break;
+
+  case MediaType::VIDEO:
+    action = "connect-video";
+    break;
+
+  default:
+    throw KurentoException (UNSUPPORTED_MEDIA_TYPE, "Invalid media type");
+  };
+
+  g_signal_emit_by_name (G_OBJECT (element), action.c_str(),
+                         sourcePort->getHandlerId(),
+                         sinkPort->getHandlerId(),
+                         &connected);
+
+  if (!connected) {
+    throw KurentoException (CONNECT_ERROR, "Can not connect video ports");
+  }
 }
 
 void MixerImpl::disconnect (std::shared_ptr<MediaType> media,
                             std::shared_ptr<HubPort> source, std::shared_ptr<HubPort> sink)
 {
-  // FIXME: Implement this
-  throw KurentoException (NOT_IMPLEMENTED,
-                          "MixerImpl::disconnect: Not implemented");
+  std::shared_ptr<HubPortImpl> sourcePort =
+    std::dynamic_pointer_cast<HubPortImpl> (source);
+  std::shared_ptr<HubPortImpl> sinkPort =
+    std::dynamic_pointer_cast<HubPortImpl> (sink);
+  std::string  action;
+  bool connected;
+
+  switch (media->getValue() ) {
+  case MediaType::AUDIO:
+    action = "disconnect-audio";
+    break;
+
+  case MediaType::VIDEO:
+    throw KurentoException (UNSUPPORTED_MEDIA_TYPE,
+                            "Video disconnection is not implemented yet");
+
+  default:
+    /* Only audio is suppported so far */
+    throw KurentoException (UNSUPPORTED_MEDIA_TYPE, "Invalid media type");
+  };
+
+  g_signal_emit_by_name (G_OBJECT (element), action.c_str(),
+                         sourcePort->getHandlerId(),
+                         sinkPort->getHandlerId(),
+                         &connected);
+
+  if (!connected) {
+    throw KurentoException (CONNECT_ERROR, "Can not connect video ports");
+  }
 }
 
 MediaObjectImpl *
