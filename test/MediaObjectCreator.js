f7eb94209a4f40676d58562114b05ec914e5ff86
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-03-04 15:38:10 +0100

    [Fix] Use definition of MediaObject for unknown constructors
    Change-Id: I4ec11216b310f53fe99b617b7b6d76074c950e2c

---
+++ test/MediaObjectCreator.js
@@ -0,0 +1,40 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+if (typeof QUnit == 'undefined') {
+  QUnit = require('qunit-cli');
+  QUnit.load();
+
+  kurentoClient = require('..');
+
+  require('./_common');
+};
+
+QUnit.module('MediaObjectCreator');
+
+QUnit.test('Get unknown constructor', function (assert) {
+  QUnit.expect(1);
+
+  var MediaObject = require('kurento-client-core').abstracts.MediaObject;
+  var MediaObjectCreator = kurentoClient.MediaObjectCreator;
+
+  function noop() {}
+
+  var mediaObject = MediaObjectCreator(null, noop, noop).createInmediate({
+    type: 'dummyUnknownType'
+  })
+
+  assert.ok(mediaObject instanceof MediaObject)
+});
