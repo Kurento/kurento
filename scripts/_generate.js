9333bf73863078bea3fa71ff69018ee924a5d955
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-04-09 16:08:54 +0200

    Generate internal plugins automatically when using development version
    Change-Id: Idbd4f370c1d12e2d6f4ee7a441c6bf4d24751c50

--- scripts/_generate.js
+++ scripts/_generate.js
@@ -3,15 +3,15 @@ var exists = require('fs').exists
 var npm = require('npm')
 
 exists('lib', function (found) {
-  //  if(!found)
-  npm.load(function (error) {
-    if (error) return console.trace(error)
+  if (!found)
+    npm.load(function (error) {
+      if (error) return console.trace(error)
 
-    npm.commands.install(function (error, data) {
-      if (error) {
-        console.trace(error)
-        process.exit(1)
-      }
+      npm.commands.install(function (error, data) {
+        if (error) {
+          console.trace(error)
+          process.exit(1)
+        }
+      })
     })
-  })
 })
