5dd7b0e91f5a5d454c5fe0c93208d0fcbc03b549
David Fernandez <d.fernandezlop@gmail.com>
2015-04-10 09:46:01 +0200

    [kurento-test] Add AlphaBlending tests
    Change-Id: Ib34ac3e3618c13c14aa6f1484c1c30f1fa65026b

---
+++ kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/alphablending/AlphaBlendingPlayerTest.java
@@ -0,0 +1,109 @@
+package org.kurento.test.functional.alphablending;
+
+import java.awt.Color;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.kurento.client.AlphaBlending;
+import org.kurento.client.HubPort;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.PlayerEndpoint;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.test.base.FunctionalTest;
+import org.kurento.test.config.TestScenario;
+
+/**
+ * 
+ * <strong>Description</strong>: Three synthetic videos are played by four
+ * PlayerEndpoint and mixed by a AlphaBlending. The resulting video is played in
+ * an WebRtcEndpoint.<br/>
+ * <strong>Pipeline</strong>:
+ * <ul>
+ * <li>3xPlayerEndpoint -> AlphaBlending -> WebRtcEndpoint</li>
+ * </ul>
+ * <strong>Pass criteria</strong>:
+ * <ul>
+ * <li>Browser starts before default timeout</li>
+ * <li>Color of the video should be the expected (red, green, blue)</li>
+ * </ul>
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.0.0
+ */
+
+public class AlphaBlendingPlayerTest extends FunctionalTest {
+	private static final int PLAYTIME = 5; // seconds
+
+	public AlphaBlendingPlayerTest(TestScenario testScenario) {
+		super(testScenario);
+	}
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> data() {
+		return TestScenario.localChrome();
+	}
+
+	@Test
+	public void testAlphaBlendingPlayer() throws Exception {
+		// Media Pipeline
+		MediaPipeline mp = kurentoClient.createMediaPipeline();
+
+		PlayerEndpoint playerRed = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/red.webm").build();
+		PlayerEndpoint playerGreen = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/green.webm").build();
+		PlayerEndpoint playerBlue = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/blue.webm").build();
+
+		AlphaBlending alphaBlending = new AlphaBlending.Builder(mp).build();
+		HubPort hubPort1 = new HubPort.Builder(alphaBlending).build();
+		HubPort hubPort2 = new HubPort.Builder(alphaBlending).build();
+		HubPort hubPort3 = new HubPort.Builder(alphaBlending).build();
+		HubPort hubPort4 = new HubPort.Builder(alphaBlending).build();
+		WebRtcEndpoint webRtcEP = new WebRtcEndpoint.Builder(mp).build();
+
+		playerRed.connect(hubPort1);
+		playerGreen.connect(hubPort2);
+		playerBlue.connect(hubPort3);
+
+		hubPort4.connect(webRtcEP);
+
+		alphaBlending.setMaster(hubPort1, 1);
+
+		alphaBlending.setPortProperties(0F, 0F, 8, 0.2F, 0.2F, hubPort2);
+		alphaBlending.setPortProperties(0.4F, 0.4F, 7, 0.2F, 0.2F, hubPort3);
+
+		getBrowser().subscribeEvents("playing");
+
+		playerRed.play();
+		playerGreen.play();
+		playerBlue.play();
+
+		Thread.sleep(2000);
+		Assert.assertTrue("Not received media (timeout waiting playing event)",
+				getBrowser().waitForEvent("playing"));
+
+		Thread.sleep(2000);
+
+		// Assertions
+		Assert.assertTrue("Upper left part of the video must be blue",
+				getBrowser().similarColorAt(Color.GREEN, 0, 0));
+		Assert.assertTrue("Lower right part of the video must be red",
+				getBrowser().similarColorAt(Color.RED, 315, 235));
+		Assert.assertTrue("Center of the video must be blue", getBrowser()
+				.similarColorAt(Color.BLUE, 160, 120));
+
+		// alphaBlending.setMaster(hubPort3, 1);
+		alphaBlending.setPortProperties(0.8F, 0.8F, 7, 0.2F, 0.2F, hubPort3);
+
+		Assert.assertTrue("Lower right part of the video must be blue",
+				getBrowser().similarColorAt(Color.BLUE, 315, 235));
+		Assert.assertTrue("Center of the video must be red", getBrowser()
+				.similarColorAt(Color.RED, 160, 120));
+
+		Thread.sleep(PLAYTIME * 1000);
+	}
+}
---
+++ kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/alphablending/AlphaBlendingWebRtcTest.java
@@ -0,0 +1,143 @@
+package org.kurento.test.functional.alphablending;
+
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.kurento.client.AlphaBlending;
+import org.kurento.client.HubPort;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.test.base.FunctionalTest;
+import org.kurento.test.client.BrowserClient;
+import org.kurento.test.client.BrowserType;
+import org.kurento.test.client.Client;
+import org.kurento.test.client.WebRtcChannel;
+import org.kurento.test.client.WebRtcMode;
+import org.kurento.test.config.BrowserScope;
+import org.kurento.test.config.TestScenario;
+
+/**
+ * 
+ * <strong>Description</strong>: Three synthetic videos are played by three
+ * WebRtcEndpoint and mixed by an AlphaBlending. The resulting video is played
+ * in an WebRtcEndpoint.<br/>
+ * <strong>Pipeline</strong>:
+ * <ul>
+ * <li>3xWebRtcEndpoint -> AlphaBlending -> WebRtcEndpoint</li>
+ * </ul>
+ * <strong>Pass criteria</strong>:
+ * <ul>
+ * <li>Browser starts before default timeout</li>
+ * <li>Color of the video should be the expected (red, green, blue)</li>
+ * </ul>
+ * 
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.0.0
+ */
+
+public class AlphaBlendingWebRtcTest extends FunctionalTest {
+	private static int PLAYTIME = 5;
+	private static final String BROWSER1 = "browser1";
+	private static final String BROWSER2 = "browser2";
+	private static final String BROWSER3 = "browser3";
+	private static final String BROWSER4 = "browser4";
+
+	public AlphaBlendingWebRtcTest(TestScenario testScenario) {
+		super(testScenario);
+	}
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> data() {
+		TestScenario test = new TestScenario();
+
+		test.addBrowser(
+				BROWSER1,
+				new BrowserClient.Builder().browserType(BrowserType.CHROME)
+						.scope(BrowserScope.LOCAL).client(Client.WEBRTC)
+						.video(getPathTestFiles() + "/video/10sec/red.y4m")
+						.build());
+		test.addBrowser(
+				BROWSER2,
+				new BrowserClient.Builder().browserType(BrowserType.CHROME)
+						.scope(BrowserScope.LOCAL).client(Client.WEBRTC)
+						.video(getPathTestFiles() + "/video/10sec/green.y4m")
+						.build());
+		test.addBrowser(
+				BROWSER3,
+				new BrowserClient.Builder().browserType(BrowserType.CHROME)
+						.scope(BrowserScope.LOCAL).client(Client.WEBRTC)
+						.video(getPathTestFiles() + "/video/10sec/blue.y4m")
+						.build());
+		test.addBrowser(BROWSER4,
+				new BrowserClient.Builder().browserType(BrowserType.CHROME)
+						.scope(BrowserScope.LOCAL).client(Client.WEBRTC)
+						.build());
+
+		return Arrays.asList(new Object[][] { { test } });
+	}
+
+	@Test
+	public void testAlphaBlendingWebRtc() throws Exception {
+		// Media Pipeline
+		MediaPipeline mp = kurentoClient.createMediaPipeline();
+		WebRtcEndpoint webRtcEPRed = new WebRtcEndpoint.Builder(mp).build();
+		WebRtcEndpoint webRtcEPGreen = new WebRtcEndpoint.Builder(mp).build();
+		WebRtcEndpoint webRtcEPBlue = new WebRtcEndpoint.Builder(mp).build();
+		WebRtcEndpoint webRtcEPAlphabaBlending = new WebRtcEndpoint.Builder(mp)
+				.build();
+
+		AlphaBlending alphaBlending = new AlphaBlending.Builder(mp).build();
+		HubPort hubPort1 = new HubPort.Builder(alphaBlending).build();
+		HubPort hubPort2 = new HubPort.Builder(alphaBlending).build();
+		HubPort hubPort3 = new HubPort.Builder(alphaBlending).build();
+		HubPort hubPort4 = new HubPort.Builder(alphaBlending).build();
+
+		webRtcEPRed.connect(hubPort1);
+		webRtcEPGreen.connect(hubPort2);
+		webRtcEPBlue.connect(hubPort3);
+		hubPort4.connect(webRtcEPAlphabaBlending);
+
+		alphaBlending.setMaster(hubPort1, 1);
+
+		alphaBlending.setPortProperties(0F, 0F, 8, 0.2F, 0.2F, hubPort2);
+		alphaBlending.setPortProperties(0.4F, 0.4F, 7, 0.2F, 0.2F, hubPort3);
+
+		getBrowser(BROWSER1).subscribeLocalEvents("playing");
+		getBrowser(BROWSER1).initWebRtc(webRtcEPRed,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+
+		getBrowser(BROWSER2).subscribeLocalEvents("playing");
+		getBrowser(BROWSER2).initWebRtc(webRtcEPGreen,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+
+		getBrowser(BROWSER3).subscribeLocalEvents("playing");
+		getBrowser(BROWSER3).initWebRtc(webRtcEPBlue,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+
+		getBrowser(BROWSER4).subscribeEvents("playing");
+		getBrowser(BROWSER4).initWebRtc(webRtcEPAlphabaBlending,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.RCV_ONLY);
+
+		// Assertions
+		Assert.assertTrue("Upper left part of the video must be blue",
+				getBrowser(BROWSER4).similarColorAt(Color.GREEN, 0, 0));
+		Assert.assertTrue("Lower right part of the video must be red",
+				getBrowser(BROWSER4).similarColorAt(Color.RED, 315, 235));
+		Assert.assertTrue("Center of the video must be blue",
+				getBrowser(BROWSER4).similarColorAt(Color.BLUE, 160, 120));
+
+		// alphaBlending.setMaster(hubPort3, 1);
+		alphaBlending.setPortProperties(0.8F, 0.8F, 7, 0.2F, 0.2F, hubPort3);
+
+		Assert.assertTrue("Lower right part of the video must be blue",
+				getBrowser(BROWSER4).similarColorAt(Color.BLUE, 315, 235));
+		Assert.assertTrue("Center of the video must be red",
+				getBrowser(BROWSER4).similarColorAt(Color.RED, 160, 120));
+		Thread.sleep(PLAYTIME * 1000);
+	}
+}
