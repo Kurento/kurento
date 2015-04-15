5457bd93f43936723a7264c21da48ae795824af5
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-04-15 11:52:55 +0200

    Add install_deb_depenedencies.sh
    Change-Id: I9858332cddfb85f742953f7ab9d627cc68c5d78b

---
+++ install_deb_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -s debian/control ] ; then
+  echo "Warning, no debian/control file found"
+  exit 1
+fi
+
+
+if [ $(dpkg -l | grep postpone | wc -l) -lt 1 ]
+then
+  DEBIAN_FRONTEND=noninteractive sudo apt-get install --force-yes -y postpone
+fi
+
+DEBIAN_FRONTEND=noninteractive sudo postpone -d -f apt-get update || exit 1
+
+pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
+DEBIAN_FRONTEND=noninteractive sudo postpone -d -f apt-get install --force-yes -y $pkgs || exit 1
