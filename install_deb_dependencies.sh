1f3028ce628aecf1423d4a7a8df25a0740d59fe1
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-04-21 15:37:46 +0200

    install_deb_dependencies: Do not abort if apt-get update cannot be done
    Change-Id: I3f1d1f950e77d23e14717ec871854f47c7795b2f

--- install_deb_dependencies.sh
+++ install_deb_dependencies.sh
@@ -11,7 +11,7 @@ then
   DEBIAN_FRONTEND=noninteractive sudo apt-get install --force-yes -y postpone
 fi
 
-DEBIAN_FRONTEND=noninteractive sudo postpone -d -f apt-get update || exit 1
+DEBIAN_FRONTEND=noninteractive sudo postpone -d -f apt-get update || echo "Error updating packages"
 
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 DEBIAN_FRONTEND=noninteractive sudo postpone -d -f apt-get install --force-yes -y $pkgs || exit 1
