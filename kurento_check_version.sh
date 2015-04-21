850c1489de2a9763e4ac3eecb9639c879b6c7ba8
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-04-21 17:22:14 +0200

    Use kurento_get_version.sh script in check_version.sh
    Change-Id: I6b6f0b85b3c9a1014b42087f94bdd8239e2562e6

--- kurento_check_version.sh
+++ kurento_check_version.sh
@@ -11,53 +11,26 @@ fi
 
 echo Create tag $CREATE_TAG
 
-if [ -f CMakeLists.txt ]
-then
-  mkdir check_version
-  cd check_version
-  echo "@PROJECT_VERSION@" > version.txt.in
-  echo "@PROJECT_NAME@" > name.txt.in
-  echo 'configure_file(${CMAKE_BINARY_DIR}/version.txt.in version.txt)' >> ../CMakeLists.txt
-  echo 'configure_file(${CMAKE_BINARY_DIR}/name.txt.in name.txt)' >> ../CMakeLists.txt
-  cmake .. -DCALCULATE_VERSION_WITH_GIT=FALSE -DDISABLE_LIBRARIES_GENERATION=TRUE 2>%1 > /dev/null
-  PROJECT_VERSION=`cat version.txt`
-  PROJECT_NAME=`cat name.txt`
-  echo Version: ${PROJECT_VERSION}
-  echo ProjectName: ${PROJECT_NAME}
-  cd ..
-  rm -rf check_version
-  sed -i '$ d' CMakeLists.txt
-  sed -i '$ d' CMakeLists.txt
-
-  # check dev version
-  if [[ ${PROJECT_VERSION} == *-dev ]]; then
-    echo "dev version"
-    exit 0
-  fi
-fi
-
-if [ -f pom.xml ]
-then
-  PROJECT_VERSION=`mvn help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[" | grep -v "Down"`
-  PROJECT_NAME=`mvn help:evaluate -U -Dexpression=project.artifactId 2>/dev/null| grep -v "^\[" | grep -v "Down"`
-
-  echo Version: ${PROJECT_VERSION}
-  echo ProjectName: ${PROJECT_NAME}
-
-  if [[ ${PROJECT_VERSION} == *-SNAPSHOT ]]; then
-    echo "SNAPSHOT version ${PROJECT_VERSION}"
-    exit 0
-  fi
-fi
+PROJECT_VERSION=`kurento_get_version.sh`
 
 if [ "${PROJECT_VERSION}x" = "x" ]; then
-  echo "PROJECT_VERSION not defined, need a CMakeLists.txt or a pom.xml file"
   exit 1
 fi
 
-if [ "${PROJECT_NAME}x" = "x" ]; then
-  echo "PROJECT_NAME not defined"
-  exit 1
+if [[ ${PROJECT_VERSION} == *-SNAPSHOT ]]; then
+  echo "SNAPSHOT version ${PROJECT_VERSION}"
+  exit 0
+fi
+
+if [[ ${PROJECT_VERSION} == *-dev ]]; then
+  echo "dev version"
+  exit 0
+fi
+
+if [[ $(echo $PROJECT_VERSION | grep -o '\.' | wc -l) -gt 2 ]]
+then
+  echo "Found more than two dots, should be a configure.ac dev version"
+  exit 0
 fi
 
 # Check if all submodules are in a tag
@@ -86,12 +59,8 @@ then
   #Create a new tag
   echo "Creating tag"
   tag_name=v${PROJECT_VERSION}
-  #tag_name=${PROJECT_NAME}-${PROJECT_VERSION}
-  #git branch -f master
-  #git checkout master
   if git tag ${tag_name}
   then
     git push --tags
-    #git push origin master || echo Push failed
   fi
 fi
