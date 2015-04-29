b66e8faac059c7f7c08c8bf0085b1e6b2bc28daa
Boni Garcia <bgarcia@gsyc.es>
2015-04-29 16:14:18 +0200

    [kurento-test] Added JUnit rule to allow test retries
    Change-Id: I3ea14d5f1f3909287df0daba68e715d6c5aa25b2

---
+++ kurento-integration-tests/kurento-test/src/main/java/org/kurento/test/config/Retry.java
@@ -0,0 +1,81 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+package org.kurento.test.config;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * JUnit rule to retry test case in case of failure.
+ *
+ * @author Boni Garcia (bgarcia@gsyc.es)
+ * @since 6.0.0
+ */
+public class Retry implements TestRule {
+	private Logger log = LoggerFactory.getLogger(Retry.class);
+
+	private static final String SEPARATOR = "=======================================";
+
+	private int retryCount;
+
+	private int currentRetry = 1;
+
+	public Retry(int retryCount) {
+		this.retryCount = retryCount;
+	}
+
+	public Statement apply(Statement base, Description description) {
+		return statement(base, description);
+	}
+
+	private Statement statement(final Statement base,
+			final Description description) {
+		return new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				Throwable caughtThrowable = null;
+
+				// implement retry logic here
+				for (; currentRetry <= retryCount; currentRetry++) {
+					try {
+						base.evaluate();
+						return;
+					} catch (Throwable t) {
+						caughtThrowable = t;
+						log.error(SEPARATOR);
+						log.error("{}: run {} failed",
+								description.getDisplayName(), currentRetry);
+						log.error(SEPARATOR);
+					}
+				}
+
+				log.error(SEPARATOR);
+				System.err.println(description.getDisplayName()
+						+ ": giving up after " + retryCount + " failures");
+				log.error(SEPARATOR);
+
+				throw caughtThrowable;
+			}
+		};
+	}
+
+	public int getCurrentRetry() {
+		return currentRetry;
+	}
+
+}
