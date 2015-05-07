d2d3a7efdbde8641129dc4302f73a2838c1a80ea
Miguel París Díaz <mparisdiaz@gmail.com>
2015-05-07 18:03:56 +0200

    WebRtcEndpointImpl: remove PEM certificate management
    Change-Id: I0228d2d98d3463e3c51279c5506a0ad389eea58d

--- src/server/implementation/objects/WebRtcEndpointImpl.cpp
+++ src/server/implementation/objects/WebRtcEndpointImpl.cpp
@@ -20,99 +20,7 @@ GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
 namespace kurento
 {
 
-static const std::string CERTTOOL_TEMPLATE = "autoCerttool.tmpl";
-static const std::string CERT_KEY_PEM_FILE = "autoCertkey.pem";
-
 static const uint DEFAULT_STUN_PORT = 3478;
-
-static std::shared_ptr<std::string> pemCertificate;
-std::mutex WebRtcEndpointImpl::certificateMutex;
-
-class TemporalDirectory
-{
-
-public:
-  ~TemporalDirectory() {
-    if (!dir.string ().empty() ) {
-      boost::filesystem::remove_all (dir);
-    }
-  }
-
-  void setDir (boost::filesystem::path &dir) {
-    this->dir = dir;
-  }
-private:
-  boost::filesystem::path dir;
-};
-
-static TemporalDirectory tmpDir;
-
-static void
-create_pem_certificate ()
-{
-  int ret;
-  boost::filesystem::path temporalDirectory = boost::filesystem::unique_path (
-        boost::filesystem::temp_directory_path() / "WebRtcEndpoint_%%%%%%%%" );
-  boost::filesystem::create_directories (temporalDirectory);
-  tmpDir.setDir (temporalDirectory);
-
-  boost::filesystem::path pemFile = temporalDirectory / CERT_KEY_PEM_FILE;
-  std::string pemGenerationCommand =
-    "/bin/sh -c \"certtool --generate-privkey --outfile " + pemFile .string()  +
-    "\"";
-
-  ret = system (pemGenerationCommand.c_str() );
-
-  if (ret == -1) {
-    return;
-  }
-
-  boost::filesystem::path templateFile = temporalDirectory / CERTTOOL_TEMPLATE;
-  std::string certtoolCommand = "/bin/sh -c \"echo 'organization = kurento' > " +
-                                templateFile.string() + " && certtool --generate-self-signed --load-privkey " +
-                                pemFile.string() + " --template " + templateFile.string() +  " >> " +
-                                pemFile.string() + " 2>/dev/null\"";
-
-  ret = system (certtoolCommand.c_str() );
-
-  if (ret == -1) {
-    return;
-  }
-
-  pemCertificate = std::shared_ptr <std::string> (new std::string (
-                     pemFile.string() ) );
-}
-
-std::shared_ptr<std::string>
-WebRtcEndpointImpl::getPemCertificate ()
-{
-  std::unique_lock<std::mutex> lock (certificateMutex);
-
-  if (pemCertificate) {
-    return pemCertificate;
-  }
-
-  try {
-    boost::filesystem::path pem_certificate_file_name (
-      getConfigValue<std::string, WebRtcEndpoint> ("pemCertificate") );
-
-    if (pem_certificate_file_name.is_relative() ) {
-      pem_certificate_file_name = boost::filesystem::path (
-                                    config.get<std::string> ("configPath") ) / pem_certificate_file_name;
-    }
-
-    pemCertificate = std::shared_ptr <std::string> (new std::string (
-                       pem_certificate_file_name.string() ) );
-
-    return pemCertificate;
-  } catch (boost::property_tree::ptree_error &e) {
-
-  }
-
-  create_pem_certificate();
-
-  return pemCertificate;
-}
 
 static const gchar *supported_codecs[] = { "VP8", "opus", "PCMU" };
 
@@ -315,9 +223,6 @@ WebRtcEndpointImpl::WebRtcEndpointImpl (const boost::property_tree::ptree &conf,
   } catch (boost::property_tree::ptree_error &e) {
 
   }
-
-  g_object_set ( G_OBJECT (element), "certificate-pem-file",
-                 getPemCertificate ()->c_str(), NULL);
 }
 
 WebRtcEndpointImpl::~WebRtcEndpointImpl()
@@ -372,7 +277,6 @@ WebRtcEndpointImpl::setStunServerPort (int stunServerPort)
   g_object_set ( G_OBJECT (element), "stun-server-port",
                  stunServerPort, NULL);
 }
-
 
 std::string
 WebRtcEndpointImpl::getTurnUrl ()
