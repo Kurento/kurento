0b7a7fb5dfd7543638e194e7d4d75da8a7a4f935
patxi <patxi.gortazar@gmail.com>
2015-07-09 09:26:17 +0200

    Add kurento_maven_deploy.sh
    Change-Id: I20caf300bac35172fe9de0dfd2e2a291bf6f247f

--- kurento_maven_deploy.sh
+++ kurento_maven_deploy.sh
@@ -1,47 +1,56 @@
 #!/bin/bash -x
 
-echo "##################### EXECUTE: maven-deploy #####################"
+echo "##################### EXECUTE: kurento_maven_deploy.sh #####################"
 
 # Param management
+if [ $# -lt 1 ]
+then
+  echo "Usage: $0 <MAVEN_SETTINGS> [<repository> [<sign artifacts (yes|no)>]]"
+  exit 1
+fi
 
-# Refspec
-[ -n "$1" ] && REFSPEC=$1 || exit 1
 
 # Maven settins
-[ -n "$2" ] && MAVEN_SETTINGS=$2 || exit 1
+[ -n "$1" ] && MAVEN_SETTINGS=$1 || exit 1
 
 # Maven repository
-[ -n "$3" ] && REPO="-DaltDeploymentRepository=$3"
+[ -n "$2" ] && REPOSITORY=$2 || REPOSITORY=$MAVEN_KURENTO_SNAPSHOTS
+
+# Sign artifacts (no, yes). We never sign SNAPSHOT artifacts
+[ -n "$3" ] && SIGN_ARTIFACTS=$3 || SIGN_ARTIFACTS="yes"
 
 # Maven options
-OPTS="-Dmaven.test.skip=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true" 
-[ -n "$4" ] && OPTS="$OPTS $4"
+OPTS="-Dmaven.test.skip=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true"
 
-git checkout $REFSPEC || exit 1
+PROJECT_VERSION=$(kurento_get_version.sh)
+echo "Deploying version $PROJECT_VERSION"
 
-if [ -n "$REPO" ]; then
-	# Deploy to requested repo
-	mvn --settings $MAVEN_SETTINGS clean package javadoc:jar source:jar gpg:sign org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy $OPTS $REPO || exit 1
+if [[ ${PROJECT_VERSION} == *-SNAPSHOT ]]; then
+	echo "Deploying SNAPSHOT version"
+	mvn --settings $MAVEN_SETTINGS clean package org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy $OPTS -DaltSnapshotDeploymentRepository=$REPOSITORY || exit 1
 else
-	# Deploy to all known repos
-	mvn --settings $MAVEN_SETTINGS clean package javadoc:jar source:jar gpg:sign org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -U $OPTS \
-		-DaltReleaseDeploymentRepository=$MAVEN_KURENTO_RELEASES \
-		-DaltSnapshotDeploymentRepository=$MAVEN_KURENTO_SNAPSHOTS || exit 1
-	mvn --settings $MAVEN_SETTINGS clean package javadoc:jar source:jar gpg:sign org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -U $OPTS  \
-		-DaltReleaseDeploymentRepository=$MAVEN_SONATYPE_NEXUS_STAGING \
-		-DaltSnapshotDeploymentRepository=$MAVEN_KURENTO_SNAPSHOTS || exit 1
+	if [[ $SIGN_ARTIFACTS == yes ]]; then
+		echo "Deploying release version signing artifacts"
+		# Deploy signing artifacts
+		mvn --settings $MAVEN_SETTINGS clean package javadoc:jar source:jar gpg:sign org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy $OPTS -DaltReleaseDeploymentRepository=$REPOSITORY || exit 1
+
+		#Verify signed files (if any)
+		SIGNED_FILES=$(find ./target -type f | egrep '\.asc$')
+
+		if [ -z "$SIGNED_FILES" ]; then
+			echo "No signed files found"
+			exit 0
+		fi
+
+		for FILE in $SIGNED_FILES
+		do
+		    SIGNED_FILE=`echo $FILE | sed 's/.asc\+$//'`
+		    gpg --verify $FILE $SIGNED_FILE || exit 1
+		done
+
+	else
+		echo "Deploying release version without signing artifacts"
+		# Deploy without signing artifacts
+		mvn --settings $MAVEN_SETTINGS clean package javadoc:jar source:jar org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -U $OPTS -DaltReleaseDeploymentRepository=$REPOSITORY || exit 1
+	fi
 fi
-
-#Verify signed files (if any)
-SIGNED_FILES=$(find ./target -type f | egrep '\.asc$')
-
-if [ -z "$SIGNED_FILES" ]; then
-	echo "No signed files found"
-	exit 0
-fi
-
-for FILE in $SIGNED_FILES
-do
-    SIGNED_FILE=`echo $FILE | sed 's/.asc\+$//'`
-    gpg --verify $FILE $SIGNED_FILE || exit 1
-done
