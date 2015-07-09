2a16e53be7cfb4794a4261c7dd2e625fe44b6e88
Boni Garcia <bgarcia@gsyc.es>
2015-07-09 17:59:15 +0200

    [kurento-platedetector] Bug-fixes and refactoring
    Change-Id: I8b8b77c87cb9e08ff63c6c154f4cee5b82b09e27

--- kurento-platedetector/server.js
+++ kurento-platedetector/server.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 /*
  * (C) Copyright 2014-2015 Kurento (http://kurento.org/)
  *
@@ -15,175 +14,285 @@
  */
 
 var path = require('path');
-var url  = require('url');
-
+var url = require('url');
+var cookieParser = require('cookie-parser')
+var express = require('express');
+var session = require('express-session')
 var minimist = require('minimist');
+var ws = require('ws');
+var kurento = require('kurento-client');
+kurento.register('kurento-module-platedetector');
 
-var express   = require('express');
-var expressWs = require('express-ws');
-
-var kurentoClient = require('kurento-client')
-var RpcBuilder    = require('kurento-jsonrpc');
-
-const packer = RpcBuilder.packers.JsonRPC;
-
-
-var args = minimist(process.argv.slice(2),
-{
-  default:
-  {
-    as_uri: "http://localhost:8080/",
-    ws_uri: "ws://localhost:8888/kurento"
-  }
+var argv = minimist(process.argv.slice(2), {
+    default: {
+        as_uri: 'http://localhost:8080/',
+        ws_uri: 'ws://localhost:8888/kurento'
+    }
 });
 
-var app = expressWs(express()).app;
+var app = express();
 
-kurentoClient.register('kurento-module-platedetector')
+/*
+ * Management of sessions
+ */
+app.use(cookieParser());
 
+var sessionHandler = session({
+    secret : 'none',
+    rolling : true,
+    resave : true,
+    saveUninitialized : true
+});
+
+app.use(sessionHandler);
+
+/*
+ * Definition of global variables.
+ */
+var sessions = {};
+var candidatesQueue = {};
+var kurentoClient = null;
 
 /*
  * Server startup
  */
-
-var asUrl = url.parse(args.as_uri);
+var asUrl = url.parse(argv.as_uri);
 var port = asUrl.port;
 var server = app.listen(port, function() {
-	console.log('Kurento Tutorial started');
-	console.log('Open ' + url.format(asUrl) + ' with a WebRTC capable browser');
+    console.log('Kurento Tutorial started');
+    console.log('Open ' + url.format(asUrl) + ' with a WebRTC capable browser');
 });
 
+var wss = new ws.Server({
+    server : server,
+    path : '/'
+});
 
 /*
  * Management of WebSocket messages
  */
-app.ws('/', function(ws)
-{
-  var pipeline
-  var webRtcEndpoint
+wss.on('connection', function(ws) {
+    var sessionId = null;
+    var request = ws.upgradeReq;
+    var response = {
+        writeHead : {}
+    };
 
-  var rpcBuilder = new RpcBuilder(packer, ws, function(request)
-  {
-    switch(request.method)
-    {
-      case 'start':
-        start(request)
-      break;
+    sessionHandler(request, response, function(err) {
+        sessionId = request.session.id;
+        console.log('Connection received with sessionId ' + sessionId);
+    });
 
-      case 'candidate':
-        processCandidate(request)
-      break;
+    ws.on('error', function(error) {
+        console.log('Connection ' + sessionId + ' error');
+        stop(sessionId);
+    });
 
-      case 'stop':
-        stop();
-      break;
+    ws.on('close', function() {
+        console.log('Connection ' + sessionId + ' closed');
+        stop(sessionId);
+    });
 
-      default:
-        console.error(request)
-        request.reply('Invalid message')
-    }
-  });
+    ws.on('message', function(_message) {
+        var message = JSON.parse(_message);
+        console.log('Connection ' + sessionId + ' received message ', message);
 
+        switch (message.id) {
+        case 'start':
+            sessionId = request.session.id;
+            start(sessionId, ws, message.sdpOffer, function(error, type, data) {
+                if (error) {
+                    return ws.send(JSON.stringify({
+                        id : 'error',
+                        message : error
+                    }));
+                }
+                switch (type) {
+                case 'sdpAnswer':
+                    ws.send(JSON.stringify({
+                        id : 'startResponse',
+                        sdpAnswer : data
+                    }));
+                    break;
+                case 'plateDetected':
+                    ws.send(JSON.stringify({
+                        id : 'plateDetected',
+                        data : data
+                    }));
+                    break;
+                }
+            });
+            break;
 
-  var candidatesQueue = []
+        case 'stop':
+            stop(sessionId);
+            break;
 
-  function start(request)
-  {
-    var sdpOffer = request.params[0];
+        case 'onIceCandidate':
+            onIceCandidate(sessionId, message.candidate);
+            break;
 
-  	// Check if session is already transmitting
-  	if(pipeline) return request.reply("Close current session before starting a new one or use another browser to open a tutorial.")
-
-    kurentoClient.getSingleton(args.ws_uri, function(error, client) {
-  		if (error) return request.reply(error);
-
-      client.create('MediaPipeline', function(error, _pipeline) {
-  			if (error) return request.reply(error);
-
-        pipeline = _pipeline
-
-        function onError(error)
-        {
-          if(error)
-          {
-            pipeline.release();
-            request.reply(error);
-          }
+        default:
+            ws.send(JSON.stringify({
+                id : 'error',
+                message : 'Invalid message ' + message
+            }));
+            break;
         }
 
-        pipeline.create('WebRtcEndpoint', function(error, _webRtcEndpoint)
-        {
-      		if (error) return onError(error);
-
-          webRtcEndpoint = _webRtcEndpoint
-
-          while(candidatesQueue.length)
-          {
-            var candidate = candidatesQueue.shift()
-
-            webRtcEndpoint.addIceCandidate(candidate)
-          }
-
-          webRtcEndpoint.on('OnIceCandidate', function(event) {
-            rpcBuilder.encode('candidate', [event.candidate])
-          });
-
-          webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer)
-          {
-            if (error) return onError(error);
-
-            request.reply(null, sdpAnswer);
-          });
-          webRtcEndpoint.gatherCandidates(onError);
-
-          pipeline.create('PlateDetectorFilter', function(error, plateDetectorFilter)
-          {
-        		if (error) return onError(error);
-
-            plateDetectorFilter.on('PlateDetected', function(data){
-              rpcBuilder.encode('PlateDetected', data);
-            });
-
-            client.connect(webRtcEndpoint, plateDetectorFilter, webRtcEndpoint,
-              onError);
-        	});
-      	});
-  		});
-  	});
-  }
-
-  function processCandidate(request)
-  {
-    var candidate = request.params[0];
-
-    candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
-
-    if(webRtcEndpoint)
-      webRtcEndpoint.addIceCandidate(candidate)
-    else
-      candidatesQueue.push(candidate)
-  }
-
-  function stop()
-  {
-  	if(pipeline)
-    {
-  		pipeline.release();
-  		pipeline = null;
-  	}
-  }
-
-
-	ws.on('error', function(error) {
-		console.log('Connection error');
-		stop();
-	});
-
-	ws.on('close', function() {
-		console.log('Connection closed');
-		stop();
-	});
+    });
 });
 
+/*
+ * Definition of functions
+ */
+
+// Recover kurentoClient for the first time.
+function getKurentoClient(callback) {
+    if (kurentoClient !== null) {
+        return callback(null, kurentoClient);
+    }
+
+    kurento(argv.ws_uri, function(error, _kurentoClient) {
+        if (error) {
+            console.log("Could not find media server at address " + argv.ws_uri);
+            return callback("Could not find media server at address" + argv.ws_uri
+                    + ". Exiting with error " + error);
+        }
+
+        kurentoClient = _kurentoClient;
+        callback(null, kurentoClient);
+    });
+}
+
+function start(sessionId, ws, sdpOffer, callback) {
+    if (!sessionId) {
+        return callback('Cannot use undefined sessionId');
+    }
+
+    getKurentoClient(function(error, kurentoClient) {
+        if (error) {
+            return callback(error);
+        }
+
+        kurentoClient.create('MediaPipeline', function(error, pipeline) {
+            if (error) {
+                return callback(error);
+            }
+
+            createMediaElements(pipeline, ws, function(error, webRtcEndpoint, filter) {
+                if (error) {
+                    pipeline.release();
+                    return callback(error);
+                }
+
+                if (candidatesQueue[sessionId]) {
+                    while(candidatesQueue[sessionId].length) {
+                        var candidate = candidatesQueue[sessionId].shift();
+                        webRtcEndpoint.addIceCandidate(candidate);
+                    }
+                }
+
+                connectMediaElements(webRtcEndpoint, filter, function(error) {
+                    if (error) {
+                        pipeline.release();
+                        return callback(error);
+                    }
+
+                    webRtcEndpoint.on('OnIceCandidate', function(event) {
+                        var candidate = kurento.register.complexTypes.IceCandidate(event.candidate);
+                        ws.send(JSON.stringify({
+                            id : 'iceCandidate',
+                            candidate : candidate
+                        }));
+                    });
+
+                    filter.on('PlateDetected', function (data){
+                        return callback(null, 'plateDetected', data);
+                    });
+
+                    webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer) {
+                        if (error) {
+                            pipeline.release();
+                            return callback(error);
+                        }
+
+                        sessions[sessionId] = {
+                            'pipeline' : pipeline,
+                            'webRtcEndpoint' : webRtcEndpoint
+                        }
+                        return callback(null, 'sdpAnswer', sdpAnswer);
+                    });
+
+                    webRtcEndpoint.gatherCandidates(function(error) {
+                        if (error) {
+                            return callback(error);
+                        }
+                    });
+                });
+            });
+        });
+    });
+}
+
+function createMediaElements(pipeline, ws, callback) {
+    pipeline.create('WebRtcEndpoint', function(error, webRtcEndpoint) {
+        if (error) {
+            return callback(error);
+        }
+
+        pipeline.create('PlateDetectorFilter', function(error, filter) {
+            if (error) {
+                return callback(error);
+            }
+
+            return callback(null, webRtcEndpoint, filter);
+        });
+    });
+}
+
+function connectMediaElements(webRtcEndpoint, filter, callback) {
+    webRtcEndpoint.connect(filter, function(error) {
+        if (error) {
+            return callback(error);
+        }
+
+        filter.connect(webRtcEndpoint, function(error) {
+            if (error) {
+                 return callback(error);
+            }
+
+            return callback(null);
+        });
+    });
+}
+
+function stop(sessionId) {
+    if (sessions[sessionId]) {
+        var pipeline = sessions[sessionId].pipeline;
+        console.info('Releasing pipeline');
+        pipeline.release();
+
+        delete sessions[sessionId];
+        delete candidatesQueue[sessionId];
+    }
+}
+
+function onIceCandidate(sessionId, _candidate) {
+    var candidate = kurento.register.complexTypes.IceCandidate(_candidate);
+
+    if (sessions[sessionId]) {
+        console.info('Sending candidate');
+        var webRtcEndpoint = sessions[sessionId].webRtcEndpoint;
+        webRtcEndpoint.addIceCandidate(candidate);
+    }
+    else {
+        console.info('Queueing candidate');
+        if (!candidatesQueue[sessionId]) {
+            candidatesQueue[sessionId] = [];
+        }
+        candidatesQueue[sessionId].push(candidate);
+    }
+}
 
 app.use(express.static(path.join(__dirname, 'static')));
--- kurento-platedetector/static/js/index.js
+++ kurento-platedetector/static/js/index.js
@@ -13,158 +13,181 @@
  *
  */
 
+var ws = new WebSocket('ws://' + location.host);
+var videoInput;
+var videoOutput;
+var webRtcPeer;
+var state = null;
+
 const I_CAN_START = 0;
 const I_CAN_STOP = 1;
 const I_AM_STARTING = 2;
 
 window.onload = function() {
-  console = new Console();
+	console = new Console();
+	console.log('Page loaded ...');
+	videoInput = document.getElementById('videoInput');
+	videoOutput = document.getElementById('videoOutput');
+	setState(I_CAN_START);
+}
 
-  console.log("Page loaded...");
+window.onbeforeunload = function() {
+	ws.close();
+}
 
-  var webRtcPeer;
+ws.onmessage = function(message) {
+	var parsedMessage = JSON.parse(message.data);
+	console.info('Received message: ' + message.data);
 
-  var videoInput = document.getElementById('videoInput');
-  var videoOutput = document.getElementById('videoOutput');
+	switch (parsedMessage.id) {
+	case 'startResponse':
+		startResponse(parsedMessage);
+		break;
+	case 'error':
+		if (state == I_AM_STARTING) {
+			setState(I_CAN_START);
+		}
+		onError('Error message from server: ' + parsedMessage.message);
+		break;
+	case 'plateDetected':
+		plateDetected(parsedMessage);
+		break;
+	case 'iceCandidate':
+		webRtcPeer.addIceCandidate(parsedMessage.candidate)
+		break;
+	default:
+		if (state == I_AM_STARTING) {
+			setState(I_CAN_START);
+		}
+		onError('Unrecognized message', parsedMessage);
+	}
+}
 
-  var btnStart = document.getElementById('btnStart');
-  var btnStop = document.getElementById('btnStop');
+function start() {
+	console.log('Starting video call ...')
 
-	function onRequest(request)
-  {
-    switch(request.method)
-    {
-      case 'candidate':
-        webRtcPeer.addIceCandidate(request.params[0])
-      break;
+	// Disable start button
+	setState(I_AM_STARTING);
+	showSpinner(videoInput, videoOutput);
 
-		  case 'PlateDetected':
-		    plateDetected(request.params);
-	    break;
+	console.log('Creating WebRtcPeer and generating local sdp offer ...');
 
-      default:
-        console.error(request)
-    }
-  }
-
-	btnStart.addEventListener('click', function()
-  {
-    console.log("Starting video call...")
-
-    const packer = RpcBuilder.packers.JsonRPC;
-
-    rpcBuilder = new RpcBuilder(packer, new WebSocket('ws:'+location.host),
-      onRequest);
-
-    // Disable start button
-    setState(I_AM_STARTING);
-    showSpinner(videoInput, videoOutput);
-
-    console.log("Creating WebRtcPeer and generating local sdp offer...");
-
-    var options =
-    {
+    var options = {
       localVideo: videoInput,
-      remoteVideo: videoOutput
+      remoteVideo: videoOutput,
+      onicecandidate : onIceCandidate
     }
 
-    webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function(error)
-    {
-      if(error) return onError(error)
-
-      this.generateOffer(onOffer)
+    webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerSendrecv(options, function(error) {
+        if(error) return onError(error);
+        this.generateOffer(onOffer);
     });
-
-    webRtcPeer.on('icecandidate', function(candidate) {
-      rpcBuilder.encode('candidate', [candidate])
-    });
-  })
-
-	btnStop.addEventListener('click', function(){
-    console.log("Stopping video call...");
-    setState(I_CAN_START);
-
-    if (webRtcPeer) {
-      webRtcPeer.dispose();
-      webRtcPeer = null;
-
-      rpcBuilder.encode('stop');
-    }
-    hideSpinner(videoInput, videoOutput);
-  })
-
-	function onOffer(error, offerSdp)
-  {
-    if(error) return onError(error)
-
-    console.info('Invoking SDP offer callback function ' + location.host);
-
-    rpcBuilder.encode('start', [offerSdp], processAnswer);
-  }
-
-	function processAnswer(error, sdpAnswer) {
-    if(error) return onError(error)
-
-    setState(I_CAN_STOP);
-
-    console.log("SDP answer received from server. Processing...");
-    webRtcPeer.processAnswer(sdpAnswer);
-  }
-
-  setState(I_CAN_START);
 }
 
+function onIceCandidate(candidate) {
+	   console.log('Local candidate' + JSON.stringify(candidate));
 
-function plateDetected(data) {
-  console.log("License plate detected " + data.plate);
+	   var message = {
+	      id : 'onIceCandidate',
+	      candidate : candidate
+	   };
+	   sendMessage(message);
 }
 
+function plateDetected(message) {
+	console.log("License plate detected " + message.data.plate);
+}
+
+function onOffer(error, offerSdp) {
+	if(error) return onError(error);
+
+	console.info('Invoking SDP offer callback function ' + location.host);
+	var message = {
+		id : 'start',
+		sdpOffer : offerSdp
+	}
+	sendMessage(message);
+}
 
 function onError(error) {
-  if(error) console.error(error);
+	console.error(error);
+}
+
+function startResponse(message) {
+	setState(I_CAN_STOP);
+	console.log('SDP answer received from server. Processing ...');
+	webRtcPeer.processAnswer(message.sdpAnswer);
+}
+
+function stop() {
+	console.log('Stopping video call ...');
+	setState(I_CAN_START);
+	if (webRtcPeer) {
+		webRtcPeer.dispose();
+		webRtcPeer = null;
+
+		var message = {
+			id : 'stop'
+		}
+		sendMessage(message);
+	}
+	hideSpinner(videoInput, videoOutput);
 }
 
 function setState(nextState) {
-  switch (nextState) {
-    case I_CAN_START:
-      $('#start').attr('disabled', false);
-      $('#stop').attr('disabled', true);
-    break;
+	switch (nextState) {
+	case I_CAN_START:
+		$('#start').attr('disabled', false);
+		$('#start').attr('onclick', 'start()');
+		$('#stop').attr('disabled', true);
+		$('#stop').removeAttr('onclick');
+		break;
 
-    case I_CAN_STOP:
-      $('#start').attr('disabled', true);
-      $('#stop').attr('disabled', false);
-    break;
+	case I_CAN_STOP:
+		$('#start').attr('disabled', true);
+		$('#stop').attr('disabled', false);
+		$('#stop').attr('onclick', 'stop()');
+		break;
 
-    case I_AM_STARTING:
-      $('#start').attr('disabled', true);
-      $('#stop').attr('disabled', true);
-    break;
+	case I_AM_STARTING:
+		$('#start').attr('disabled', true);
+		$('#start').removeAttr('onclick');
+		$('#stop').attr('disabled', true);
+		$('#stop').removeAttr('onclick');
+		break;
 
-    default:
-      onError("Unknown state " + nextState);
-  }
+	default:
+		onError('Unknown state ' + nextState);
+		return;
+	}
+	state = nextState;
+}
+
+function sendMessage(message) {
+	var jsonMessage = JSON.stringify(message);
+	console.log('Senging message: ' + jsonMessage);
+	ws.send(jsonMessage);
 }
 
 function showSpinner() {
-  for (var i = 0; i < arguments.length; i++) {
-    arguments[i].poster = './img/transparent-1px.png';
-    arguments[i].style.background = "center transparent url('./img/spinner.gif') no-repeat";
-  }
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].poster = './img/transparent-1px.png';
+		arguments[i].style.background = 'center transparent url("./img/spinner.gif") no-repeat';
+	}
 }
 
 function hideSpinner() {
-  for (var i = 0; i < arguments.length; i++) {
-    arguments[i].src = '';
-    arguments[i].poster = './img/webrtc.png';
-    arguments[i].style.background = '';
-  }
+	for (var i = 0; i < arguments.length; i++) {
+		arguments[i].src = '';
+		arguments[i].poster = './img/webrtc.png';
+		arguments[i].style.background = '';
+	}
 }
 
 /**
  * Lightbox utility (to display media pipeline image in a modal dialog)
  */
 $(document).delegate('*[data-toggle="lightbox"]', 'click', function(event) {
-  event.preventDefault();
-  $(this).ekkoLightbox();
+	event.preventDefault();
+	$(this).ekkoLightbox();
 });
