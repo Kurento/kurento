5eb9580e1fc2bd64d4feebee09937bb94ebe10c0
patxi <patxi.gortazar@gmail.com>
2015-07-14 15:15:02 +0200

    Fix maven repositories in kurento_generate_java_module.sh
    Change-Id: I56bf7b2a3f6a776ed5ec638eeed5a53d8abfa879

--- kurento_generate_java_module.sh
+++ kurento_generate_java_module.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -n $1 ] && FULL_RELEASE=$1 || FULL_RELEASE=0
+
 rm -rf build
 mkdir build && cd build && cmake .. -DGENERATE_JAVA_CLIENT_PROJECT=TRUE -DDISABLE_LIBRARIES_GENERATION=TRUE
 
@@ -16,10 +18,10 @@ OPTS="$OPTS org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -Dmaven.test
 echo $OPTS
 
 if [[ ${PROJECT_VERSION} != *-SNAPSHOT ]]; then
-  echo "Deploying release version to ${KURENTO_RELEASE_REPOSITORY} and ${SONATYPE_NEXUS_STAGING_REPOSITORY}"
-  mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${KURENTO_RELEASE_REPOSITORY} || exit 1
-	mvn --settings ${MAVEN_SETTINGS} deploy $OPTS -DaltDeploymentRepository=${SONATYPE_NEXUS_STAGING_REPOSITORY} || exit 1
+  echo "Deploying release version to ${MAVEN_KURENTO_RELEASES} and ${MAVEN_SONATYPE_NEXUS_STAGING}"
+  mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_RELEASES} || exit 1
+	mvn --settings ${MAVEN_SETTINGS} deploy $OPTS -DaltDeploymentRepository=${MAVEN_SONATYPE_NEXUS_STAGING} || exit 1
 else
-  echo "Deploying snapshot version to ${KURENTO_SNAPSHOTS_REPOSITORY}"
-  mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${KURENTO_SNAPSHOTS_REPOSITORY} -DaltSnapshotDeploymentRepository=${KURENTO_SNAPSHOTS_REPOSITORY} || exit 1
+  echo "Deploying snapshot version to ${MAVEN_KURENTO_SNAPSHOTS}"
+  mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} -DaltSnapshotDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} || exit 1
 fi
