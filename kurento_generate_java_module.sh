91ee6663a21f39bfc34c4b237027b4fd4b37b558
patxi <patxi.gortazar@gmail.com>
2015-07-15 11:13:58 +0200

    Only release to maven central when module is public
    Change-Id: I6940722c61f8ad0d25ddee46588aacab7a286e65

--- kurento_generate_java_module.sh
+++ kurento_generate_java_module.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 [ -n $1 ] && FULL_RELEASE=$1 || FULL_RELEASE=0
+[ -n $2 ] && PUBLIC=$2 || PUBLIC=no
 
 rm -rf build
 mkdir build && cd build && cmake .. -DGENERATE_JAVA_CLIENT_PROJECT=TRUE -DDISABLE_LIBRARIES_GENERATION=TRUE
@@ -20,7 +21,7 @@ echo $OPTS
 if [[ ${PROJECT_VERSION} != *-SNAPSHOT ]]; then
   echo "Deploying release version to ${MAVEN_KURENTO_RELEASES} and ${MAVEN_SONATYPE_NEXUS_STAGING}"
   mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_RELEASES} || exit 1
-	mvn --settings ${MAVEN_SETTINGS} deploy $OPTS -DaltDeploymentRepository=${MAVEN_SONATYPE_NEXUS_STAGING} || exit 1
+  [ "$PUBLIC" == "yes" ] && mvn --settings ${MAVEN_SETTINGS} deploy $OPTS -DaltDeploymentRepository=${MAVEN_SONATYPE_NEXUS_STAGING} || exit 1
 else
   echo "Deploying snapshot version to ${MAVEN_KURENTO_SNAPSHOTS}"
   mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} -DaltSnapshotDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} || exit 1
