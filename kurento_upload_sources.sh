91d15e16e439c37ad05b37bb7acd2e76de8c8335
patxi <patxi.gortazar@gmail.com>
2015-07-15 13:32:11 +0200

    Allow different servers for origin and target ubuntu repos in kurento_upload_sources.sh
    Change-Id: I3df9303ae617783dcefb245c84534afcb039c69d

--- kurento_upload_sources.sh
+++ kurento_upload_sources.sh
@@ -10,7 +10,7 @@ exec 3>&1 >/dev/tty || exec 3>&1 >./upload_sources_logs
 
 SOURCE="Source: $1"
 ORIG_DIST=$2
-DEST_DIST=$3
+TARGET_DIST=$3
 
 if [ "${ID_RSA_FILE}x" == "x" ]
 then
@@ -24,9 +24,15 @@ then
   exit 1
 fi
 
-if [ "${REPREPRO_URL}x" == "x" ]
+if [ "${ORIG_REPREPRO_URL}x" == "x" ]
 then
-  echo "You need to specify environment variable REPREPRO_URL with the hostname of your repository. Omit the http:// part, just hostname."
+  echo "You need to specify environment variable ORIG_REPREPRO_URL with the address of your repository"
+  exit 1
+fi
+
+if [ "${TARGET_REPREPRO_URL}x" == "x" ]
+then
+  echo "You need to specify environment variable TARGET_REPREPRO_URL with the address of your repository"
   exit 1
 fi
 
@@ -41,16 +47,16 @@ KEY=$ID_RSA_FILE
 mkdir tmp
 cd tmp
 
-for file in $(curl -s http://${REPREPRO_URL}/dists/${ORIG_DIST}/${COMPONENT}/binary-amd64/Packages | awk -v RS='' -v p=$SOURCE '$0 ~ p'  | grep Filename | cut -d':' -f2)
+for file in $(curl -s ${ORIG_REPREPRO_URL}/dists/${ORIG_DIST}/${COMPONENT}/binary-amd64/Packages | awk -v RS='' -v p=$SOURCE '$0 ~ p'  | grep Filename | cut -d':' -f2)
 do
   wget ${REPREPRO_URL}/$file
 done
 
-for file in $(curl -s http://${REPREPRO_URL}/dists/${ORIG_DIST}/${COMPONENT}/binary-amd64/Packages | awk -v RS='' -v p=$SOURCE '$0 ~ p'  | grep Filename | cut -d':' -f2)
+for file in $(curl -s ${ORIG_REPREPRO_URL}/dists/${ORIG_DIST}/${COMPONENT}/binary-amd64/Packages | awk -v RS='' -v p=$SOURCE '$0 ~ p'  | grep Filename | cut -d':' -f2)
 do
   PACKAGE=$(basename $file)
-  echo "Uploading package $PACKAGE to ${REPREPRO_URL}/dists/$DEST_DIST/$COMPONENT"
-  curl --insecure --key $KEY --cert $CERT -X POST https://${REPREPRO_URL}/upload?dist=$DEST_DIST\&comp=$COMPONENT --data-binary @$PACKAGE || exit 1
+  echo "Uploading package $PACKAGE to ${TARGET_REPREPRO_URL}/dists/$DEST_DIST/$COMPONENT"
+  curl --insecure --key $KEY --cert $CERT -X POST ${TARGET_REPREPRO_URL}/upload?dist=$TARGET_DIST\&comp=$COMPONENT --data-binary @$PACKAGE || exit 1
 done
 
 cd ..
