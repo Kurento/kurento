ac0aff88c837c3c0a386cd1dc0aa6e2f3fd74201
Miguel París Díaz <mparisdiaz@gmail.com>
2015-09-01 13:18:58 +0200

    kmswebrtcsctpconnection: remove set_certificate_pem_file impl
    Change-Id: Ie22e25a479bde3e8464514c47589126985d3a5a1

--- src/gst-plugins/webrtcendpoint/kmswebrtcsctpconnection.c
+++ src/gst-plugins/webrtcendpoint/kmswebrtcsctpconnection.c
@@ -54,13 +54,6 @@ G_DEFINE_TYPE_WITH_CODE (KmsWebRtcSctpConnection, kms_webrtc_sctp_connection,
     G_IMPLEMENT_INTERFACE (KMS_TYPE_I_RTP_CONNECTION,
         kms_webrtc_rtp_connection_interface_init));
 
-static void
-kms_webrtc_sctp_connection_set_certificate_pem_file (KmsWebRtcBaseConnection
-    * base_conn, const gchar * pem)
-{
-  GST_WARNING_OBJECT (base_conn, "Deprectated (using erdtls)");
-}
-
 static gchar *
 kms_webrtc_sctp_connection_get_certificate_pem (KmsWebRtcBaseConnection *
     base_conn)
@@ -271,8 +264,6 @@ kms_webrtc_sctp_connection_class_init (KmsWebRtcSctpConnectionClass * klass)
   gobject_class->get_property = kms_webrtc_sctp_connection_get_property;
 
   base_conn_class = KMS_WEBRTC_BASE_CONNECTION_CLASS (klass);
-  base_conn_class->set_certificate_pem_file =
-      kms_webrtc_sctp_connection_set_certificate_pem_file;
   base_conn_class->get_certificate_pem =
       kms_webrtc_sctp_connection_get_certificate_pem;
 
