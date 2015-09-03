a78d04f5a84264b2df036839325dacb721aab634
Boni Garcia <bgarcia@gsyc.es>
2015-09-03 14:12:17 +0200

    Added index.html links to fixlinks.sh script
    Change-Id: I4d8b4974a752c6d37d8842801e7958207271a173

--- fixlinks.sh
+++ fixlinks.sh
@@ -24,6 +24,17 @@ changelink "#kurento-java-client-javadoc" "langdoc/javadoc/index.html" "./build/
 changelink "#kurento-javascript-client-jsdoc" "langdoc/jsdoc/kurento-client-js/index.html" "./build/html/mastering_kurento.html"
 changelink "#kurento-javascript-utils-jsdoc" "langdoc/jsdoc/kurento-utils-js/index.html" "./build/html/mastering_kurento.html"
 
+# ./build/html/index.html
+changelink "mastering_kurento.html#kurento-architecture" "mastering/kurento_architecture.html" "./build/html/index.html"
+changelink "mastering_kurento.html#kurento-api-reference" "mastering/kurento_API.html" "./build/html/index.html"
+changelink "mastering_kurento.html#kurento-protocol" "mastering/kurento_protocol.html" "./build/html/index.html"
+changelink "mastering_kurento.html#advanced-installation-guide" "mastering/advanced_installation_guide.html" "./build/html/index.html"
+changelink "mastering_kurento.html#working-with-nightly-builds" "mastering/kurento_development.html" "./build/html/index.html"
+changelink "mastering_kurento.html#kurento-modules" "mastering/kurento_modules.html" "./build/html/index.html"
+changelink "mastering_kurento.html#kurento-java-client-javadoc" "langdoc/javadoc/index.html" "./build/html/index.html"
+changelink "mastering_kurento.html#kurento-javascript-client-jsdoc" "langdoc/jsdoc/kurento-client-js/index.html" "./build/html/index.html"
+changelink "mastering_kurento.html#kurento-javascript-utils-jsdoc" "langdoc/jsdoc/kurento-utils-js/index.html" "./build/html/index.html"
+
 # ./build/html/mastering/*.html
 files=(kurento_architecture kurento_API kurento_protocol advanced_installation_guide kurento_development kurento_modules develop_kurento_modules)
 for i in ${files[@]}
