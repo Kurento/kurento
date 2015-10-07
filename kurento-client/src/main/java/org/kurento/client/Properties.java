c48a08f7abf1cceb5b30835a69f571aa7a6ca885
mgallego <micael.gallego@gmail.com>
2015-10-07 18:09:30 +0200

    [bug-fix] Properties bugs (values initialization and checks)
    Change-Id: Ic3df3b8ed2d681ea874405a623ecc8387b8af759

--- kurento-client/src/main/java/org/kurento/client/Properties.java
+++ kurento-client/src/main/java/org/kurento/client/Properties.java
@@ -1,10 +1,11 @@
 package org.kurento.client;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Properties {
 
-	private Map<String, Object> values;
+	private Map<String, Object> values = new HashMap<>();
 
 	public static Properties of(Object... params) {
 
@@ -16,7 +17,7 @@ public class Properties {
 
 		Properties p = new Properties();
 
-		for (int i = 0; i < params.length; i++) {
+		for (int i = 0; i < params.length; i += 2) {
 			if (!(params[i] instanceof String)) {
 				throw new IllegalArgumentException(
 						"Property key should be an String value. Parameter " + i
