e79fb09e4ab65de0a55aa8f777d5daba3528be55
patxi <patxi.gortazar@gmail.com>
2015-10-08 12:30:40 +0200

    capability_test.sh: refactor to allow generic MAVEN_OPTS passed in
    Change-Id: Ic3d65447630c1ae9d2d90c0b6b276ff4b1ae0b2a

--- kurento_capability_test.sh
+++ kurento_capability_test.sh
@@ -3,25 +3,13 @@
 echo "##################### EXECUTE: capability-test #####################"
 # Parameter management
 
-if [ $# -lt 2 ]; then
-  echo "Usage: $0 <TEST_FILTER> <GROUP_FILTER> <PROJECT_PATH> [<KMS_AUTOSTART> <SERVER_PORT> <HTTP_PORT>]"
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 <PROJECT_PATH>"
   exit 1
 fi
 
 # TEST_FILTER
-[ -n "$1" ] && TEST_FILTER="$1" || exit 1
-
-[ -n "$2" ] && GROUP_FILTER="$2" || exit 1
-
-[ -n "$3" ] && PROJECT_PATH="$3" || exit 1
-
-[ -n "$4" ] && KMS_AUTOSTART="$4" || KMS_AUTOSTART="test"
-
-# SERVER_PORT
-[ -n "$5" ] && SERVER_PORT="$5"
-
-# HTTP_PORT
-[ -n "$6" ] && HTTP_PORT="$6"
+[ -n "$1" ] && PROJECT_PATH="$1" || exit 1
 
 echo "Checking KMS_PORT_8888_TCP_ADDR env variable: $KMS_PORT_8888_TCP_ADDR"
 [ -n "$KMS_PORT_8888_TCP_ADDR" ] || KMS_PORT_8888_TCP_ADDR="127.0.0.1"
@@ -34,21 +22,10 @@ echo "Checking KMS_HTTP_PORT env variable: $KMS_HTTP_PORT"
 [ -n "$USE_FFMPEG" ] || USE_FFMPEG="no"
 echo "Checking USE_FFMPEG env variable: $USE_FFMPEG"
 
-# Abort if port is not available
-if [ -n "$SERVER_PORT" ]; then
-  echo "Checking port $SERVER_PORT"
-  netstat -tauen | grep $SERVER_PORT && exit 1
-fi
-if [ -n "$HTTP_PORT" ]; then
-  echo "Checking port $HTTP_PORT"
-  netstat -tauen | grep $HTTP_PORT && exit 1
-fi
 if [ ! -n "$KMS_PORT_8888_TCP_ADDR" ]; then
   echo "Checking port $KMS_PORT_8888_TCP_PORT"
   netstat -tauen | grep $KMS_PORT_8888_TCP_PORT && exit 1
 fi
-echo "Checking port $KMS_HTTP_PORT"
-netstat -tauen | grep $KMS_HTTP_PORT && exit 1
 
 #Restart xvfb to avoid problems with corrupted xvfb instances
 sudo service xvfb stop
@@ -69,22 +46,9 @@ if [ "$USE_FFMPEG" == "yes" ] ; then
 fi
 
 mavenOpts="-U -am -pl $PROJECT_PATH"
-mavenOpts="$mavenOpts -Dgroups=$GROUP_FILTER"
-mavenOpts="$mavenOpts -Dtest=$TEST_FILTER"
 mavenOpts="$mavenOpts -DfailIfNoTests=false"
-mavenOpts="$mavenOpts -Dkms.autostart=$KMS_AUTOSTART"
-mavenOpts="$mavenOpts -Dkms.ws.uri=ws://$KMS_PORT_8888_TCP_ADDR:$KMS_PORT_8888_TCP_PORT/kurento"
-mavenOpts="$mavenOpts -Dkms.http.port=$KMS_HTTP_PORT"
-if [ -n "$SERVER_PORT" ]; then
-  mavenOpts="$mavenOpts -Dserver.port=$SERVER_PORT"
-fi
-if [ -n "$HTTP_PORT" ]; then
-  mavenOpts="$mavenOpts -Dhttp.port=$HTTP_PORT"
-fi
-mavenOpts="$mavenOpts -Dkms.command=/usr/bin/kurento-media-server"
-
-mavenOpts="$mavenOpts -Dkurento.test.files=/var/lib/jenkins/test-files"
-mavenOpts="$mavenOpts -Dkurento.workspace=${WORKSPACE}"
+#mavenOpts="$mavenOpts -Dkms.ws.uri=ws://$KMS_PORT_8888_TCP_ADDR:$KMS_PORT_8888_TCP_PORT/kurento"
+mavenOpts="$mavenOpts -Dkurento.workspace=$WORKSPACE"
 mavenOpts="$mavenOpts -Dproject.path=$WORKSPACE/$PROJECT_PATH"
 
 mvn --settings $MAVEN_SETTINGS clean verify $mavenOpts $MAVEN_OPTS
