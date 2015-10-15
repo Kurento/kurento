fc46e4b547f177333ad3d3396d528d08bb74febe
Santiago Carot-Nemesio <sancane@gmail.com>
2015-10-15 13:12:06 +0200

    kmssrtpconnection: Add a new class to manage srtp
    Change-Id: Iacbcb639fc3609dc4e0365b01b2de9d0bdf64605

---
+++ src/gst-plugins/rtpendpoint/kmssrtpconnection.c
@@ -0,0 +1,532 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#include "kmssrtpconnection.h"
+#include "kmssocketutils.h"
+#include <string.h>
+
+#define GST_CAT_DEFAULT kmsrtpconnection
+GST_DEBUG_CATEGORY_STATIC (GST_CAT_DEFAULT);
+
+#define MAX_RETRIES 4
+
+#define GST_DEFAULT_NAME "kmsrtpconnection"
+#define kms_srtp_connection_parent_class parent_class
+
+#define KMS_SRTP_CONNECTION_GET_PRIVATE(obj) (  \
+  G_TYPE_INSTANCE_GET_PRIVATE (                 \
+    (obj),                                      \
+    KMS_TYPE_SRTP_CONNECTION,                   \
+    KmsSrtpConnectionPrivate                    \
+  )                                             \
+)
+
+enum
+{
+  PROP_0,
+  PROP_ADDED,
+  PROP_CONNECTED,
+  PROP_IS_CLIENT,
+  PROP_MIN_PORT,
+  PROP_MAX_PORT
+};
+
+struct _KmsSrtpConnectionPrivate
+{
+  GRecMutex mutex;
+
+  GSocket *rtp_socket;
+  GstElement *rtp_udpsink;
+  GstElement *rtp_udpsrc;
+
+  GSocket *rtcp_socket;
+  GstElement *rtcp_udpsink;
+  GstElement *rtcp_udpsrc;
+
+  GstElement *srtpenc;
+  GstElement *srtpdec;
+
+  gboolean added;
+  gboolean connected;
+  gboolean is_client;
+
+  gchar *r_key;
+  guint r_auth;
+  guint r_cipher;
+  gboolean r_updated;
+  gboolean r_key_set;
+};
+
+#define KMS_SRTP_CONNECTION_LOCK(conn) \
+  (g_rec_mutex_lock (&KMS_SRTP_CONNECTION ((conn))->priv->mutex))
+#define KMS_SRTP_CONNECTION_UNLOCK(conn) \
+  (g_rec_mutex_unlock (&KMS_SRTP_CONNECTION ((conn))->priv->mutex))
+
+static void
+kms_srtp_connection_interface_init (KmsIRtpConnectionInterface * iface);
+
+G_DEFINE_TYPE_WITH_CODE (KmsSrtpConnection, kms_srtp_connection,
+    KMS_TYPE_RTP_BASE_CONNECTION,
+    G_IMPLEMENT_INTERFACE (KMS_TYPE_I_RTP_CONNECTION,
+        kms_srtp_connection_interface_init));
+
+static gchar *auths[] = {
+  NULL,
+  "hmac-sha1-32",
+  "hmac-sha1-80"
+};
+
+static gchar *ciphers[] = {
+  NULL,
+  "aes-128-icm",
+  "aes-256-icm"
+};
+
+static guint
+kms_srtp_connection_get_rtp_port (KmsRtpBaseConnection * base_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_conn);
+
+  return kms_socket_get_port (self->priv->rtp_socket);
+}
+
+static guint
+kms_srtp_connection_get_rtcp_port (KmsRtpBaseConnection * base_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_conn);
+
+  return kms_socket_get_port (self->priv->rtcp_socket);
+}
+
+static void
+kms_srtp_connection_set_remote_info (KmsRtpBaseConnection * base_conn,
+    const gchar * host, gint rtp_port, gint rtcp_port)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_conn);
+  KmsSrtpConnectionPrivate *priv = self->priv;
+
+  g_object_set (priv->rtp_udpsink, "host", host, "port", rtp_port, NULL);
+  g_object_set (priv->rtcp_udpsink, "host", host, "port", rtcp_port, NULL);
+}
+
+static void
+kms_srtp_connection_add (KmsIRtpConnection * base_rtp_conn, GstBin * bin,
+    gboolean active)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+  KmsSrtpConnectionPrivate *priv = self->priv;
+
+  self->priv->is_client = active;
+
+  gst_bin_add_many (bin, g_object_ref (priv->rtp_udpsink),
+      g_object_ref (priv->rtp_udpsrc),
+      g_object_ref (priv->rtcp_udpsink),
+      g_object_ref (priv->rtcp_udpsrc),
+      g_object_ref (priv->srtpenc), g_object_ref (priv->srtpdec), NULL);
+
+  gst_element_link_pads (priv->rtp_udpsrc, "src", priv->srtpdec, "rtp_sink");
+  gst_element_link_pads (priv->rtcp_udpsrc, "src", priv->srtpdec, "rtcp_sink");
+}
+
+static void
+kms_srtp_connection_src_sync_state_with_parent (KmsIRtpConnection *
+    base_rtp_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+  KmsSrtpConnectionPrivate *priv = self->priv;
+
+  gst_element_sync_state_with_parent (priv->srtpdec);
+  gst_element_sync_state_with_parent (priv->rtp_udpsrc);
+  gst_element_sync_state_with_parent (priv->rtcp_udpsrc);
+}
+
+static void
+kms_srtp_connection_sink_sync_state_with_parent (KmsIRtpConnection *
+    base_rtp_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+  KmsSrtpConnectionPrivate *priv = self->priv;
+
+  gst_element_sync_state_with_parent (priv->srtpenc);
+  gst_element_sync_state_with_parent (priv->rtp_udpsink);
+  gst_element_sync_state_with_parent (priv->rtcp_udpsink);
+}
+
+static GstPad *
+kms_srtp_connection_request_rtp_sink (KmsIRtpConnection * base_rtp_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+
+  return gst_element_get_request_pad (self->priv->srtpenc, "rtp_sink_0");
+}
+
+static GstPad *
+kms_srtp_connection_request_rtp_src (KmsIRtpConnection * base_rtp_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+
+  return gst_element_get_static_pad (self->priv->srtpdec, "rtp_src");
+}
+
+static GstPad *
+kms_srtp_connection_request_rtcp_sink (KmsIRtpConnection * base_rtp_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+
+  return gst_element_get_request_pad (self->priv->srtpenc, "rtcp_sink_0");
+}
+
+static GstPad *
+kms_srtp_connection_request_rtcp_src (KmsIRtpConnection * base_rtp_conn)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (base_rtp_conn);
+
+  return gst_element_get_static_pad (self->priv->srtpdec, "rtcp_src");
+}
+
+static void
+kms_srtp_connection_set_property (GObject * object, guint prop_id,
+    const GValue * value, GParamSpec * pspec)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (object);
+
+  switch (prop_id) {
+    case PROP_ADDED:
+      self->priv->added = g_value_get_boolean (value);
+      break;
+    case PROP_CONNECTED:
+      self->priv->connected = g_value_get_boolean (value);
+      break;
+    case PROP_MIN_PORT:
+      self->parent.min_port = g_value_get_uint (value);
+      break;
+    case PROP_MAX_PORT:
+      self->parent.max_port = g_value_get_uint (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+kms_srtp_connection_get_property (GObject * object,
+    guint prop_id, GValue * value, GParamSpec * pspec)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (object);
+
+  switch (prop_id) {
+    case PROP_ADDED:
+      g_value_set_boolean (value, self->priv->added);
+      break;
+    case PROP_CONNECTED:
+      g_value_set_boolean (value, self->priv->connected);
+      break;
+    case PROP_IS_CLIENT:
+      g_value_set_boolean (value, self->priv->is_client);
+      break;
+    case PROP_MIN_PORT:
+      g_value_set_uint (value, self->parent.min_port);
+      break;
+    case PROP_MAX_PORT:
+      g_value_set_uint (value, self->parent.max_port);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+  }
+}
+
+static void
+kms_srtp_connection_new_pad_cb (GstElement * element, GstPad * pad,
+    KmsSrtpConnection * conn)
+{
+  GstPadTemplate *templ;
+  GstPad *sinkpad = NULL;
+
+  templ = gst_pad_get_pad_template (pad);
+
+  if (g_strcmp0 (GST_PAD_TEMPLATE_NAME_TEMPLATE (templ), "rtp_src_%u") == 0) {
+    sinkpad = gst_element_get_static_pad (conn->priv->rtp_udpsink, "sink");
+  } else if (g_strcmp0 (GST_PAD_TEMPLATE_NAME_TEMPLATE (templ),
+          "rtcp_src_%u") == 0) {
+    sinkpad = gst_element_get_static_pad (conn->priv->rtcp_udpsink, "sink");
+  } else {
+    goto end;
+  }
+
+  gst_pad_link (pad, sinkpad);
+
+end:
+  g_object_unref (templ);
+  g_clear_object (&sinkpad);
+}
+
+static const gchar *
+get_str_auth (guint auth)
+{
+  const gchar *str_auth = NULL;
+
+  if (auth < G_N_ELEMENTS (auths)) {
+    str_auth = auths[auth];
+  }
+
+  return str_auth;
+}
+
+static const gchar *
+get_str_cipher (guint cipher)
+{
+  const gchar *str_cipher = NULL;
+
+  if (cipher < G_N_ELEMENTS (ciphers)) {
+    str_cipher = ciphers[cipher];
+  }
+
+  return str_cipher;
+}
+
+static GstCaps *
+create_key_caps (guint ssrc, const gchar * key, guint auth, guint cipher)
+{
+  const gchar *str_cipher = NULL, *str_auth = NULL;
+  GstBuffer *buff_key;
+  guint8 *bin_buff;
+  GstCaps *caps;
+  gsize len;
+
+  str_cipher = get_str_cipher (cipher);
+  str_auth = get_str_auth (auth);
+
+  if (str_cipher == NULL || str_auth == NULL) {
+    return NULL;
+  }
+
+  bin_buff = g_base64_decode (key, &len);
+  buff_key = gst_buffer_new_wrapped (bin_buff, len);
+
+  caps = gst_caps_new_simple ("application/x-srtp",
+      "srtp-key", GST_TYPE_BUFFER, buff_key,
+      "srtp-cipher", G_TYPE_STRING, str_cipher,
+      "srtp-auth", G_TYPE_STRING, str_auth,
+      "srtcp-cipher", G_TYPE_STRING, str_cipher,
+      "srtcp-auth", G_TYPE_STRING, str_auth, NULL);
+
+  gst_buffer_unref (buff_key);
+
+  return caps;
+}
+
+static GstCaps *
+kms_srtp_connection_request_remote_key_cb (GstElement * srtpdec, guint ssrc,
+    KmsSrtpConnection * conn)
+{
+  GstCaps *caps = NULL;
+
+  KMS_SRTP_CONNECTION_LOCK (conn);
+
+  if (!conn->priv->r_key_set) {
+    GST_DEBUG_OBJECT (conn, "key is not yet set");
+    goto end;
+  }
+
+  if (!conn->priv->r_updated) {
+    GST_DEBUG_OBJECT (conn, "Key is not yet updated");
+  } else {
+    GST_DEBUG_OBJECT (conn, "Using new key");
+    conn->priv->r_updated = FALSE;
+  }
+
+  caps = create_key_caps (ssrc, conn->priv->r_key, conn->priv->r_auth,
+      conn->priv->r_cipher);
+
+  GST_DEBUG_OBJECT (srtpdec, "Key Caps: %" GST_PTR_FORMAT, caps);
+
+end:
+  KMS_SRTP_CONNECTION_UNLOCK (conn);
+
+  return caps;
+}
+
+KmsSrtpConnection *
+kms_srtp_connection_new (guint16 min_port, guint16 max_port)
+{
+  GObject *obj;
+  KmsSrtpConnection *conn;
+  KmsSrtpConnectionPrivate *priv;
+  gint retries = 0;
+
+  obj = g_object_new (KMS_TYPE_SRTP_CONNECTION, NULL);
+  conn = KMS_SRTP_CONNECTION (obj);
+  priv = conn->priv;
+
+  while (!kms_rtp_connection_get_rtp_rtcp_sockets
+      (&priv->rtp_socket, &priv->rtcp_socket, min_port, max_port)
+      && retries++ < MAX_RETRIES) {
+    GST_WARNING_OBJECT (obj, "Getting ports failed, retring");
+  }
+
+  if (priv->rtp_socket == NULL) {
+    GST_ERROR_OBJECT (obj, "Cannot get ports");
+    g_object_unref (obj);
+    return NULL;
+  }
+
+  priv->r_updated = FALSE;
+  priv->r_key_set = FALSE;
+
+  priv->srtpenc = gst_element_factory_make ("srtpenc", NULL);
+  priv->srtpdec = gst_element_factory_make ("srtpdec", NULL);
+  g_signal_connect (priv->srtpenc, "pad-added",
+      G_CALLBACK (kms_srtp_connection_new_pad_cb), obj);
+  g_signal_connect (priv->srtpdec, "request-key",
+      G_CALLBACK (kms_srtp_connection_request_remote_key_cb), obj);
+
+  priv->rtp_udpsink = gst_element_factory_make ("udpsink", NULL);
+  priv->rtp_udpsrc = gst_element_factory_make ("udpsrc", NULL);
+  g_object_set (priv->rtp_udpsink, "socket", priv->rtp_socket,
+      "sync", FALSE, "async", FALSE, NULL);
+  g_object_set (priv->rtp_udpsrc, "socket", priv->rtp_socket, NULL);
+
+  priv->rtcp_udpsink = gst_element_factory_make ("udpsink", NULL);
+  priv->rtcp_udpsrc = gst_element_factory_make ("udpsrc", NULL);
+  g_object_set (priv->rtcp_udpsink, "socket", priv->rtcp_socket,
+      "sync", FALSE, "async", FALSE, NULL);
+  g_object_set (priv->rtcp_udpsrc, "socket", priv->rtcp_socket, NULL);
+
+  kms_i_rtp_connection_connected_signal (KMS_I_RTP_CONNECTION (conn));
+
+  return conn;
+}
+
+static void
+kms_srtp_connection_finalize (GObject * object)
+{
+  KmsSrtpConnection *self = KMS_SRTP_CONNECTION (object);
+  KmsSrtpConnectionPrivate *priv = self->priv;
+
+  GST_DEBUG_OBJECT (self, "finalize");
+
+  g_clear_object (&priv->rtp_udpsink);
+  g_clear_object (&priv->rtp_udpsrc);
+
+  g_clear_object (&priv->rtcp_udpsink);
+  g_clear_object (&priv->rtcp_udpsrc);
+
+  g_clear_object (&priv->srtpenc);
+  g_clear_object (&priv->srtpdec);
+
+  kms_socket_finalize (&self->priv->rtp_socket);
+  kms_socket_finalize (&self->priv->rtcp_socket);
+
+  g_free (priv->r_key);
+
+  g_rec_mutex_clear (&priv->mutex);
+
+  /* chain up */
+  G_OBJECT_CLASS (parent_class)->finalize (object);
+}
+
+static void
+kms_srtp_connection_init (KmsSrtpConnection * self)
+{
+  self->priv = KMS_SRTP_CONNECTION_GET_PRIVATE (self);
+  self->priv->connected = FALSE;
+  g_rec_mutex_init (&self->priv->mutex);
+}
+
+static void
+kms_srtp_connection_class_init (KmsSrtpConnectionClass * klass)
+{
+  GObjectClass *gobject_class;
+  KmsRtpBaseConnectionClass *base_conn_class;
+
+  GST_DEBUG_CATEGORY_INIT (GST_CAT_DEFAULT, GST_DEFAULT_NAME, 0,
+      GST_DEFAULT_NAME);
+
+  gobject_class = G_OBJECT_CLASS (klass);
+  gobject_class->finalize = kms_srtp_connection_finalize;
+  gobject_class->get_property = kms_srtp_connection_get_property;
+  gobject_class->set_property = kms_srtp_connection_set_property;
+
+  base_conn_class = KMS_RTP_BASE_CONNECTION_CLASS (klass);
+  base_conn_class->get_rtp_port = kms_srtp_connection_get_rtp_port;
+  base_conn_class->get_rtcp_port = kms_srtp_connection_get_rtcp_port;
+  base_conn_class->set_remote_info = kms_srtp_connection_set_remote_info;
+
+  g_type_class_add_private (klass, sizeof (KmsSrtpConnectionPrivate));
+
+  g_object_class_override_property (gobject_class, PROP_ADDED, "added");
+  g_object_class_override_property (gobject_class, PROP_CONNECTED, "connected");
+  g_object_class_override_property (gobject_class, PROP_IS_CLIENT, "is-client");
+  g_object_class_override_property (gobject_class, PROP_MAX_PORT, "max-port");
+  g_object_class_override_property (gobject_class, PROP_MIN_PORT, "min-port");
+}
+
+void
+kms_srtp_connection_set_key (KmsSrtpConnection * conn, const gchar * key,
+    guint auth, guint cipher, gboolean local)
+{
+  g_return_if_fail (KMS_IS_SRTP_CONNECTION (conn));
+
+  if (local) {
+    GstBuffer *buff_key;
+    guint8 *bin_buff;
+    gsize len;
+
+    bin_buff = g_base64_decode (key, &len);
+    buff_key = gst_buffer_new_wrapped (bin_buff, len);
+
+    g_object_set (conn->priv->srtpenc, "key", buff_key, "rtp-cipher", cipher,
+        "rtcp-cipher", cipher, "rtp-auth", auth, "rtcp-auth", auth, NULL);
+    gst_buffer_unref (buff_key);
+  } else {
+    gboolean changed;
+
+    KMS_SRTP_CONNECTION_LOCK (conn);
+
+    changed = !conn->priv->r_key_set || g_strcmp0 (key, conn->priv->r_key) != 0
+        || conn->priv->r_auth != auth || conn->priv->r_cipher != cipher;
+
+    if (changed) {
+      g_free (conn->priv->r_key);
+      conn->priv->r_key = g_strdup (key);
+      conn->priv->r_auth = auth;
+      conn->priv->r_cipher = cipher;
+      conn->priv->r_updated = TRUE;
+      conn->priv->r_key_set = TRUE;
+    }
+
+    KMS_SRTP_CONNECTION_UNLOCK (conn);
+  }
+}
+
+static void
+kms_srtp_connection_interface_init (KmsIRtpConnectionInterface * iface)
+{
+  iface->add = kms_srtp_connection_add;
+  iface->src_sync_state_with_parent =
+      kms_srtp_connection_src_sync_state_with_parent;
+  iface->sink_sync_state_with_parent =
+      kms_srtp_connection_sink_sync_state_with_parent;
+  iface->request_rtp_sink = kms_srtp_connection_request_rtp_sink;
+  iface->request_rtp_src = kms_srtp_connection_request_rtp_src;
+  iface->request_rtcp_sink = kms_srtp_connection_request_rtcp_sink;
+  iface->request_rtcp_src = kms_srtp_connection_request_rtcp_src;
+
+  /* This endpoint does not support latency statistics yet */
+  iface->set_latency_callback = NULL;
+  iface->collect_latency_stats = NULL;
+}
---
+++ src/gst-plugins/rtpendpoint/kmssrtpconnection.h
@@ -0,0 +1,57 @@
+/*
+ * (C) Copyright 2015 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+#ifndef __KMS_SRTP_CONNECTION_H__
+#define __KMS_SRTP_CONNECTION_H__
+
+#include "kmsrtpbaseconnection.h"
+
+G_BEGIN_DECLS
+
+#define KMS_TYPE_SRTP_CONNECTION \
+  (kms_srtp_connection_get_type())
+#define KMS_SRTP_CONNECTION(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),KMS_TYPE_SRTP_CONNECTION,KmsSrtpConnection))
+#define KMS_SRTP_CONNECTION_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),KMS_TYPE_SRTP_CONNECTION,KmsSrtpConnectionClass))
+#define KMS_IS_SRTP_CONNECTION(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),KMS_TYPE_SRTP_CONNECTION))
+#define KMS_IS_SRTP_CONNECTION_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),KMS_TYPE_SRTP_CONNECTION))
+#define KMS_SRTP_CONNECTION_CAST(obj) ((KmsSrtpConnection*)(obj))
+
+typedef struct _KmsSrtpConnectionPrivate KmsSrtpConnectionPrivate;
+typedef struct _KmsSrtpConnection KmsSrtpConnection;
+typedef struct _KmsSrtpConnectionClass KmsSrtpConnectionClass;
+
+struct _KmsSrtpConnection
+{
+  KmsRtpBaseConnection parent;
+
+  KmsSrtpConnectionPrivate *priv;
+};
+
+struct _KmsSrtpConnectionClass
+{
+  KmsRtpBaseConnectionClass parent_class;
+};
+
+GType kms_srtp_connection_get_type (void);
+
+KmsSrtpConnection *kms_srtp_connection_new (guint16 min_port, guint16 max_port);
+void kms_srtp_connection_set_key (KmsSrtpConnection *conn, const gchar *key, guint auth, guint cipher, gboolean local);
+
+G_END_DECLS
+#endif /* __KMS_RTP_CONNECTION_H__ */
