23c928bf3aedcbdaac27d2e96519aca381668110
Javier Lopez <fjlopez@kurento.com>
2015-10-17 21:41:44 +0200

    OPtimize tool kurento_capability_test
    Change-Id: I1f691ffd3cdd6e4c2a5859c503f9504ba396ad29

--- kurento_capability_test.sh
+++ kurento_capability_test.sh
@@ -11,28 +11,41 @@ cleanup() {
 
 trap cleanup EXIT
 echo "##################### EXECUTE: capability-test #####################"
+# This tool uses a set of variables expected to be exported by tester
 # PROJECT_PATH string
-#    Identifies the module to execute within a reactor project
+#    Identifies the module to execute within a reactor project.
+#
+# WORKSPACE path
+#    Jenkins workspace path. This variable is expected to be exported by
+#    script caller.
+#
+# MAVEN_SETTINGS path
+#     Location of the settings.xml file used by maven
+#
+# MAVEN_OPTS string
+#     All settings defined in this varible will be added to mvn command line
 #
 # RECORD_TEST [ true | false ]
 #    Activates session recording in case ffmpeg is available
 #    DEFAULT: false
 #
-# Parameter management
 
-if [ $# -lt 1 ]; then
-  echo "Usage: $0 <PROJECT_PATH>"
-  exit 1
-fi
+# Get CLI parameter for backward compatibility
+[ -n "$1" ] && PROJECT_PATH=$1
 
-# TEST_FILTER
-[ -n "$1" ] && PROJECT_PATH="$1" || exit 1
-
-# Check if we want to record session
-[ -n "$RECORD_TEST" ] || RECORD_TEST="false"
-
+# Set default environment if required
 export DISPLAY=:1
-if [ "$USE_FFMPEG" == "true" ] ; then
+mavenOpts=""
+[ -z "$WORKSPACE" ] && WORKSPACE="."
+if [ -n "$PROJECT_PATH" ]; then
+  mavenOpts="$mavenOpts -am -pl $PROJECT_PATH"
+  mavenOpts="$mavenOpts -Dproject.path=$WORKSPACE/$PROJECT_PATH"
+fi
+mavenOpts="$mavenOpts -Dkurento.workspace=$WORKSPACE"
+mavenOpts="$mavenOpts -DfailIfNoTests=false"
+mavenOpts="$mavenOpts -U"
+
+if [ "$RECORD_TEST" = "true" ]; then
   echo "***************** Recording session to  $WORKSPACE/session-recording.mp4"
   ffmpeg -video_size 800x600 -framerate 2 -f x11grab -i :1.0+0,0 $WORKSPACE/session-recording.mp4 &
   echo $! > ffmpeg.pid
@@ -42,9 +55,5 @@ fi
 [ -d $WORKSPACE/kurento-java ] &&  \
   (cd kurento-java &&  mvn --settings $MAVEN_SETTINGS clean install -Pdeploy -U -Dmaven.test.skip=true)
 
-mavenOpts="-U -am -pl $PROJECT_PATH"
-mavenOpts="$mavenOpts -DfailIfNoTests=false"
-mavenOpts="$mavenOpts -Dkurento.workspace=$WORKSPACE"
-mavenOpts="$mavenOpts -Dproject.path=$WORKSPACE/$PROJECT_PATH"
-
+# Execute capability test
 mvn --settings $MAVEN_SETTINGS clean verify $mavenOpts $MAVEN_OPTS
