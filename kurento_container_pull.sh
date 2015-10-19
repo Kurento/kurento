f0abfc14309495bf5f657b312538d700242b3c9d
patxi <patxi.gortazar@gmail.com>
2015-10-19 12:58:44 +0200

    Add kurento_container_pull script to pull all images needed on container agents
    Change-Id: I0022095f8f3dcde09ec0fcf5e36c143b1085ca77

---
+++ kurento_container_pull.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/dev-integration:jdk-7-node-0.12
+dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/dev-integration:jdk-8-node-0.12
+dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/kurento-media-server-dev:latest
+dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/test-files:1.0.0
+dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/dev-documentation:1.0.0
+
+docker pull mongo:2.6.11
+docker pull selenium/hub:2.47.1
+docker pull selenium/node-chrome:2.47.1
+docker pull selenium/node-firefox:2.47.1
+
+docker images > container_images.txt
+
+# Keep just KEEP_IMAGES last kms dev images
+KEEP_IMAGES=3
+NUM_IMAGES=$(docker images | grep kurento-media-server-dev | awk '{print $2}' | sort | wc -l)
+if [ $NUM_IMAGES -gt $KEEP_IMAGES ]; then
+	NUM_REMOVE_IMAGES=$[$NUM_IMAGES-$KEEP_IMAGES]
+	REMOVE_IMAGES=$(docker images | grep kurento-media-server-dev | awk '{print $2}' | sort | head -$NUM_REMOVE_IMAGES)
+    status=0
+    for image in $REMOVE_IMAGES
+    do
+    	echo "Removing image $image"
+        docker rmi kurento/kurento-media-server-dev:$image || $status=$[$status || $?]
+    done
+fi
+
+exit $status
