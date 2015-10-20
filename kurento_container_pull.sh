1a2fd7c05e4f3660767106ddd70ef7ed72a1f13e
patxi <patxi.gortazar@gmail.com>
2015-10-20 12:38:40 +0200

    Add selenium debug images
    Change-Id: I7479ae47e24584704ccf999510c3dde57cdcaf31

--- kurento_container_pull.sh
+++ kurento_container_pull.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+# Internal (private) images
 dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/dev-integration:jdk-7-node-0.12
 dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/dev-integration:jdk-8-node-0.12
 dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/kurento-media-server-dev:latest
 dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/test-files:1.0.0
 dogestry pull s3://kurento-docker/?region=eu-west-1 kurento/dev-documentation:1.0.0
 
-docker pull mongo:2.6.11
+# Selenium images
 docker pull selenium/hub:2.47.1
 docker pull selenium/node-chrome:2.47.1
 docker pull selenium/node-firefox:2.47.1
+docker pull selenium/node-chrome-debug:2.47.1
+docker pull selenium/node-firefox-debug:2.47.1
+
+# Image to record vnc sessions
 docker pull softsam/vncrecorder:latest
+
+# Mongo image
+docker pull mongo:2.6.11
 
 docker images > container_images.txt
 
