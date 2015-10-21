68f7c59ebd19f4f31cfc5efc4fa917c9cba64502
patxi <patxi.gortazar@gmail.com>
2015-10-21 16:19:41 +0200

    Add dockerized js_sanity_checks
    Change-Id: I5439a843fe440ce188ec6cebb7b66d89bfffb21b

---
+++ kurento_ci_js_sanity_checks.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "##################### EXECUTE: kurento-js-sanity-checks #####################"
+
+# Param management
+
+# Kurento Websockets URL
+[ -n "$1" ] && WS_URI=$1 || exit 1
+
+# Kurento js release URL
+[ -n "$2" ] && KURENTO_JS_RELEASE_URL=$2 || exit 1
+
+if [ -z "$MAVEN_SETTINGS"]; then
+  echo "MAVEN_SETTINGS must be available as an environment variable"
+fi
+
+echo "Building $KURENTO_JS_RELEASE_URL"
+
+#!/bin/bash
+
+# Test autostart KMS
+
+if [ $# -lt 2 ]
+then
+  echo "Usage: $0 <groups> <test>"
+  exit 1
+fi
+
+TEST_GROUP="org.kurento.commons.testing.SanityTests"
+
+# Create Integration container
+TEST_HOME=/opt/kurento-java
+
+MAVEN_OPTS=""
+MAVEN_OPTS="$MAVEN_OPTS -Djava.awt.headless=true"
+MAVEN_OPTS="$MAVEN_OPTS -Dwdm.chromeDriverUrl=http://chromedriver.kurento.org/"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.autostart=test"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.scope=docker"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.docker.image.name=kurento/kurento-media-server-dev:latest"
+MAVEN_OPTS="$MAVEN_OPTS -Dproject.path=$TEST_HOME/kurento-integration-tests/kurento-sanity-test"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.selenium.scope=docker"
+MAVEN_OPTS="$MAVEN_OPTS -Dgroups=$TEST_GROUP"
+MAVEN_OPTS="$MAVEN_OPTS -Dkms.ws.uri=$WS_URI"
+MAVEN_OPTS="$MAVEN_OPTS -Dkurento.release.url=$KURENTO_JS_RELEASE_URL"
+
+# Execute Presenter test
+docker run --rm \
+  --name $BUILD_TAG-INTEGRATION \
+  -v $MAVEN_SETTINGS:/opt/kurento-settings.xml \
+  -v $KURENTO_SCRIPTS_HOME:/opt/adm-scripts \
+  -v $WORKSPACE:$TEST_HOME \
+  -e "WORKSPACE=$TEST_HOME" \
+  -e "MAVEN_SETTINGS=/opt/kurento-settings.xml" \
+  -e "MAVEN_OPTS=$MAVEN_OPTS" \
+  -w $TEST_HOME \
+  -u "root" \
+  kurento/dev-integration:jdk-8-node-0.12 \
+  /opt/adm-scripts/kurento_capability_test.sh kurento-integration-tests/kurento-sanity-test || status=$?
+
+exit $status
