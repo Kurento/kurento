5459641aff7c46c49f1e69566b818a7568db3aac
patxi <patxi.gortazar@gmail.com>
2015-10-22 16:22:24 +0200

    Fix temp folder for capability tests
    Change-Id: I5ccce5779c697f58d46b630b15eae730b469d49e

--- kurento_ci_capability_test.sh
+++ kurento_ci_capability_test.sh
@@ -34,7 +34,7 @@ MAVEN_OPTS="$MAVEN_OPTS -Dwdm.chromeDriverUrl=http://chromedriver.kurento.org/"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.autostart=test"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.scope=docker"
 MAVEN_OPTS="$MAVEN_OPTS -Dproject.path=$TEST_HOME/kurento-integration-tests/kurento-test"
-MAVEN_OPTS="$MAVEN_OPTS -Dtest.workspace=$TEST_WORKSPACE"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.workspace=$TEST_HOME/tmp"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.files=/var/lib/test-files/kurento"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.docker.image.name=kurento/kurento-media-server-dev:latest"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.selenium.scope=docker"
@@ -48,6 +48,7 @@ docker run --rm \
   -v $MAVEN_SETTINGS:/opt/kurento-settings.xml \
   -v $KURENTO_SCRIPTS_HOME:/opt/adm-scripts \
   -v $WORKSPACE:$TEST_HOME \
+  -v $TEST_WORKSPACE:$TEST_HOME/tmp
   -e "WORKSPACE=$TEST_HOME" \
   -e "MAVEN_SETTINGS=/opt/kurento-settings.xml" \
   -e "MAVEN_OPTS=$MAVEN_OPTS" \
