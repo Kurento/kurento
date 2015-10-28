194967ac2cb8444bd87ec1344a528d26520a2f6f
patxi <patxi.gortazar@gmail.com>
2015-10-28 16:58:50 +0100

    ci_js_sanity_checks: fix docker run
    Change-Id: I3b4656a05cf6b8d87e48fdea6cda9b9e0749eba4

--- kurento_ci_js_sanity_checks.sh
+++ kurento_ci_js_sanity_checks.sh
@@ -22,10 +22,15 @@ echo "Building $KURENTO_JS_RELEASE_URL"
 
 TEST_GROUP="org.kurento.commons.testing.SanityTests"
 
+# Create temporary folder for container
+TEST_WORKSPACE=$WORKSPACE/tmp
+mkdir $TEST_WORKSPACE
+
 # Create Integration container
 TEST_HOME=/opt/kurento-java
 
 MAVEN_OPTS=""
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.docker.image.forcepulling=false"
 MAVEN_OPTS="$MAVEN_OPTS -Djava.awt.headless=true"
 MAVEN_OPTS="$MAVEN_OPTS -Dwdm.chromeDriverUrl=http://chromedriver.kurento.org/"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.autostart=test"
@@ -33,6 +38,10 @@ MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.scope=docker"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.kms.docker.image.name=kurento/kurento-media-server-dev:latest"
 MAVEN_OPTS="$MAVEN_OPTS -Dproject.path=$TEST_HOME/kurento-integration-tests/kurento-sanity-test"
 MAVEN_OPTS="$MAVEN_OPTS -Dtest.selenium.scope=docker"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.selenium.record=$TEST_SELENIUM_RECORD"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.files=/var/lib/test-files/kurento"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.workspace=$TEST_HOME/tmp"
+MAVEN_OPTS="$MAVEN_OPTS -Dtest.workspace.host=$TEST_WORKSPACE"
 MAVEN_OPTS="$MAVEN_OPTS -Dgroups=$TEST_GROUP"
 MAVEN_OPTS="$MAVEN_OPTS -Dkms.ws.uri=$WS_URI"
 MAVEN_OPTS="$MAVEN_OPTS -Dkurento.release.url=$KURENTO_JS_RELEASE_URL"
@@ -40,9 +49,11 @@ MAVEN_OPTS="$MAVEN_OPTS -Dkurento.release.url=$KURENTO_JS_RELEASE_URL"
 # Execute Presenter test
 docker run --rm \
   --name $BUILD_TAG-INTEGRATION \
+  -v /var/lib/jenkins/test-files:/var/lib/test-files \
   -v $MAVEN_SETTINGS:/opt/kurento-settings.xml \
   -v $KURENTO_SCRIPTS_HOME:/opt/adm-scripts \
   -v $WORKSPACE:$TEST_HOME \
+  -v $TEST_WORKSPACE:$TEST_HOME/tmp \
   -e "WORKSPACE=$TEST_HOME" \
   -e "MAVEN_SETTINGS=/opt/kurento-settings.xml" \
   -e "MAVEN_OPTS=$MAVEN_OPTS" \
