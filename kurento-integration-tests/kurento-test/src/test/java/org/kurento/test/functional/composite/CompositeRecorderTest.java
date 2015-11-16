2bfe742e6025d4a39525d93554a916a4ca1ef89c
David Fernandez <d.fernandezlop@gmail.com>
2015-11-16 11:14:14 +0100

    [kurento-test] Add tests with composite and recorder
    Change-Id: Ibb4e2c9c5f1ba2166076a55267afbafc8863c67a

---
+++ kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/composite/CompositeRecorderTest.java
@@ -0,0 +1,138 @@
+package org.kurento.test.functional.composite;
+
+import java.awt.Color;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.kurento.client.Composite;
+import org.kurento.client.HubPort;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.PlayerEndpoint;
+import org.kurento.client.RecorderEndpoint;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.test.config.Protocol;
+import org.kurento.test.config.TestScenario;
+import org.kurento.test.functional.recorder.BaseRecorder;
+
+/**
+ * Four synthetic videos are played by four PlayerEndpoint and mixed by a
+ * Composite. The resulting video is recording using a RecorderEndpoint. The
+ * recorded video is played using a PlayerEndpoint.<br/>
+ * 
+ * Media Pipeline(s): <br>
+ * · 4xPlayerEndpoint -> Composite -> RecorderEndpoint <br>
+ * · PlayerEndpoint -> WebRtcEndpoint <br>
+ * 
+ * Browser(s): <br>
+ * · Chrome <br>
+ * · Firefox <br>
+ * 
+ * Test logic: <br>
+ * 1. (KMS) Media server implements a grid with the media from 4 PlayerEndpoints
+ * and it records the grid using the RecorderEndpoint. <br>
+ * 2. (KMS) Media server implements a player to reproduce the video recorded in
+ * step 1. <br>
+ * 3. (Browser) WebRtcPeer in rcv-only receives media <br>
+ * 
+ * Main assertion(s): <br>
+ * · Color of the video should be the expected in the recorded video (red,
+ * green, blue, and white)<br>
+ * 
+ * Secondary assertion(s): <br>
+ * · Playing event should be received in remote video tag <br>
+ * 
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.1.1
+ */
+
+public class CompositeRecorderTest extends BaseRecorder {
+
+	private static int RECORDTIME = 20; // seconds
+	private static int PLAYTIME = 20; // seconds
+
+	public CompositeRecorderTest(TestScenario testScenario) {
+		super(testScenario);
+	}
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> data() {
+		return TestScenario.localChromeAndFirefox();
+	}
+
+	@Test
+	public void testCompositeRecorder() throws Exception {
+		// MediaPipeline
+		MediaPipeline mp = kurentoClient.createMediaPipeline();
+
+		PlayerEndpoint playerRed = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/red.webm").build();
+		PlayerEndpoint playerGreen = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/green.webm").build();
+		PlayerEndpoint playerBlue = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/blue.webm").build();
+		PlayerEndpoint playerWhite = new PlayerEndpoint.Builder(mp,
+				"http://files.kurento.org/video/30sec/white.webm").build();
+
+		Composite composite = new Composite.Builder(mp).build();
+		HubPort hubPort1 = new HubPort.Builder(composite).build();
+		HubPort hubPort2 = new HubPort.Builder(composite).build();
+		HubPort hubPort3 = new HubPort.Builder(composite).build();
+		HubPort hubPort4 = new HubPort.Builder(composite).build();
+		HubPort hubPort5 = new HubPort.Builder(composite).build();
+		String recordingFile = getDefaultOutputFile(EXTENSION_WEBM);
+		RecorderEndpoint recorderEP = new RecorderEndpoint.Builder(mp,
+				Protocol.FILE + recordingFile).build();
+
+		playerRed.connect(hubPort1);
+		playerGreen.connect(hubPort2);
+		playerBlue.connect(hubPort3);
+		playerWhite.connect(hubPort4);
+
+		hubPort5.connect(recorderEP);
+
+		playerRed.play();
+		playerGreen.play();
+		playerBlue.play();
+		playerWhite.play();
+
+		recorderEP.record();
+
+		Thread.sleep(RECORDTIME * 1000);
+
+		recorderEP.stop();
+
+		playerRed.stop();
+		playerGreen.stop();
+		playerBlue.stop();
+		playerWhite.stop();
+
+		// Media Pipeline #2
+		MediaPipeline mp2 = kurentoClient.createMediaPipeline();
+		PlayerEndpoint playerEP2 = new PlayerEndpoint.Builder(mp2,
+				Protocol.FILE + recordingFile).build();
+		WebRtcEndpoint webRtcEP2 = new WebRtcEndpoint.Builder(mp2).build();
+		playerEP2.connect(webRtcEP2);
+
+		// Playing the recording
+		launchBrowser(mp, webRtcEP2, playerEP2, null,
+				EXPECTED_VIDEO_CODEC_WEBM, EXPECTED_AUDIO_CODEC_WEBM,
+				recordingFile, Color.RED, 0, 0, PLAYTIME);
+
+		// Assertions
+		Assert.assertTrue("Upper left part of the video must be red", getPage()
+				.similarColorAt(Color.RED, 0, 0));
+		Assert.assertTrue("Upper right part of the video must be green",
+				getPage().similarColorAt(Color.GREEN, 450, 0));
+		Assert.assertTrue("Lower left part of the video must be blue",
+				getPage().similarColorAt(Color.BLUE, 0, 450));
+		Assert.assertTrue("Lower right part of the video must be white",
+				getPage().similarColorAt(Color.WHITE, 450, 450));
+
+		// Release Media Pipeline #2
+		mp2.release();
+
+		success = true;
+	}
+}
---
+++ kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/composite/CompositeWebRtcRecorderTest.java
@@ -0,0 +1,174 @@
+package org.kurento.test.functional.composite;
+
+import java.awt.Color;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runners.Parameterized.Parameters;
+import org.kurento.client.Composite;
+import org.kurento.client.HubPort;
+import org.kurento.client.MediaPipeline;
+import org.kurento.client.MediaType;
+import org.kurento.client.PlayerEndpoint;
+import org.kurento.client.RecorderEndpoint;
+import org.kurento.client.WebRtcEndpoint;
+import org.kurento.test.browser.Browser;
+import org.kurento.test.browser.BrowserType;
+import org.kurento.test.browser.WebPageType;
+import org.kurento.test.browser.WebRtcChannel;
+import org.kurento.test.browser.WebRtcMode;
+import org.kurento.test.config.BrowserScope;
+import org.kurento.test.config.Protocol;
+import org.kurento.test.config.TestScenario;
+import org.kurento.test.functional.recorder.BaseRecorder;
+
+/**
+ * Four synthetic videos are played by four WebRtcEndpoint (Connect only audio
+ * for thres videos) and mixed by a Composite. The resulting video is recording
+ * using a RecorderEndpoint. The recorded video is played using a
+ * PlayerEndpoint.<br/>
+ * 
+ * Media Pipeline(s): <br>
+ * · 4xWebRtcEndpoint -> Composite -> RecorderEndpoint <br>
+ * · PlayerEndpoint -> WebRtcEndpoint <br>
+ * 
+ * Browser(s): <br>
+ * · Chrome <br>
+ * 
+ * Test logic: <br>
+ * 1. (KMS) Media server mix the media from 4 WebRtcEndpoint and it records the
+ * grid using the RecorderEndpoint. Only red color (video from first player) and
+ * audio from four players.<br>
+ * 2. (KMS) Media server implements a player to reproduce the video recorded in
+ * step 1. <br>
+ * 3. (Browser) WebRtcPeer in rcv-only receives media <br>
+ * 
+ * Main assertion(s): <br>
+ * · Color of the video should be the expected in the recorded video (red)<br>
+ * 
+ * Secondary assertion(s): <br>
+ * · Playing event should be received in remote video tag <br>
+ * 
+ * @author David Fernandez (d.fernandezlop@gmail.com)
+ * @since 6.1.1
+ */
+
+public class CompositeWebRtcRecorderTest extends BaseRecorder {
+
+	private static int RECORDTIME = 20; // seconds
+	private static int PLAYTIME = 10; // seconds
+
+	private static final String BROWSER1 = "browser1";
+	private static final String BROWSER2 = "browser2";
+	private static final String BROWSER3 = "browser3";
+	private static final String BROWSER4 = "browser4";
+	private static final String BROWSER5 = "browser5";
+
+	public CompositeWebRtcRecorderTest(TestScenario testScenario) {
+		super(testScenario);
+	}
+
+	@Parameters(name = "{index}: {0}")
+	public static Collection<Object[]> data() {
+		// Test: 5 local Chrome's
+		TestScenario test = new TestScenario();
+		test.addBrowser(
+				BROWSER1,
+				new Browser.Builder().browserType(BrowserType.CHROME)
+						.webPageType(WebPageType.WEBRTC)
+						.scope(BrowserScope.LOCAL).build());
+		test.addBrowser(
+				BROWSER2,
+				new Browser.Builder().browserType(BrowserType.CHROME)
+						.webPageType(WebPageType.WEBRTC)
+						.scope(BrowserScope.LOCAL)
+						.video(getPathTestFiles() + "/video/10sec/red.y4m")
+						.build());
+		test.addBrowser(
+				BROWSER3,
+				new Browser.Builder().browserType(BrowserType.CHROME)
+						.webPageType(WebPageType.WEBRTC)
+						.scope(BrowserScope.LOCAL)
+						.video(getPathTestFiles() + "/video/10sec/green.y4m")
+						.build());
+		test.addBrowser(
+				BROWSER4,
+				new Browser.Builder().browserType(BrowserType.CHROME)
+						.webPageType(WebPageType.WEBRTC)
+						.scope(BrowserScope.LOCAL)
+						.video(getPathTestFiles() + "/video/10sec/blue.y4m")
+						.build());
+		test.addBrowser(
+				BROWSER5,
+				new Browser.Builder().browserType(BrowserType.CHROME)
+						.webPageType(WebPageType.WEBRTC)
+						.scope(BrowserScope.LOCAL)
+						.video(getPathTestFiles() + "/video/10sec/white.y4m")
+						.build());
+		return Arrays.asList(new Object[][] { { test } });
+	}
+
+	@Test
+	public void testCompositeRecorder() throws Exception {
+
+		// MediaPipeline
+		MediaPipeline mp = kurentoClient.createMediaPipeline();
+
+		WebRtcEndpoint webRtcEPRed = new WebRtcEndpoint.Builder(mp).build();
+		WebRtcEndpoint webRtcEPGreen = new WebRtcEndpoint.Builder(mp).build();
+		WebRtcEndpoint webRtcEPBlue = new WebRtcEndpoint.Builder(mp).build();
+		WebRtcEndpoint webRtcEPWhite = new WebRtcEndpoint.Builder(mp).build();
+
+		Composite composite = new Composite.Builder(mp).build();
+		HubPort hubPort1 = new HubPort.Builder(composite).build();
+		HubPort hubPort2 = new HubPort.Builder(composite).build();
+		HubPort hubPort3 = new HubPort.Builder(composite).build();
+		HubPort hubPort4 = new HubPort.Builder(composite).build();
+		HubPort hubPort5 = new HubPort.Builder(composite).build();
+		String recordingFile = getDefaultOutputFile(EXTENSION_WEBM);
+		RecorderEndpoint recorderEP = new RecorderEndpoint.Builder(mp,
+				Protocol.FILE + recordingFile).build();
+
+		webRtcEPRed.connect(hubPort1);
+		webRtcEPGreen.connect(hubPort2, MediaType.AUDIO);
+		webRtcEPBlue.connect(hubPort3, MediaType.AUDIO);
+		webRtcEPWhite.connect(hubPort4, MediaType.AUDIO);
+
+		hubPort5.connect(recorderEP);
+
+		// WebRTC browsers
+		getPage(BROWSER2).initWebRtc(webRtcEPRed,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+		getPage(BROWSER3).initWebRtc(webRtcEPGreen,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+		getPage(BROWSER4).initWebRtc(webRtcEPBlue,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+		getPage(BROWSER5).initWebRtc(webRtcEPWhite,
+				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+
+		recorderEP.record();
+
+		Thread.sleep(PLAYTIME * 1000);
+
+		recorderEP.stop();
+		mp.release();
+
+		// Media Pipeline #2
+		MediaPipeline mp2 = kurentoClient.createMediaPipeline();
+		PlayerEndpoint playerEP2 = new PlayerEndpoint.Builder(mp2,
+				Protocol.FILE + recordingFile).build();
+		WebRtcEndpoint webRtcEP2 = new WebRtcEndpoint.Builder(mp2).build();
+		playerEP2.connect(webRtcEP2);
+
+		// Playing the recorded file
+		launchBrowser(mp2, webRtcEP2, playerEP2, null,
+				EXPECTED_VIDEO_CODEC_WEBM, EXPECTED_AUDIO_CODEC_WEBM,
+				recordingFile, Color.RED, 0, 0, PLAYTIME);
+
+		// Release Media Pipeline #2
+		mp2.release();
+
+		success = true;
+	}
+}
