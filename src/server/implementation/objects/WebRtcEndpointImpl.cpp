46756d58abd3fe28b7919594d79047486ad5fc68
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2015-11-18 11:18:09 +0100

    WebRtcEndpoint: Do not use c_str() pointer from temporal string.
    Change-Id: Ic56de4644645df81297ac202e3bc9b989821e543

--- src/server/implementation/objects/WebRtcEndpointImpl.cpp
+++ src/server/implementation/objects/WebRtcEndpointImpl.cpp
@@ -405,10 +405,11 @@ void
 WebRtcEndpointImpl::addIceCandidate (std::shared_ptr<IceCandidate> candidate)
 {
   gboolean ret;
-  const char *cand_str = candidate->getCandidate().c_str ();
-  const char *mid_str = candidate->getSdpMid().c_str ();
+  std::string cand_str = candidate->getCandidate();
+  std::string mid_str = candidate->getSdpMid ();
   guint8 sdp_m_line_index = candidate->getSdpMLineIndex ();
-  KmsIceCandidate *cand = kms_ice_candidate_new (cand_str, mid_str,
+  KmsIceCandidate *cand = kms_ice_candidate_new (cand_str.c_str(),
+                          mid_str.c_str(),
                           sdp_m_line_index);
 
   g_signal_emit_by_name (element, "add-ice-candidate", this->sessId.c_str (),
