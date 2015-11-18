6118b70a0227de4e5cdb357c67bdfe796524c74c
patxi <patxi.gortazar@gmail.com>
2015-11-19 00:32:15 +0100

    Add kurento_ci_container_run_bash.sh to run a generic command in a bash shell within a container
    Change-Id: I25a59a6b3b7ca978134ee5ebc1b1c0d95f7e0ee9

---
+++ kurento_ci_container_run_bash.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -x
+
+# This tool uses a set of variables expected to be exported by tester
+# WORKSPACE path
+#    Mandatory
+#    Jenkins workspace path. This variable is expected to be exported by
+#    script caller.
+#
+# BUILD_TAG string
+#    Mandatory
+#    A name to uniquely identify containers created within this job
+#
+# DOCKER_IMAGE
+#    Mandatory
+#    Docker image to run
+#
+# COMMAND
+#    Mandatory
+#    Command to run. The command is run with bash -c "$COMMAND"
+#
+# MAVEN_SETTINGS path
+#    Mandatory
+#    Location of the settings.xml file used by maven
+#
+# MAVEN_KURENTO_RELEASES
+#    Optional
+#
+# MAVEN_KURENTO_SNAPSHOTS
+#    Optional
+#
+# MAVEN_SONATYPE_NEXUS_STAGING
+#    Optional
+#
+# KURENTO_GIT_REPOSITORY
+#    Optional
+#
+
+cat >./.root-config <<EOL
+StrictHostKeyChecking no
+User jenkins
+IdentityFile /root/.ssh/id_rsa
+EOL
+
+docker run \
+  --name ${BUILD_TAG} \
+  --rm \
+  -v $WORKSPACE:/opt/kurento \
+  -v $KURENTO_SCRIPTS_HOME:/opt/adm-scripts \
+  -v $HOME/.ssh:/root/.ssh:ro \
+  -v $HOME/.gitconfig:/root/.gitconfig \
+  -v $PWD/.root-config:/root/.ssh/config \
+  -e "MAVEN_KURENTO_RELEASES=$MAVEN_KURENTO_RELEASES" \
+  -e "MAVEN_KURENTO_SNAPSHOTS=$MAVEN_KURENTO_SNAPSHOTS" \
+  -e "MAVEN_SONATYPE_NEXUS_STAGING=$MAVEN_SONATYPE_NEXUS_STAGING" \
+  -e "KURENTO_GIT_REPOSITORY=$KURENTO_GIT_REPOSITORY" \
+  -w /opt/kurento \
+    $DOCKER_IMAGE \
+      bash -c \
+      	"$COMMAND"
