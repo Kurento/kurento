306f726d89f65646a95aafd6166731c6f672d0b4
patxi <patxi.gortazar@gmail.com>
2015-11-20 13:25:38 +0100

    Add merge java project script
    Change-Id: Ie51585c58292b33d245409b4d52c0ccdd3a4f1f4

---
+++ kurento_merge_java_project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+PATH=$PATH:${KURENTO_SCRIPTS_HOME}
+kurento_check_version.sh true
+
+# Deploy to Kurento repositories
+export SNAPSHOT_REPOSITORY=$MAVEN_KURENTO_SNAPSHOTS
+export RELEASE_REPOSITORY=$MAVEN_KURENTO_RELEASES
+kurento_maven_deploy.sh "$MAVEN_SETTINGS"
+
+# Deploy to Central (only release)
+export SNAPSHOT_REPOSITORY=
+export RELEASE_REPOSITORY=$MAVEN_SONATYPE_NEXUS_STAGING
+kurento_maven_deploy.sh
+
+# Upload to builds
+VERSION=$(kurento_get_version.sh)
+echo "$VERSION - $(date) - $(date +'%Y%m%d-%H%M%S')" > project-version.txt
