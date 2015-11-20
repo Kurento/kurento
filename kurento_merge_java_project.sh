1bd07cf1b0346cf6729333db91dc5faf1b3a1ecd
patxi <patxi.gortazar@gmail.com>
2015-11-20 13:42:01 +0100

    merge_java_project: change env variables to use. Document mandatory env variables.
    Change-Id: Ifedbf5b70170d387134c3afe7905ce9aab8f2d03

--- kurento_merge_java_project.sh
+++ kurento_merge_java_project.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -x
+
+# SNAPSHOT_REPOSITORY
+# Mandatory
+#
+# RELEASE_REPOSITORY
+# Mandatory
+#
+
 PATH=$PATH:${KURENTO_SCRIPTS_HOME}
 kurento_check_version.sh true
 
 # Deploy to Kurento repositories
-export SNAPSHOT_REPOSITORY=$MAVEN_KURENTO_SNAPSHOTS
-export RELEASE_REPOSITORY=$MAVEN_KURENTO_RELEASES
 kurento_maven_deploy.sh "$MAVEN_SETTINGS"
 
 # Deploy to Central (only release)
-export SNAPSHOT_REPOSITORY=
-export RELEASE_REPOSITORY=$MAVEN_SONATYPE_NEXUS_STAGING
 kurento_maven_deploy.sh
 
 # Upload to builds
