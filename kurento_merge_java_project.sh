3bba52dc442d218b0897466cac380e9e94c5f1c5
patxi <patxi.gortazar@gmail.com>
2015-11-23 16:22:35 +0100

    kurento_merge_java_project.sh: publish http files
    Change-Id: I0618464be1678c014a5bd59a70039a22e264eab6

--- kurento_merge_java_project.sh
+++ kurento_merge_java_project.sh
@@ -1,21 +1,36 @@
 #!/bin/bash -x
 
-# SNAPSHOT_REPOSITORY
-# Mandatory
+# MAVEN_KURENTO_SNAPSHOTS url
+#   URL of Kurento repository for maven snapshots
 #
-# RELEASE_REPOSITORY
-# Mandatory
+# MAVEN_KURENTO_RELEASES url
+#   URL of Kurento repository for maven releases
 #
+# MAVEN_SONATYPE_NEXUS_STAGING url
+#   URL of Central staging repositories.
+#   Pass it empty to avoid deploying to nexus (private projects):
+#   export MAVEN_SONATYPE_NEXUS_STAGING=
+#   kurento_merge_java_project.sh
 
 PATH=$PATH:${KURENTO_SCRIPTS_HOME}
 kurento_check_version.sh true
 
 # Deploy to Kurento repositories
-kurento_maven_deploy.sh "$MAVEN_SETTINGS"
-
-# Deploy to Central (only release)
+export SNAPSHOT_REPOSITORY=$MAVEN_KURENTO_SNAPSHOTS
+export RELEASE_REPOSITORY=$MAVEN_KURENTO_RELEASES
 kurento_maven_deploy.sh
 
-# Upload to builds
+# Deploy to Central (only release)
+export SNAPSHOT_REPOSITORY=
+export RELEASE_REPOSITORY=$MAVEN_SONATYPE_NEXUS_STAGING
+kurento_maven_deploy.sh
+
+# Deploy to builds only when it is release
 VERSION=$(kurento_get_version.sh)
-echo "$VERSION" > project-version.txt
+if [[ $VERSION != *-SNAPSHOT ]]; then
+  # Create version file
+  echo "$VERSION - $(date) - $(date +"%Y%m%d-%H%M%S")" > project.version
+
+  export FILES
+  kurento_http_publish.sh
+fi
