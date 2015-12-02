9e2e678f4dfa0797745296ea341397c223a6f359
patxi <patxi.gortazar@gmail.com>
2015-12-02 14:25:50 +0100

    entrypoint: fix missing ;
    Change-Id: I75bd1c3629f99c086afe390cba63fa7df4a46e12

--- kurento_ci_container_entrypoint.sh
+++ kurento_ci_container_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[[ -n "$1" ]] || { echo "No script to run specified. Need one to run after preparing the environment"; exit 1 }
+[ -n "$1" ] || { echo "No script to run specified. Need one to run after preparing the environment"; exit 1; }
 BUILD_COMMAND=$1
 
 PATH=$PATH:$(realpath $(dirname "$0"))
@@ -11,9 +11,10 @@ if [ -n "$KEY" ]; then
   echo "Add private key to ssh agent"
   eval $(ssh-agent -s)
   ssh-add $KEY
+  ls -la /opt
 fi
 
-if [ -f /root/.ssh/config ]; then
+if [ -f $SSH_CONFIG ]; then
   echo "Set correct owner & permissions to /root/.ssh/config file"
   chown root:root $SSH_CONFIG
   chmod 600 $SSH_CONFIG
@@ -21,7 +22,6 @@ if [ -f /root/.ssh/config ]; then
   chmod 600 $KEY
   ls -la /root
   ls -la /root/.ssh
-  ls -la /opt
 fi
 
 echo "Running command $BUILD_COMMAND"
