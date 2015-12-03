54deff5e122bc849ade6846a30b0b9646b6df265
patxi <patxi.gortazar@gmail.com>
2015-12-03 10:50:32 +0100

    kurento_api_client_js_W_node_restart: Add kms port
    Change-Id: Ib6291f0b5a0f83aa8c50605e97f2d4fa468f1376

--- kurento_api_client_js_W_node_restart_audit.sh
+++ kurento_api_client_js_W_node_restart_audit.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z KMS_PORT ]
+then
+  echo "Need a KMS_PORT env variable with a kms port to connect to"
+  exit 1
+fi
+
 # Prepare js project
 echo "Preparing js project"
 npm install
@@ -7,4 +13,4 @@ npm install
 echo "Running tests"
 rm -f node_modules/kurento-client && ln -s .. node_modules/kurento-client
 cd test_reconnect
-node index.js --scope=docker
+node index.js --scope=docker --ws_port=$KMS_PORT
