6a0df6a1bc876a02ab74bbe381489f841148d87f
patxi <patxi.gortazar@gmail.com>
2015-12-03 16:09:39 +0100

    kurento_container_install_deb_dependencies: remove postpone
    Change-Id: Ic59863fa54b27aee80d3625ee9ab23a7229b9f67

--- kurento_container_install_deb_dependencies.sh
+++ kurento_container_install_deb_dependencies.sh
@@ -5,14 +5,8 @@ if [ ! -s debian/control ] ; then
   exit 1
 fi
 
-if [ $(dpkg -l | grep postpone | wc -l) -lt 1 ]
-then
-  DEBIAN_FRONTEND=noninteractive apt-get update
-  DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y postpone
-fi
-
-DEBIAN_FRONTEND=noninteractive postpone -d -f apt-get update || echo "Error updating packages"
+DEBIAN_FRONTEND=noninteractive apt-get update
 
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
-DEBIAN_FRONTEND=noninteractive postpone -d -f apt-get install --force-yes -y $pkgs || exit 1
+DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y $pkgs || exit 1
