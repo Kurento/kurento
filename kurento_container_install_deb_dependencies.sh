9d40e8b4801b4540a2ede1563e22a72221951cec
patxi <patxi.gortazar@gmail.com>
2015-12-03 16:30:16 +0100

    kurento_container_install_deb_dependencies: try to fix broken packages
    Change-Id: Ic22bf2804a96f64b0c3fe3af4933344e32cf1648

--- kurento_container_install_deb_dependencies.sh
+++ kurento_container_install_deb_dependencies.sh
@@ -10,4 +10,4 @@ DEBIAN_FRONTEND=noninteractive apt-get update
 
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
-DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y $pkgs || exit 1
+DEBIAN_FRONTEND=noninteractive apt-get install --force-yes --fix-broken -y $pkgs || exit 1
