c8b7bda74a12a779d9925d6a97e4cdd10ef74c36
patxi <patxi.gortazar@gmail.com>
2015-12-14 13:36:07 +0100

    kurento_generate_java_module: sign artifacts when project public & release
    Change-Id: I9c52759a3dac3ff3d7045923dbfa88fb543b2397

--- kurento_generate_java_module.sh
+++ kurento_generate_java_module.sh
@@ -1,10 +1,5 @@
 #!/bin/bash -x
 
-if [ -z "$FULL_RELEASE" ];
-then
-  [ -n $1 ] && FULL_RELEASE=$1 || FULL_RELEASE=1
-fi
-
 if [ -z "$KURENTO_PUBLIC_PROJECT" ];
 then
   [ -n $2 ] && PUBLIC=$2 || PUBLIC=no
@@ -22,7 +17,7 @@ PROJECT_VERSION=`kurento_get_version.sh`
 
 OPTS="package"
 if [[ ${PROJECT_VERSION} != *-SNAPSHOT ]]; then
-  [ $FULL_RELEASE -eq 1 ] && OPTS="$OPTS javadoc:jar source:jar gpg:sign"
+  [ "$PUBLIC" == "yes" ] && OPTS="$OPTS javadoc:jar source:jar gpg:sign"
 fi
 OPTS="$OPTS org.apache.maven.plugins:maven-deploy-plugin:2.8:deploy -Dmaven.test.skip=true -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -U"
 
@@ -31,12 +26,12 @@ echo $OPTS
 if [[ ${PROJECT_VERSION} != *-SNAPSHOT ]]; then
   echo "Deploying release version to ${MAVEN_KURENTO_RELEASES} and ${MAVEN_SONATYPE_NEXUS_STAGING}"
   echo "Deploying with options $OPTS"
-  mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_RELEASES} || exit 1
+  mvn --batch-mode --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_RELEASES} || exit 1
   if [ "$PUBLIC" == "yes" ]
   then
-    mvn --settings ${MAVEN_SETTINGS} deploy $OPTS -DaltDeploymentRepository=${MAVEN_SONATYPE_NEXUS_STAGING} || exit 1
+    mvn --batch-mode --settings ${MAVEN_SETTINGS} deploy $OPTS -DaltDeploymentRepository=${MAVEN_SONATYPE_NEXUS_STAGING} || exit 1
   fi
 else
   echo "Deploying snapshot version to ${MAVEN_KURENTO_SNAPSHOTS}"
-  mvn --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} -DaltSnapshotDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} || exit 1
+  mvn --batch-mode --settings ${MAVEN_SETTINGS} clean deploy $OPTS -DaltDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} -DaltSnapshotDeploymentRepository=${MAVEN_KURENTO_SNAPSHOTS} || exit 1
 fi
