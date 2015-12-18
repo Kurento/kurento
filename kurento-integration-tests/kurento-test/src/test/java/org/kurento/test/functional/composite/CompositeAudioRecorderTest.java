49e63edbc3a87a82d2acfd0687201e44de26c912
mgallego <micael.gallego@gmail.com>
2015-12-18 16:32:00 +0100

    Fix compilation error in Composite tests
    Change-Id: I773bd1ba87b84d4888111ef15ea0a2e8bd4c754a

--- kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/composite/CompositeAudioRecorderTest.java
+++ kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/composite/CompositeAudioRecorderTest.java
@@ -17,10 +17,9 @@ import org.kurento.test.config.TestScenario;
 import org.kurento.test.functional.recorder.BaseRecorder;
 
 /**
- * Four synthetic videos are played by four PlayerEndpoint and mixed by a
- * Composite. Only audio is connected for three players. The resulting video is
- * recording using a RecorderEndpoint. The recorded video is played using a
- * PlayerEndpoint.<br/>
+ * Four synthetic videos are played by four PlayerEndpoint and mixed by a Composite. Only audio is
+ * connected for three players. The resulting video is recording using a RecorderEndpoint. The
+ * recorded video is played using a PlayerEndpoint.<br/>
  * 
  * Media Pipeline(s): <br>
  * · 4xPlayerEndpoint -> Composite -> RecorderEndpoint <br>
@@ -31,11 +30,9 @@ import org.kurento.test.functional.recorder.BaseRecorder;
  * · Firefox <br>
  * 
  * Test logic: <br>
- * 1. (KMS) Media server mix the media from 4 PlayerEndpoints and it records the
- * grid using the RecorderEndpoint. Only red color (video from first player) and
- * audio from four players.<br>
- * 2. (KMS) Media server implements a player to reproduce the video recorded in
- * step 1. <br>
+ * 1. (KMS) Media server mix the media from 4 PlayerEndpoints and it records the grid using the
+ * RecorderEndpoint. Only red color (video from first player) and audio from four players.<br>
+ * 2. (KMS) Media server implements a player to reproduce the video recorded in step 1. <br>
  * 3. (Browser) WebRtcPeer in rcv-only receives media <br>
  * 
  * Main assertion(s): <br>
@@ -50,80 +47,75 @@ import org.kurento.test.functional.recorder.BaseRecorder;
 
 public class CompositeAudioRecorderTest extends BaseRecorder {
 
-	private static int RECORDTIME = 20; // seconds
-	private static int PLAYTIME = 20; // seconds
+  private static int RECORDTIME = 20; // seconds
+  private static int PLAYTIME = 20; // seconds
 
-	public CompositeAudioRecorderTest(TestScenario testScenario) {
-		super(testScenario);
-	}
+  @Parameters(name = "{index}: {0}")
+  public static Collection<Object[]> data() {
+    return TestScenario.localChromeAndFirefox();
+  }
 
-	@Parameters(name = "{index}: {0}")
-	public static Collection<Object[]> data() {
-		return TestScenario.localChromeAndFirefox();
-	}
+  @Test
+  public void testCompositeRecorder() throws Exception {
+    // MediaPipeline
+    MediaPipeline mp = kurentoClient.createMediaPipeline();
 
-	@Test
-	public void testCompositeRecorder() throws Exception {
-		// MediaPipeline
-		MediaPipeline mp = kurentoClient.createMediaPipeline();
+    PlayerEndpoint playerRed = new PlayerEndpoint.Builder(mp,
+        "http://files.kurento.org/video/30sec/red.webm").build();
+    PlayerEndpoint playerGreen = new PlayerEndpoint.Builder(mp,
+        "http://files.kurento.org/video/30sec/green.webm").build();
+    PlayerEndpoint playerBlue = new PlayerEndpoint.Builder(mp,
+        "http://files.kurento.org/video/30sec/blue.webm").build();
+    PlayerEndpoint playerWhite = new PlayerEndpoint.Builder(mp,
+        "http://files.kurento.org/video/30sec/white.webm").build();
 
-		PlayerEndpoint playerRed = new PlayerEndpoint.Builder(mp,
-				"http://files.kurento.org/video/30sec/red.webm").build();
-		PlayerEndpoint playerGreen = new PlayerEndpoint.Builder(mp,
-				"http://files.kurento.org/video/30sec/green.webm").build();
-		PlayerEndpoint playerBlue = new PlayerEndpoint.Builder(mp,
-				"http://files.kurento.org/video/30sec/blue.webm").build();
-		PlayerEndpoint playerWhite = new PlayerEndpoint.Builder(mp,
-				"http://files.kurento.org/video/30sec/white.webm").build();
+    Composite composite = new Composite.Builder(mp).build();
+    HubPort hubPort1 = new HubPort.Builder(composite).build();
+    HubPort hubPort2 = new HubPort.Builder(composite).build();
+    HubPort hubPort3 = new HubPort.Builder(composite).build();
+    HubPort hubPort4 = new HubPort.Builder(composite).build();
+    HubPort hubPort5 = new HubPort.Builder(composite).build();
+    String recordingFile = getDefaultOutputFile(EXTENSION_WEBM);
+    RecorderEndpoint recorderEP = new RecorderEndpoint.Builder(mp, Protocol.FILE + recordingFile)
+        .build();
 
-		Composite composite = new Composite.Builder(mp).build();
-		HubPort hubPort1 = new HubPort.Builder(composite).build();
-		HubPort hubPort2 = new HubPort.Builder(composite).build();
-		HubPort hubPort3 = new HubPort.Builder(composite).build();
-		HubPort hubPort4 = new HubPort.Builder(composite).build();
-		HubPort hubPort5 = new HubPort.Builder(composite).build();
-		String recordingFile = getDefaultOutputFile(EXTENSION_WEBM);
-		RecorderEndpoint recorderEP = new RecorderEndpoint.Builder(mp,
-				Protocol.FILE + recordingFile).build();
+    playerRed.connect(hubPort1);
+    playerGreen.connect(hubPort2, MediaType.AUDIO);
+    playerBlue.connect(hubPort3, MediaType.AUDIO);
+    playerWhite.connect(hubPort4, MediaType.AUDIO);
 
-		playerRed.connect(hubPort1);
-		playerGreen.connect(hubPort2, MediaType.AUDIO);
-		playerBlue.connect(hubPort3, MediaType.AUDIO);
-		playerWhite.connect(hubPort4, MediaType.AUDIO);
+    hubPort5.connect(recorderEP);
 
-		hubPort5.connect(recorderEP);
+    playerRed.play();
+    playerGreen.play();
+    playerBlue.play();
+    playerWhite.play();
 
-		playerRed.play();
-		playerGreen.play();
-		playerBlue.play();
-		playerWhite.play();
+    recorderEP.record();
 
-		recorderEP.record();
+    Thread.sleep(RECORDTIME * 1000);
 
-		Thread.sleep(RECORDTIME * 1000);
+    recorderEP.stop();
 
-		recorderEP.stop();
+    playerRed.stop();
+    playerGreen.stop();
+    playerBlue.stop();
+    playerWhite.stop();
 
-		playerRed.stop();
-		playerGreen.stop();
-		playerBlue.stop();
-		playerWhite.stop();
+    // Media Pipeline #2
+    MediaPipeline mp2 = kurentoClient.createMediaPipeline();
+    PlayerEndpoint playerEP2 = new PlayerEndpoint.Builder(mp2, Protocol.FILE + recordingFile)
+        .build();
+    WebRtcEndpoint webRtcEP2 = new WebRtcEndpoint.Builder(mp2).build();
+    playerEP2.connect(webRtcEP2);
 
-		// Media Pipeline #2
-		MediaPipeline mp2 = kurentoClient.createMediaPipeline();
-		PlayerEndpoint playerEP2 = new PlayerEndpoint.Builder(mp2,
-				Protocol.FILE + recordingFile).build();
-		WebRtcEndpoint webRtcEP2 = new WebRtcEndpoint.Builder(mp2).build();
-		playerEP2.connect(webRtcEP2);
+    // Playing the recording
+    launchBrowser(mp, webRtcEP2, playerEP2, null, EXPECTED_VIDEO_CODEC_WEBM,
+        EXPECTED_AUDIO_CODEC_WEBM, recordingFile, Color.RED, 0, 0, PLAYTIME);
 
-		// Playing the recording
-		launchBrowser(mp, webRtcEP2, playerEP2, null,
-				EXPECTED_VIDEO_CODEC_WEBM, EXPECTED_AUDIO_CODEC_WEBM,
-				recordingFile, Color.RED, 0, 0, PLAYTIME);
+    // Release Media Pipeline #2
+    mp2.release();
 
-		// Release Media Pipeline #2
-		mp2.release();
-
-		success = true;
-	}
+    success = true;
+  }
 }
--- kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/composite/CompositeWebRtcRecorderTest.java
+++ kurento-integration-tests/kurento-test/src/test/java/org/kurento/test/functional/composite/CompositeWebRtcRecorderTest.java
@@ -24,10 +24,9 @@ import org.kurento.test.config.TestScenario;
 import org.kurento.test.functional.recorder.BaseRecorder;
 
 /**
- * Four synthetic videos are played by four WebRtcEndpoint (Connect only audio
- * for thres videos) and mixed by a Composite. The resulting video is recording
- * using a RecorderEndpoint. The recorded video is played using a
- * PlayerEndpoint.<br/>
+ * Four synthetic videos are played by four WebRtcEndpoint (Connect only audio for thres videos) and
+ * mixed by a Composite. The resulting video is recording using a RecorderEndpoint. The recorded
+ * video is played using a PlayerEndpoint.<br/>
  * 
  * Media Pipeline(s): <br>
  * · 4xWebRtcEndpoint -> Composite -> RecorderEndpoint <br>
@@ -37,11 +36,9 @@ import org.kurento.test.functional.recorder.BaseRecorder;
  * · Chrome <br>
  * 
  * Test logic: <br>
- * 1. (KMS) Media server mix the media from 4 WebRtcEndpoint and it records the
- * grid using the RecorderEndpoint. Only red color (video from first player) and
- * audio from four players.<br>
- * 2. (KMS) Media server implements a player to reproduce the video recorded in
- * step 1. <br>
+ * 1. (KMS) Media server mix the media from 4 WebRtcEndpoint and it records the grid using the
+ * RecorderEndpoint. Only red color (video from first player) and audio from four players.<br>
+ * 2. (KMS) Media server implements a player to reproduce the video recorded in step 1. <br>
  * 3. (Browser) WebRtcPeer in rcv-only receives media <br>
  * 
  * Main assertion(s): <br>
@@ -56,119 +53,94 @@ import org.kurento.test.functional.recorder.BaseRecorder;
 
 public class CompositeWebRtcRecorderTest extends BaseRecorder {
 
-	private static int RECORDTIME = 20; // seconds
-	private static int PLAYTIME = 10; // seconds
+  private static int PLAYTIME = 10; // seconds
 
-	private static final String BROWSER1 = "browser1";
-	private static final String BROWSER2 = "browser2";
-	private static final String BROWSER3 = "browser3";
-	private static final String BROWSER4 = "browser4";
-	private static final String BROWSER5 = "browser5";
+  private static final String BROWSER1 = "browser1";
+  private static final String BROWSER2 = "browser2";
+  private static final String BROWSER3 = "browser3";
+  private static final String BROWSER4 = "browser4";
+  private static final String BROWSER5 = "browser5";
 
-	public CompositeWebRtcRecorderTest(TestScenario testScenario) {
-		super(testScenario);
-	}
+  @Parameters(name = "{index}: {0}")
+  public static Collection<Object[]> data() {
+    // Test: 5 local Chrome's
+    TestScenario test = new TestScenario();
+    test.addBrowser(BROWSER1, new Browser.Builder().browserType(BrowserType.CHROME)
+        .webPageType(WebPageType.WEBRTC).scope(BrowserScope.LOCAL).build());
+    test.addBrowser(BROWSER2,
+        new Browser.Builder().browserType(BrowserType.CHROME).webPageType(WebPageType.WEBRTC)
+            .scope(BrowserScope.LOCAL).video(getTestFilesPath() + "/video/10sec/red.y4m").build());
+    test.addBrowser(BROWSER3,
+        new Browser.Builder().browserType(BrowserType.CHROME).webPageType(WebPageType.WEBRTC)
+            .scope(BrowserScope.LOCAL).video(getTestFilesPath() + "/video/10sec/green.y4m")
+            .build());
+    test.addBrowser(BROWSER4,
+        new Browser.Builder().browserType(BrowserType.CHROME).webPageType(WebPageType.WEBRTC)
+            .scope(BrowserScope.LOCAL).video(getTestFilesPath() + "/video/10sec/blue.y4m").build());
+    test.addBrowser(BROWSER5,
+        new Browser.Builder().browserType(BrowserType.CHROME).webPageType(WebPageType.WEBRTC)
+            .scope(BrowserScope.LOCAL).video(getTestFilesPath() + "/video/10sec/white.y4m")
+            .build());
+    return Arrays.asList(new Object[][] { { test } });
+  }
 
-	@Parameters(name = "{index}: {0}")
-	public static Collection<Object[]> data() {
-		// Test: 5 local Chrome's
-		TestScenario test = new TestScenario();
-		test.addBrowser(
-				BROWSER1,
-				new Browser.Builder().browserType(BrowserType.CHROME)
-						.webPageType(WebPageType.WEBRTC)
-						.scope(BrowserScope.LOCAL).build());
-		test.addBrowser(
-				BROWSER2,
-				new Browser.Builder().browserType(BrowserType.CHROME)
-						.webPageType(WebPageType.WEBRTC)
-						.scope(BrowserScope.LOCAL)
-						.video(getPathTestFiles() + "/video/10sec/red.y4m")
-						.build());
-		test.addBrowser(
-				BROWSER3,
-				new Browser.Builder().browserType(BrowserType.CHROME)
-						.webPageType(WebPageType.WEBRTC)
-						.scope(BrowserScope.LOCAL)
-						.video(getPathTestFiles() + "/video/10sec/green.y4m")
-						.build());
-		test.addBrowser(
-				BROWSER4,
-				new Browser.Builder().browserType(BrowserType.CHROME)
-						.webPageType(WebPageType.WEBRTC)
-						.scope(BrowserScope.LOCAL)
-						.video(getPathTestFiles() + "/video/10sec/blue.y4m")
-						.build());
-		test.addBrowser(
-				BROWSER5,
-				new Browser.Builder().browserType(BrowserType.CHROME)
-						.webPageType(WebPageType.WEBRTC)
-						.scope(BrowserScope.LOCAL)
-						.video(getPathTestFiles() + "/video/10sec/white.y4m")
-						.build());
-		return Arrays.asList(new Object[][] { { test } });
-	}
+  @Test
+  public void testCompositeRecorder() throws Exception {
 
-	@Test
-	public void testCompositeRecorder() throws Exception {
+    // MediaPipeline
+    MediaPipeline mp = kurentoClient.createMediaPipeline();
 
-		// MediaPipeline
-		MediaPipeline mp = kurentoClient.createMediaPipeline();
+    WebRtcEndpoint webRtcEPRed = new WebRtcEndpoint.Builder(mp).build();
+    WebRtcEndpoint webRtcEPGreen = new WebRtcEndpoint.Builder(mp).build();
+    WebRtcEndpoint webRtcEPBlue = new WebRtcEndpoint.Builder(mp).build();
+    WebRtcEndpoint webRtcEPWhite = new WebRtcEndpoint.Builder(mp).build();
 
-		WebRtcEndpoint webRtcEPRed = new WebRtcEndpoint.Builder(mp).build();
-		WebRtcEndpoint webRtcEPGreen = new WebRtcEndpoint.Builder(mp).build();
-		WebRtcEndpoint webRtcEPBlue = new WebRtcEndpoint.Builder(mp).build();
-		WebRtcEndpoint webRtcEPWhite = new WebRtcEndpoint.Builder(mp).build();
+    Composite composite = new Composite.Builder(mp).build();
+    HubPort hubPort1 = new HubPort.Builder(composite).build();
+    HubPort hubPort2 = new HubPort.Builder(composite).build();
+    HubPort hubPort3 = new HubPort.Builder(composite).build();
+    HubPort hubPort4 = new HubPort.Builder(composite).build();
+    HubPort hubPort5 = new HubPort.Builder(composite).build();
+    String recordingFile = getDefaultOutputFile(EXTENSION_WEBM);
+    RecorderEndpoint recorderEP = new RecorderEndpoint.Builder(mp, Protocol.FILE + recordingFile)
+        .build();
 
-		Composite composite = new Composite.Builder(mp).build();
-		HubPort hubPort1 = new HubPort.Builder(composite).build();
-		HubPort hubPort2 = new HubPort.Builder(composite).build();
-		HubPort hubPort3 = new HubPort.Builder(composite).build();
-		HubPort hubPort4 = new HubPort.Builder(composite).build();
-		HubPort hubPort5 = new HubPort.Builder(composite).build();
-		String recordingFile = getDefaultOutputFile(EXTENSION_WEBM);
-		RecorderEndpoint recorderEP = new RecorderEndpoint.Builder(mp,
-				Protocol.FILE + recordingFile).build();
+    webRtcEPRed.connect(hubPort1);
+    webRtcEPGreen.connect(hubPort2, MediaType.AUDIO);
+    webRtcEPBlue.connect(hubPort3, MediaType.AUDIO);
+    webRtcEPWhite.connect(hubPort4, MediaType.AUDIO);
 
-		webRtcEPRed.connect(hubPort1);
-		webRtcEPGreen.connect(hubPort2, MediaType.AUDIO);
-		webRtcEPBlue.connect(hubPort3, MediaType.AUDIO);
-		webRtcEPWhite.connect(hubPort4, MediaType.AUDIO);
+    hubPort5.connect(recorderEP);
 
-		hubPort5.connect(recorderEP);
+    // WebRTC browsers
+    getPage(BROWSER2).initWebRtc(webRtcEPRed, WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+    getPage(BROWSER3).initWebRtc(webRtcEPGreen, WebRtcChannel.AUDIO_AND_VIDEO,
+        WebRtcMode.SEND_ONLY);
+    getPage(BROWSER4).initWebRtc(webRtcEPBlue, WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+    getPage(BROWSER5).initWebRtc(webRtcEPWhite, WebRtcChannel.AUDIO_AND_VIDEO,
+        WebRtcMode.SEND_ONLY);
 
-		// WebRTC browsers
-		getPage(BROWSER2).initWebRtc(webRtcEPRed,
-				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
-		getPage(BROWSER3).initWebRtc(webRtcEPGreen,
-				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
-		getPage(BROWSER4).initWebRtc(webRtcEPBlue,
-				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
-		getPage(BROWSER5).initWebRtc(webRtcEPWhite,
-				WebRtcChannel.AUDIO_AND_VIDEO, WebRtcMode.SEND_ONLY);
+    recorderEP.record();
 
-		recorderEP.record();
+    Thread.sleep(PLAYTIME * 1000);
 
-		Thread.sleep(PLAYTIME * 1000);
+    recorderEP.stop();
+    mp.release();
 
-		recorderEP.stop();
-		mp.release();
+    // Media Pipeline #2
+    MediaPipeline mp2 = kurentoClient.createMediaPipeline();
+    PlayerEndpoint playerEP2 = new PlayerEndpoint.Builder(mp2, Protocol.FILE + recordingFile)
+        .build();
+    WebRtcEndpoint webRtcEP2 = new WebRtcEndpoint.Builder(mp2).build();
+    playerEP2.connect(webRtcEP2);
 
-		// Media Pipeline #2
-		MediaPipeline mp2 = kurentoClient.createMediaPipeline();
-		PlayerEndpoint playerEP2 = new PlayerEndpoint.Builder(mp2,
-				Protocol.FILE + recordingFile).build();
-		WebRtcEndpoint webRtcEP2 = new WebRtcEndpoint.Builder(mp2).build();
-		playerEP2.connect(webRtcEP2);
+    // Playing the recorded file
+    launchBrowser(mp2, webRtcEP2, playerEP2, null, EXPECTED_VIDEO_CODEC_WEBM,
+        EXPECTED_AUDIO_CODEC_WEBM, recordingFile, Color.RED, 0, 0, PLAYTIME);
 
-		// Playing the recorded file
-		launchBrowser(mp2, webRtcEP2, playerEP2, null,
-				EXPECTED_VIDEO_CODEC_WEBM, EXPECTED_AUDIO_CODEC_WEBM,
-				recordingFile, Color.RED, 0, 0, PLAYTIME);
+    // Release Media Pipeline #2
+    mp2.release();
 
-		// Release Media Pipeline #2
-		mp2.release();
-
-		success = true;
-	}
+    success = true;
+  }
 }
