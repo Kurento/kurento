bbdd8053dd1c349b00b1c1c716eb7177ba002609
patxi <patxi.gortazar@gmail.com>
2016-01-29 16:52:14 +0100

    [install_deb_dependencies] Allow extra packages to be defined
    Change-Id: I4311c491a3ec5847f2d1257cbde07b7c95fcd12f

--- kurento_container_install_deb_dependencies.sh
+++ kurento_container_install_deb_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ ! -s debian/control ] ; then
   echo "Warning, no debian/control file found"
@@ -12,3 +12,7 @@ DEBIAN_FRONTEND=noninteractive apt-get update
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
 DEBIAN_FRONTEND=noninteractive apt-get install --force-yes --fix-broken -y $pkgs || exit 1
+
+if [ "${EXTRA_PACKAGES}x" != "x" ]; then
+  DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y $EXTRA_PACKAGES || exit 1
+fi
