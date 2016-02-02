4f103f5a235fb5caeff90d961bd8d83141112a0a
patxi <patxi.gortazar@gmail.com>
2016-02-02 08:01:00 +0100

    [kurento_container_install_deb_dependencies] Change ubuntu.kurento.org for ubuntuci.kurento.org
    Change-Id: I156b8b2b3780ccb517b798bd2e0085fefa815c15

--- kurento_container_install_deb_dependencies.sh
+++ kurento_container_install_deb_dependencies.sh
@@ -5,8 +5,8 @@ if [ ! -s debian/control ] ; then
   exit 1
 fi
 
-echo "deb http://ubuntu.kurento.org trusty-dev kms6" | tee /etc/apt/sources.list.d/kurento.list
-wget -O - http://ubuntu.kurento.org/kurento.gpg.key | apt-key add -
+echo "deb http://ubuntuci.kurento.org trusty-dev kms6" | tee /etc/apt/sources.list.d/kurento.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv 2F819BC0
 DEBIAN_FRONTEND=noninteractive apt-get update
 
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
