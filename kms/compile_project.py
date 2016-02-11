fca42d0f1da28d2527e834ccb30d784c7657e196
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2016-02-11 17:15:12 +0100

    compile_project: Print error correctly
    Change-Id: I04316749a192cc935d204104e8b4ac7b65886c46

--- kms/compile_project.py
+++ kms/compile_project.py
@@ -288,7 +288,7 @@ def compile_project(args):
 
         config = yaml.load(f)
     except:
-        print("Config file not found: " + sys.exc_info()[0])
+        print("Config file not found: " + str (sys.exc_info()[0]))
         exit(1)
 
     cache = Cache()
