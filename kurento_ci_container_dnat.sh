7f6b940dfae2d188bba97de0b804ecd052422e23
patxi <patxi.gortazar@gmail.com>
2016-02-25 18:13:49 +0100

    [dnat] Change eth1 name to avoid collisions
    Change-Id: I4126aeca4b6de111e13a40180a625834142536c3

--- kurento_ci_container_dnat.sh
+++ kurento_ci_container_dnat.sh
@@ -108,9 +108,9 @@ ip link set A_${short} up
 # We rename it to eth1 because there is already an eth0 with a 192.168 ip
 
 ip link set B_${short} netns $pid
-ip netns exec $pid ip link set dev B_${short} name eth1
-ip netns exec $pid ip link set eth1 up
-ip netns exec $pid ip addr add ${ip}/16 dev eth1
+ip netns exec $pid ip link set dev B_${short} name eth1${pid}
+ip netns exec $pid ip link set eth1${pid} up
+ip netns exec $pid ip addr add ${ip}/16 dev eth1${pid}
 ip netns exec $pid ip route add default via 172.17.0.1
 
 fi
