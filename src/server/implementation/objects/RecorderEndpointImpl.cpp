f05dcc5b2f29da72b64656fad0a2eabaf9999e67
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2016-03-10 16:27:21 +0100

    RecordingEndpointImpl: Log state changes
    Change-Id: Iad73796ed6648f921676438ff9b7e16b8c0a49cd

--- src/server/implementation/objects/RecorderEndpointImpl.cpp
+++ src/server/implementation/objects/RecorderEndpointImpl.cpp
@@ -126,18 +126,21 @@ RecorderEndpointImpl::onStateChanged (gint newState)
 {
   switch (newState) {
   case KMS_URI_END_POINT_STATE_STOP: {
+    GST_DEBUG_OBJECT (element, "State changed to Stopped");
     Stopped event (shared_from_this(), Stopped::getName() );
     signalStopped (event);
     break;
   }
 
   case KMS_URI_END_POINT_STATE_START: {
+    GST_DEBUG_OBJECT (element, "State changed to Recording");
     Recording event (shared_from_this(), Recording::getName() );
     signalRecording (event);
     break;
   }
 
   case KMS_URI_END_POINT_STATE_PAUSE: {
+    GST_DEBUG_OBJECT (element, "State changed to Paused");
     Paused event (shared_from_this(), Paused::getName() );
     signalPaused (event);
     break;
