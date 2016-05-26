34afa15c31b57046ae6ccf718dfb3feddd34abf5
Raúl Benítez Mejías <raulbenitezmejias@gmail.com>
2016-05-26 21:58:07 +0200

    Add new getter for qualifiedName
    Change-Id: I2a68a1c5f4bbc2f0cad4f92e47b0d1a062cd45ed

--- src/main/java/org/kurento/modulecreator/definition/Type.java
+++ src/main/java/org/kurento/modulecreator/definition/Type.java
@@ -16,4 +16,21 @@ public abstract class Type extends NamedElement {
     this.module = module;
   }
 
+  /**
+   * Get qualified name mixing In the format module.name
+   *
+   * @return The qualified name
+   */
+  public String getQualifiedName() {
+    if (this.module == null || this.module.getName() == null) {
+      return name;
+    }
+    if (this.module.getName().equals("core") || this.module.getName().equals("filters")
+        || this.module.getName().equals("elements")) {
+      return "kurento." + name;
+    } else {
+      return this.module.getName() + "." + name;
+    }
+  }
+
 }
