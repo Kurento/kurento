fa14f215e57dcda2db4b93c3db6c118e297341dc
patxi <patxi.gortazar@gmail.com>
2016-07-18 15:57:04 +0200

    [kurento_ci_container_entrypoint] Remove unused lines
    Change-Id: I42421639674a84fb728ca42809ef627a5553ec5d

--- kurento_ci_container_entrypoint.sh
+++ kurento_ci_container_entrypoint.sh
@@ -45,11 +45,6 @@ if [ "$DIST" = "xenial" ]; then
   apt-get install -y --allow-change-held-packages kurento-adm-scripts
 fi
 
-RUN mkdir -p /root/.ssh \
-  && echo "Host code.kurento.org" >> /root/.ssh/config \
-  && echo "  KexAlgorithms +diffie-hellman-group1-sha1" >> /root/.ssh/config
-
-
 # Configure Kurento gnupg
 if [ -f "$GNUPG_KEY" ]; then
   gpg --import $GNUPG_KEY
