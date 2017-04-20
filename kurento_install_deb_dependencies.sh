1ffc6fbb9108aebd2159d9078dab47381490ca52
Fede Diaz <nordri@gmail.com>
2017-04-20 11:45:14 +0200

    Installing python packages

--- kurento_install_deb_dependencies.sh
+++ kurento_install_deb_dependencies.sh
@@ -7,6 +7,10 @@ fi
 
 DEBIAN_FRONTEND=noninteractive apt-get update || echo "Error updating packages"
 
+# Python needs those packages
+echo "Intalling python packages..."
+DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y python-git python-requests python-apt python-debian python-yaml
+
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
 DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y $pkgs || exit 1
