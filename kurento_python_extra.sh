237aee256442c575fd5d34fc6bb7a0349a5f3611
Fede Diaz <nordri@gmail.com>
2017-04-20 12:58:35 +0200

    add sudo issue

--- kurento_python_extra.sh
+++ kurento_python_extra.sh
@@ -4,3 +4,5 @@
 echo "Intalling python packages..."
 DEBIAN_FRONTEND=noninteractive apt-get install -y python-git python-requests python-apt python-debian python-yaml
 
+# Temp solution to sudo issue
+DEBIAN_FRONTEND=noninteractive apt-get install -y sudo
