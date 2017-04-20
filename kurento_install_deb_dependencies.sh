bef75451300b33e84cea6f5d662fbd42a4e5c2d3
Fede Diaz <nordri@gmail.com>
2017-04-20 14:17:43 +0200

    Putting all python dependencies togheter

--- kurento_install_deb_dependencies.sh
+++ kurento_install_deb_dependencies.sh
@@ -7,13 +7,6 @@ fi
 
 DEBIAN_FRONTEND=noninteractive apt-get update || echo "Error updating packages"
 
-# Python needs those packages
-echo "Intalling python packages..."
-DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y python-git python-requests python-apt python-debian python-yaml
-
-echo "Installing sudo..."
-DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y sudo
-
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
 DEBIAN_FRONTEND=noninteractive apt-get install --force-yes -y $pkgs || exit 1
--- kurento_python_extra.sh
+++ kurento_python_extra.sh
@@ -2,7 +2,7 @@
 
 # Python needs those packages
 echo "Intalling python packages..."
-DEBIAN_FRONTEND=noninteractive apt-get install -y python-git python-requests python-apt python-debian python-yaml
+DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends python-git python-requests python-apt python-debian python-yaml
 
 # Temp solution to sudo issue
-DEBIAN_FRONTEND=noninteractive apt-get install -y sudo
+DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends sudo
