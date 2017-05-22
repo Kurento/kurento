3f50795bba3d1fe090c659124dbd3b8b39e75fb2
Fede Diaz <nordri@gmail.com>
2017-05-22 17:55:13 +0200

    undoing debugging

--- kurento_mvn.sh
+++ kurento_mvn.sh
@@ -48,7 +48,7 @@ MAVEN_OPTIONS="$MAVEN_OPTIONS -DfailIfNoTests=false"
 if [ ! -f ignore ]; then
   export AWS_ACCESS_KEY_ID=$UBUNTU_PRIV_S3_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY=$UBUNTU_PRIV_S3_SECRET_ACCESS_KEY_ID
-  mvn --fail-at-end -X --batch-mode $PARAM_MAVEN_SETTINGS $PARAM_PL clean $MAVEN_GOALS $MAVEN_OPTIONS
+  mvn --fail-at-end --batch-mode $PARAM_MAVEN_SETTINGS $PARAM_PL clean $MAVEN_GOALS $MAVEN_OPTIONS
 else
   exit 0
 fi
