6edefeac8d7cc15817fa7ea535471bf4c4121058
Boni Garcia <bgarcia@gsyc.es>
2017-06-12 17:07:20 +0200

    Revert "Remove use of Preconditions in ConfigFilePropertyHolder"
    This reverts commit 9b162e47a36a1b5e8e03eea3030ee1173df7d7fa.

--- kurento-commons/src/main/java/org/kurento/commons/ConfigFilePropertyHolder.java
+++ kurento-commons/src/main/java/org/kurento/commons/ConfigFilePropertyHolder.java
@@ -30,6 +30,7 @@ import org.kurento.commons.PropertiesManager.PropertyHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
@@ -79,9 +80,7 @@ public class ConfigFilePropertyHolder implements PropertyHolder {
     lastLoadedConfigFilePath = configFilePath;
 
     // FIXME shouldn't this be the first sentence in the method?
-    if (configFilePath == null) {
-        throw new AssertionError("configFilePath paramter must be not null.");
-    }
+    Preconditions.checkNotNull(configFilePath, "configFilePath paramter must be not null.");
 
     log.debug("Using configuration file in path '{}' ({})", configFilePath,
         configFilePath.getClass().getCanonicalName());
