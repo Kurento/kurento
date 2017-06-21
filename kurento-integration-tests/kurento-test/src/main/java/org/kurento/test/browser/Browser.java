3a31335121a514272da35f9952fa2307c5202d2c
Boni Garcia <bgarcia@gsyc.es>
2017-06-21 17:32:25 +0200

    Set acceptInsecureCerts in Firefox capabilities

--- kurento-integration-tests/kurento-test/src/main/java/org/kurento/test/browser/Browser.java
+++ kurento-integration-tests/kurento-test/src/main/java/org/kurento/test/browser/Browser.java
@@ -88,6 +88,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -361,6 +362,9 @@ public class Browser implements Closeable {
     FirefoxProfile profile = new FirefoxProfile();
     // This flag avoids granting the access to the camera
     profile.setPreference("media.navigator.permission.disabled", true);
+
+    // This allows to load pages with self-signed certificates
+    capabilities.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
 
     capabilities.setCapability(FirefoxDriver.PROFILE, profile);
     capabilities.setBrowserName(DesiredCapabilities.firefox().getBrowserName());
