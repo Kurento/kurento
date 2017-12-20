4425f0531ed7d1d10090860cfaad6aa3dec92ef7
Fede Diaz <nordri@gmail.com>
2017-12-20 12:22:35 +0100

    executing update before running apt-get

--- kurento_python_extra.sh
+++ kurento_python_extra.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-get update
 # Python needs those packages
 echo "Intalling python packages..."
 DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends python-git python-requests python-apt python-debian python-yaml
