93984f9c1ae785e8c1a57521a67a3ada7d258bb2
Juan Navarro <juan.navarro@gmx.es>
2018-01-08 17:49:48 +0100

    CMake: Rename functions in DpkgBuildFlags module
    - Rename from 'get_dpkg_buildflags_c' to 'dpkg_buildflags_get_c'
    - Add function to get LDFLAGS

--- CMake/DpkgBuildFlags.cmake
+++ CMake/DpkgBuildFlags.cmake
@@ -2,17 +2,22 @@
 # DpkgBuildFlags
 # --------------
 #
-# Get the build flags provided by "dpkg-buildflags" during package generation
+# Get the compiler flags provided by `dpkg-buildflags`.
+# The returned flags are those that would be used by the Debian package
+# generation tools, such as `dpkg` and `debhelper`. This allows to make
+# local builds while closely following the compiler options that will be
+# used by automatic builds in the actual generation of Debian packages.
 #
-# get_dpkg_buildflags_c(<var>)
-# get_dpkg_buildflags_cxx(<var>)
+# Functions:
+# - dpkg_buildflags_get_c(<var>)
+# - dpkg_buildflags_get_cxx(<var>)
 #
-# <var> - Variable to store the compiler flags used by the package
-#         creation tools (dpkg and debhelper).
-#         Will be created as an internal cache variable.
+# Arguments:
+# - <var>: Variable where to store the compiler flags returned by
+#   `dpkg-buildflags`. Created as an internal cache variable.
 
 #=============================================================================
-# (C) Copyright 2016 Kurento (http://kurento.org/)
+# (C) Copyright 2018 Kurento (http://kurento.org/)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -27,16 +32,67 @@
 # limitations under the License.
 #=============================================================================
 
-find_program(DBF_EXECUTABLE dpkg-buildflags DOC "dpkg-buildflags program")
-mark_as_advanced(DBF_EXECUTABLE)
+find_program(FLAGS_EXECUTABLE dpkg-buildflags DOC "dpkg-buildflags program")
+mark_as_advanced(FLAGS_EXECUTABLE)
+
+function(dpkg_buildflags_get_cflags var)
+  if(FLAGS_EXECUTABLE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CFLAGS
+        OUTPUT_VARIABLE cflags
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CPPFLAGS
+        OUTPUT_VARIABLE cppflags
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    set(${var} "${cflags} ${cppflags}" CACHE INTERNAL "dpkg-buildflags for C")
+
+    unset(cflags)
+    unset(cppflags)
+  endif()
+endfunction()
+
+function(dpkg_buildflags_get_cxxflags var)
+  if(FLAGS_EXECUTABLE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CXXFLAGS
+        OUTPUT_VARIABLE cxxflags
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CPPFLAGS
+        OUTPUT_VARIABLE cppflags
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    set(${var} "${cxxflags} ${cppflags}" CACHE INTERNAL "dpkg-buildflags for C++")
+
+    unset(cxxflags)
+    unset(cppflags)
+  endif()
+endfunction()
+
+function(dpkg_buildflags_get_ldflags var)
+  if(FLAGS_EXECUTABLE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get LDFLAGS
+        OUTPUT_VARIABLE ldflags
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+    set(${var} "${ldflags}" CACHE INTERNAL "dpkg-buildflags for linker")
+
+    unset(ldflags)
+    unset(dbf_cppflags)
+  endif()
+endfunction()
+
 
 function(get_dpkg_buildflags_c var)
-  if(DBF_EXECUTABLE)
-    execute_process(COMMAND ${DBF_EXECUTABLE} --get CFLAGS
+  if(FLAGS_EXECUTABLE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CFLAGS
         OUTPUT_VARIABLE dbf_flags
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND ${DBF_EXECUTABLE} --get CPPFLAGS
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CPPFLAGS
         OUTPUT_VARIABLE dbf_cppflags
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -49,12 +105,12 @@ function(get_dpkg_buildflags_c var)
 endfunction()
 
 function(get_dpkg_buildflags_cxx var)
-  if(DBF_EXECUTABLE)
-    execute_process(COMMAND ${DBF_EXECUTABLE} --get CXXFLAGS
+  if(FLAGS_EXECUTABLE)
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CXXFLAGS
         OUTPUT_VARIABLE dbf_flags
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE)
-    execute_process(COMMAND ${DBF_EXECUTABLE} --get CPPFLAGS
+    execute_process(COMMAND ${FLAGS_EXECUTABLE} --get CPPFLAGS
         OUTPUT_VARIABLE dbf_cppflags
         ERROR_QUIET
         OUTPUT_STRIP_TRAILING_WHITESPACE)
