7a7eaa1201001689d6813f36a5e41cd20cdc6092
Juan Navarro <juan.navarro@gmx.es>
2018-01-18 15:24:44 +0100

    kurento_merge_doc_project: Tag releases only after successful builds

--- kurento_merge_doc_project.sh
+++ kurento_merge_doc_project.sh
@@ -4,16 +4,15 @@ echo "##################### EXECUTE: kurento_merge_doc_project #################
 env
 
 [ -z "$KURENTO_PROJECT" ] && (echo "KURENTO_PROJECT variable not defined"; exit 1;)
-kurento_check_version.sh || exit 1
 
 export GIT_SSH_KEY=$GIT_KEY
 export GERRIT_REFNAME=$(echo $GERRIT_REFNAME | sed 's|refs/heads/||g')
 kurento_prepare_readthedocs.sh || exit 1
+kurento_check_version.sh true || exit 1
 
 pushd $KURENTO_PROJECT-readthedocs
-
 export CHECK_SUBMODULES="no"
-kurento_check_version.sh || exit 1
+kurento_check_version.sh true || exit 1
 
 # Extract version
 VERSION=$(kurento_get_version.sh)
