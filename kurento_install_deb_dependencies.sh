a94b5e8ac85d770367614f463c9333fa45bdf87a
Fede Diaz <nordri@gmail.com>
2018-01-22 13:01:05 +0100

    install previous generated deb packages and script label

--- kurento_install_deb_dependencies.sh
+++ kurento_install_deb_dependencies.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -x
 
+echo "##################### EXECUTE: kurento_install_deb_dependencies #####################"
+
 if [ ! -s debian/control ] ; then
   echo "Warning, no debian/control file found"
   exit 1
 fi
 
 DEBIAN_FRONTEND=noninteractive apt-get update || echo "Error updating packages"
+
+if [ -f *deb ]; then
+  dpkg -i *deb
+  apt-get install -f
+fi
 
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
