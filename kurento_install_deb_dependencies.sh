463175a65137f95e793ddf1d2d731bf7bd2c416e
Fede Diaz <nordri@gmail.com>
2018-01-22 13:57:44 +0100

    fix typo accept changes

--- kurento_install_deb_dependencies.sh
+++ kurento_install_deb_dependencies.sh
@@ -10,7 +10,7 @@ fi
 DEBIAN_FRONTEND=noninteractive apt-get update || echo "Error updating packages"
 
 dpkg -i *deb
-apt-get install -f
+apt-get install -y -f
 
 pkgs=$(cat debian/control | sed -e "s/$/\!\!/g" | tr -d '\n' | sed "s/\!\![[:space:]]/ /g" | sed "s/\!\!/\n/g" | grep "Build-Depends" | sed "s/Build-Depends://g" | sed "s/Build-Depends-Indep://g" | sed "s/([^)]*)//g" | sed "s/,\([^,^|]*\)[^,]*/\1/g" | sed 's/\[[^]]*\]//g' | sed "s/[[:space:]]+/ /g")
 echo "Installing packages: $pkgs"
