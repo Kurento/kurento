9d57eaa0c8ac8c0937ef86932fb8568b372796c7
Fede Diaz <nordri@gmail.com>
2018-02-12 11:29:10 +0100

    No longer needed

--- kurento-update-test-repo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-echo '##################### EXECUTE: kurento_ci_container_job_setup #####################'
-
-echo "deb http://ubuntu.kurento.org xenial-test kms6" | tee /etc/apt/sources.list.d/kurento-test.list
-wget -O - http://ubuntu.kurento.org/kurento.gpg.key | apt-key add -
-apt-get update
