d1c03d38e42619dc470c3675e9a9b1bb5e359e59
Juan Navarro <juan.navarro@gmx.es>
2018-02-20 11:11:36 +0100

    kurento_mavenize_js_project: Review generation of pom.xml

--- kurento_mavenize_js_project.sh
+++ kurento_mavenize_js_project.sh
@@ -44,11 +44,10 @@ RELEASE=$(echo $VERSION | awk -F"-" '{print $1}')
 [ -f pom.xml ] && [ $VERSION == `mvn help:evaluate -Dexpression=project.version 2>/dev/null| grep -v "^\[" | grep -v "Down"` ] && exit 0
 
 # Add pom file
-cat > pom.xml <<-EOF
+cat >pom.xml <<-EOF
 <project xmlns="http://maven.apache.org/POM/4.0.0"
-xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
-http://maven.apache.org/xsd/maven-4.0.0.xsd">
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
 	<modelVersion>4.0.0</modelVersion>
 
 	<groupId>org.kurento</groupId>
@@ -56,89 +55,99 @@ http://maven.apache.org/xsd/maven-4.0.0.xsd">
 	<version>${VERSION}</version>
 	<packaging>jar</packaging>
 
+	<!-- Project-level information -->
 	<name>${PROJECT_NAME}</name>
 	<description>
-		Kurento Media Server module ${PROJECT_NAME}
-		Javascript control client code
+		Kurento Media Server, module ${PROJECT_NAME} JavaScript client code.
 	</description>
-	<url>http://www.kurento.org</url>
-
-	<licenses>
-		<license>
-			<name>GNU Lesser General Public License</name>
-			<url>http://www.gnu.org/licenses/lgpl-2.1.txt</url>
-			<distribution>repo</distribution>
-		</license>
-	</licenses>
-
-	<organization>
-		<name>Kurento</name>
-		<url>http://www.kurento.org</url>
-	</organization>
-
+	<url>https://www.kurento.org/docs/${project.version}</url>
 	<scm>
-	   <url>https://github.com/Kurento/${PROJECT_NAME}</url>
-   <connection>
-      scm:git:https://github.com/Kurento/${PROJECT_NAME}.git
-   </connection>
-   <developerConnection>
-      scm:git:ssh://git@github.com:Kurento/${PROJECT_NAME}.git
-   </developerConnection>
+		<url>https://github.com/Kurento/${PROJECT_NAME}</url>
+		<connection>scm:git:git://github.com/Kurento/${PROJECT_NAME}.git</connection>
+		<developerConnection>scm:git:git@github.com:Kurento/${PROJECT_NAME}.git</developerConnection>
 	</scm>
+
+	<!-- Organization-level information -->
 	<developers>
 		<developer>
 			<id>kurento.org</id>
-			<name>-kurento.org Community</name>
-			<organization>Kurento.org</organization>
-			<organizationUrl>http://www.kurento.org</organizationUrl>
+			<name>Kurento Community</name>
+			<organization>Kurento</organization>
+			<organizationUrl>https://www.kurento.org</organizationUrl>
 		</developer>
 	</developers>
+	<issueManagement>
+		<system>GitHub</system>
+		<url>https://github.com/Kurento/bugtracker/issues</url>
+	</issueManagement>
+	<licenses>
+		<license>
+			<name>Apache 2.0</name>
+			<url>http://www.apache.org/licenses/LICENSE-2.0</url>
+			<distribution>repo</distribution>
+		</license>
+	</licenses>
+	<mailingLists>
+		<mailingList>
+			<name>Kurento List</name>
+			<subscribe>http://groups.google.com/group/kurento/subscribe</subscribe>
+			<post>http://groups.google.com/group/kurento/post</post>
+			<archive>http://groups.google.com/group/kurento/about</archive>
+		</mailingList>
+	</mailingLists>
+	<organization>
+		<name>Kurento</name>
+		<url>https://www.kurento.org</url>
+	</organization>
+
+	<!-- Project configuration -->
+
 	<build>
-	   <plugins>
-        	<plugin>
-           		<groupId>org.codehaus.mojo</groupId>
-		  		<artifactId>shell-maven-plugin</artifactId>
-		 		<version>1.0-beta-1</version>
+		<plugins>
+			<plugin>
+				<groupId>org.codehaus.mojo</groupId>
+				<artifactId>shell-maven-plugin</artifactId>
+				<version>1.0-beta-1</version>
 				<executions>
 					<execution>
-			        	<id>stage-sources</id>
-					 	<phase>process-sources</phase>
-					 	<goals>
-					 	   <goal>shell</goal>
-					 	</goals>
+						<id>stage-sources</id>
+						<phase>process-sources</phase>
+						<goals>
+							<goal>shell</goal>
+						</goals>
 						<configuration>
-				    		<workDir>\${workDir}</workDir>
-					    	<chmod>true</chmod>
-			  	    		<keepScriptFile>true</keepScriptFile>
-  			    			<script>
-                                ${MAVEN_SHELL_SCRIPT}
-						     </script>
-					  	</configuration>
-			    	</execution>
+							<workDir>\${workDir}</workDir>
+							<chmod>true</chmod>
+							<keepScriptFile>true</keepScriptFile>
+							<script>
+								${MAVEN_SHELL_SCRIPT}
+							</script>
+						</configuration>
+					</execution>
 				</executions>
 			</plugin>
 			<plugin>
-			   <artifactId>maven-assembly-plugin</artifactId>
-			      <configuration>
-		    	    <descriptors>
-			    	 	<descriptor>
-                          \${basedir}/${ASSEMBLY_FILE}
-				  	   </descriptor>
-			        </descriptors>
-        	        <outputDirectory>
+				<artifactId>maven-assembly-plugin</artifactId>
+				<configuration>
+					<descriptors>
+						<descriptor>
+							\${basedir}/${ASSEMBLY_FILE}
+						</descriptor>
+					</descriptors>
+					<outputDirectory>
 						\${project.build.directory}
-                	</outputDirectory>
-				 	<appendAssemblyId>false</appendAssemblyId>
-			     </configuration>
-			  </plugin>
-  		</plugins>
-			<extensions>
-				<extension>
-					<groupId>org.kuali.maven.wagons</groupId>
-					<artifactId>maven-s3-wagon</artifactId>
-					<version>1.2.1</version>
-				</extension>
-			</extensions>
+					</outputDirectory>
+					<appendAssemblyId>false</appendAssemblyId>
+				</configuration>
+			</plugin>
+		</plugins>
+		<extensions>
+			<extension>
+				<groupId>org.kuali.maven.wagons</groupId>
+				<artifactId>maven-s3-wagon</artifactId>
+				<version>1.2.1</version>
+			</extension>
+		</extensions>
 	</build>
 	<profiles>
 		<!-- Kurento CI requires this profiles to exist -->
@@ -159,33 +168,31 @@ EOF
 [ -n $ASSEMBLY_FILE ] && exit 0
 
 # Add assembly file
-cat > $ASSEMBLY_FILE <<-EOF
+cat >$ASSEMBLY_FILE <<-EOF
 <?xml version="1.0" encoding="UTF-8"?>
-<assembly
-	xmlns="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2
-      http://maven.apache.org/xsd/assembly-1.1.2.xsd">
+<assembly xmlns="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2"
+		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+		xsi:schemaLocation="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2 http://maven.apache.org/xsd/assembly-1.1.2.xsd">
 	<id>dist</id>
 	<formats>
 		<format>zip</format>
 	</formats>
 	<includeBaseDirectory>false</includeBaseDirectory>
 	<fileSets>
-	  <fileSet>
-	    <includes>
-	      <include>README.md</include>
-	      <include>LICENSE</include>
-	    </includes>
-	  </fileSet>
-	  <fileSet>
-	    <directory>dist</directory>
-	    <outputDirectory>/js</outputDirectory>
-	    <includes>
-	      <include>*.js</include>
-	      <include>*.map</include>
-	    </includes>
-	  </fileSet>
+		<fileSet>
+			<includes>
+				<include>README.md</include>
+				<include>LICENSE</include>
+			</includes>
+		</fileSet>
+		<fileSet>
+			<directory>dist</directory>
+			<outputDirectory>/js</outputDirectory>
+			<includes>
+				<include>*.js</include>
+				<include>*.map</include>
+			</includes>
+		</fileSet>
 	</fileSets>
 </assembly>
 EOF
