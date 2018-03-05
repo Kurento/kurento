ac39f8b7f7954e0dcae10faa64bd361adb35be81
Juan Navarro <juan.navarro@gmx.es>
2018-03-05 13:40:29 +0100

    kurento_python_extra: Use an array like in dev scripts

--- kurento_python_extra.sh
+++ kurento_python_extra.sh
@@ -2,10 +2,18 @@
 
 echo "##################### EXECUTE: kurento_python_extra #####################"
 
-apt-get update
-# Python needs those packages
-echo "Intalling python packages..."
-DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends python-git python-requests python-apt python-debian python-yaml
+PACKAGES=(
+  # Our Python-based build system requires these packages
+  python
+  python-apt
+  python-debian
+  python-git
+  python-requests
+  python-yaml
 
-# Temp solution to sudo issue
-DEBIAN_FRONTEND=noninteractive apt-get install --yes --no-install-recommends sudo
+  # Temp solution to sudo issue
+  sudo
+)
+
+DEBIAN_FRONTEND=noninteractive apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install --yes "${PACKAGES[@]}"
