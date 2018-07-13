c4f2b525465ba0c1e8def9b5a71e15cb802b528a
Juan Navarro <juan.navarro@gmx.es>
2018-07-13 13:36:22 +0200

    merge_doc_project, clone_repo: Improve log functions

--- kurento_clone_repo.sh
+++ kurento_clone_repo.sh
@@ -25,21 +25,20 @@ set -o errexit -o errtrace -o pipefail -o nounset
 
 # Logging functions
 # These disable and re-enable debug mode (only if it was already set)
-# Source: https://superuser.com/a/1141026
+# Source: https://superuser.com/a/1338887/922762
 shopt -s expand_aliases  # This trick requires enabling aliases in Bash
 BASENAME="$(basename "$0")"  # Complete file name
 echo_and_restore() {
-    echo "[${BASENAME}] $*"
+    echo "[${BASENAME}] $(cat -)"
     case "$flags" in (*x*) set -x ; esac
 }
-alias   log='{ flags="$-"; set +x; } 2>/dev/null; echo_and_restore'
-alias error='{ flags="$-"; set +x; } 2>/dev/null; echo_and_restore ERROR'
+alias log='({ flags="$-"; set +x; } 2>/dev/null; echo_and_restore) <<<'
 
 # Trap functions
 on_error() { ERROR=1; }
 trap on_error ERR
 on_exit() {
-    (( ${ERROR-${?}} )) && error || log "SUCCESS"
+    (( ${ERROR-${?}} )) && log "ERROR" || log "SUCCESS"
     log "------------ END ------------"
 }
 trap on_exit EXIT
@@ -65,34 +64,38 @@ CLONE_DIR="${3:-${CLONE_NAME}}"
 # Internal variables
 CLONE_URL="${KURENTO_GIT_REPOSITORY}/${CLONE_NAME}.git"
 
-log "Git clone: ${CLONE_URL} (${CLONE_REF})"
+log "Git clone $CLONE_URL ($CLONE_REF) to $PWD/$CLONE_DIR"
 
 if [ -z "${GIT_KEY:+x}" ]; then
-    git clone "${CLONE_URL}" "${CLONE_DIR}" \
-    || error "Command failed: git clone"
+    git clone "$CLONE_URL" "$CLONE_DIR" \
+    || { log "ERROR Command failed: git clone"; exit 1; }
 else
     ssh-agent bash -c "\
-      ssh-add ${GIT_KEY} || exit 1; \
-      git clone "${CLONE_URL}" "${CLONE_DIR}" || exit 1;" \
-    || error "Command failed: ssh-agent bash -c git clone"
+      ssh-add $GIT_KEY || exit 1; \
+      git clone $CLONE_URL $CLONE_DIR || exit 1;" \
+    || { log "ERROR Command failed: ssh-agent bash -c git clone"; exit 1; }
 fi
 
-cd "${CLONE_DIR}/"
+{
+    pushd "$CLONE_DIR"
 
-git fetch . refs/changes/*:refs/changes/* \
-|| error "Command failed: git fetch"
+    git fetch . refs/changes/*:refs/changes/* \
+    || { log "ERROR Command failed: git fetch"; exit 1; }
 
-git checkout "${CLONE_REF}" \
-|| error "Command failed: git checkout"
+    git checkout "$CLONE_REF" \
+    || { log "ERROR Command failed: git checkout"; exit 1; }
 
-if [ -f .gitmodules ]; then
-    if [ -z "${GIT_KEY:+x}" ]; then
-        git submodule update --init --recursive \
-        || error "Command failed: git submodule update"
-    else
-        ssh-agent bash -c "\
-          ssh-add ${GIT_KEY} || exit 1; \
-          git submodule update --init --recursive || exit 1;" \
-        || error "Command failed: ssh-agent bash -c git submodule update"
+    if [ -f .gitmodules ]; then
+        if [ -z "${GIT_KEY:+x}" ]; then
+            git submodule update --init --recursive \
+            || { log "ERROR Command failed: git submodule update"; exit 1; }
+        else
+            ssh-agent bash -c "\
+              ssh-add $GIT_KEY || exit 1; \
+              git submodule update --init --recursive || exit 1;" \
+            || { log "ERROR Command failed: ssh-agent bash -c git submodule update"; exit 1; }
+        fi
     fi
-fi
+
+    popd  # $CLONE_DIR
+}
