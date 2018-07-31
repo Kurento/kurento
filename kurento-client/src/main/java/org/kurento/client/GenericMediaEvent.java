bb78d53b459e41d116f742fac121a516c574c887
pabloFuente <pablofuenteperez@gmail.com>
2018-07-31 14:14:51 +0200

    kurento-client: GenericMediaEvent into src/main declaration

---
+++ kurento-client/src/main/java/org/kurento/client/GenericMediaEvent.java
@@ -0,0 +1,22 @@
+package org.kurento.client;
+
+import org.kurento.jsonrpc.Props;
+
+public class GenericMediaEvent extends MediaEvent {
+
+	Props data;
+
+	public GenericMediaEvent(@org.kurento.client.internal.server.Param("source") org.kurento.client.MediaObject source,
+			@org.kurento.client.internal.server.Param("timestamp") String timestamp,
+			@org.kurento.client.internal.server.Param("tags") java.util.List<org.kurento.client.Tag> tags,
+			@org.kurento.client.internal.server.Param("type") String type,
+			@org.kurento.client.internal.server.Param("genericData") Props data) {
+		super(source, timestamp, tags, type);
+		this.data = data;
+	}
+
+	public Props getData() {
+		return this.data;
+	}
+
+}
