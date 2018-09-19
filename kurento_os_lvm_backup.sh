5f266f38387485009d10248fa69204d26b33544f
nordri <nordri@gmail.com>
2018-09-19 14:47:50 +0200

    Style

--- kurento_os_lvm_backup.sh
+++ kurento_os_lvm_backup.sh
@@ -4,6 +4,8 @@
 #
 # Usage:
 # ./kurento-os-lvm-backup VOL_ID VOL_NAME
+#		VOL_ID: Volume identifiaction as OpenStack name it
+#		VOL_NAME: Descriptive name for the volume and tarball
 #
 # Based: https://docs.openstack.org/cinder/latest/admin/blockstorage-backup-disks.html
 
@@ -19,11 +21,11 @@ VOL_ID=$1
 [ -z $2 ] && exit 1
 VOL_NAME=$2
 
-SIZE=$(lvdisplay $VOL_PREFIX/volume-$VOL_ID | grep "LV Size" | awk '{print $3 }')
+SIZE=$(lvdisplay $VOL_PREFIX/volume-$VOL_ID | grep "LV Size" | awk '{ print $3 }')
 BACKUPDIR="/backups"
 
 # Creating the snapshot
-lvcreate --size ${SIZE}G --snapshot --name $VOL_NAME-snap $VOL_PREFIX/volume-$VOL_ID
+lvcreate --size ${SIZE}G --snapshot --name ${VOL_NAME}-snap ${VOL_PREFIX}/volume-${VOL_ID}
 
 # Creating file table
 kpartx -a ${VOL_PREFIX}/${VOL_NAME}-snap
@@ -31,10 +33,10 @@ kpartx -a ${VOL_PREFIX}/${VOL_NAME}-snap
 # Getting device name and mounting partition
 sleep 5
 VOL_FILETABLE_NAME=$(ls -1 /dev/mapper/ | grep snap1)
-mount /dev/mapper/$VOL_FILETABLE_NAME /mnt
+mount /dev/mapper/${VOL_FILETABLE_NAME} /mnt
 
 # Creating tarball
-tar czf $BACKUPDIR/$VOL_NAME-$DATE.tar.gz /mnt
+tar czf ${BACKUPDIR}/${VOL_NAME}-${DATE}.tar.gz /mnt
 
 # Cleaning the house
 
@@ -45,4 +47,4 @@ umount /mnt
 kpartx -d ${VOL_PREFIX}/${VOL_NAME}-snap
 
 # Removing snapshot
-lvremove -f ${VOL_PREFIX}/$VOL_NAME-snap
+lvremove -f ${VOL_PREFIX}/${VOL_NAME}-snap
