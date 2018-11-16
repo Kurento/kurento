2333d89188896d9025671b28c63916b0a54458e3
nordri <nordri@gmail.com>
2018-11-16 08:57:21 +0100

    Able to update the repo if it exists

--- development/aptly_create_repo.sh
+++ development/aptly_create_repo.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -x
 
 # Strict error checking
-set -o errexit -o errtrace -o pipefail -o nounset
+set -o errexit -o errtrace -o nounset
+
+# Will CREATE or UPDATE a debian repository
 
 echo "##################### EXECUTE: aptly_create_repo #####################"
 
-aptly repo create -distribution="${DISTRIBUTION}" -component=kms6 "kurento-experimental-${DISTRIBUTION}"
+# Check if repo exists
+EXITS=$(aptly repo list | grep kurento-experimental-${DISTRIBUTION} | wc -l)
+
+if [ "${EXITS}" == 0 ]; then
+	aptly repo create -distribution="${DISTRIBUTION}" -component=kms6 "kurento-experimental-${DISTRIBUTION}"
+fi
+
 aptly repo add -force-replace "kurento-experimental-${DISTRIBUTION}" ./*.*deb
-aptly snapshot create "snap-kurento-experimental-${DISTRIBUTION}" from repo "kurento-experimental-${DISTRIBUTION}"
-aptly -gpg-key="${GPGKEY}" publish snapshot "snap-kurento-experimental-${DISTRIBUTION}" "s3:ubuntu:${DISTRIBUTION}"
+
+if [ "$EXITS" == 0 ]; then
+	aptly -gpg-key="${GPGKEY}" publish repo "kurento-experimental-${DISTRIBUTION}" s3:ubuntu:${DISTRIBUTION}
+else
+	aptly -gpg-key="${GPGKEY}" publish update "${DISTRIBUTION}" s3:ubuntu:${DISTRIBUTION}
+fi
 
--- development/aptly_delete_repo.sh
+++ development/aptly_delete_repo.sh
@@ -6,5 +6,4 @@ set -o errexit -o errtrace -o pipefail -o nounset
 echo "##################### EXECUTE: aptly_delete_repo #####################"
 
 aptly publish drop ${DISTRIBUTION} s3:ubuntu:${DISTRIBUTION}
-aptly snapshot drop snap-kurento-experimental-${DISTRIBUTION}
 aptly repo drop kurento-experimental-${DISTRIBUTION}
