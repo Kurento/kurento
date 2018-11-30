c76c6c20808db26e02af1d202ab72fa93758ea36
Juan Navarro <juan.navarro@gmx.es>
2018-11-30 13:28:09 +0100

    media-server-buildtools: entrypoint: Print list of generated files

--- media-server-buildtools/entrypoint.sh
+++ media-server-buildtools/entrypoint.sh
@@ -21,8 +21,8 @@ cd /build
 # Note: "$@" expands to all quoted arguments, as passed to this script
 /adm-scripts/kurento-buildpackage.sh "$@"
 
-echo "[Docker] Output files:"
 BASENAME="$(basename "$0")"  # Complete file name
-ls -1 ../*.* | grep --invert-match "$BASENAME" || true
+echo "[$BASENAME] Output files:"
+find .. -maxdepth 1 -type f ! -name "$BASENAME"
 
 mv ../*.*deb ./ 2>/dev/null || true
