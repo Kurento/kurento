fd9e85b59e6a7f23a457db9edff5e3ff871da148
Juan Navarro <juan.navarro@gmx.es>
2018-12-11 15:58:20 +0100

    kurento/buildtools: Clone adm-scripts, improve comments
    * Now the standalone usage mode doesn't require that you bind-mount a
    copy of Kurento 'adm-scripts' into the container; it will clone the
    latest version from GitHub.
    
    * Comments have been improved for readability and better information.

--- media-server-buildtools/entrypoint.sh
+++ media-server-buildtools/entrypoint.sh
@@ -16,13 +16,28 @@ trap on_error ERR
 
 # ------------ Script start ------------
 
+
+# ==== Clone or update 'adm-scripts' ====
+if [[ -d /adm-scripts ]]; then
+    echo "Kurento 'adm-scripts' found: update"
+    cd /adm-scripts
+    git pull
+else
+    echo "Kurento 'adm-scripts' not found: clone"
+    git clone https://github.com/Kurento/adm-scripts.git /adm-scripts
+fi
+
+
+# ==== Build the project ====
 cd /build
-
-# Note: "$@" expands to all quoted arguments, as passed to this script
 /adm-scripts/kurento-buildpackage.sh "$@"
+# Note: "$@" expands to all quoted arguments, as passed to this script
 
+
+# ==== Finish ====
 BASENAME="$(basename "$0")"  # Complete file name
 echo "[$BASENAME] Output files:"
 find .. -maxdepth 1 -type f ! -name "$BASENAME"
 
+# Get results out from the Docker container
 mv ../*.*deb ./ 2>/dev/null || true
