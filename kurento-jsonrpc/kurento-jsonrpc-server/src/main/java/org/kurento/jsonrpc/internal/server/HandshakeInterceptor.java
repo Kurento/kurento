f12edd9c196ac14f10d78c9ff19d17094a2cac46
pabloFuente <pablofuenteperez@gmail.com>
2019-01-08 15:37:59 +0100

    HandshakeInterceptor added to store HttpSession in WebSocketSession object

---
+++ kurento-jsonrpc/kurento-jsonrpc-server/src/main/java/org/kurento/jsonrpc/internal/server/HandshakeInterceptor.java
@@ -0,0 +1,26 @@
+package org.kurento.jsonrpc.internal.server;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+@Component
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		ServletServerHttpRequest serverRequest = (ServletServerHttpRequest) request;
+		HttpSession httpSession = serverRequest.getServletRequest().getSession(this.isCreateSession());
+		attributes.put("httpSession", httpSession);
+		return true;
+	}
+
+}
