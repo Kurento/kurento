4fa9c6476eb8d78cddf2c5cf78fa0e2601bf6dbe
Juan Navarro <juan.navarro@gmx.es>
2019-04-08 13:08:03 +0200

    VERSIONS: Update versions for release 6.10.0

--- VERSIONS.conf.sh
+++ VERSIONS.conf.sh
@@ -12,20 +12,20 @@
 
 declare -A PROJECT_VERSIONS=(
     # Version of the documentation itself; it appears in the main menu
-    [VERSION_DOC]="6.9.0"
+    [VERSION_DOC]="6.10.0"
 
     # Version of Kurento Media Server
-    [VERSION_KMS]="6.9.0"
+    [VERSION_KMS]="6.10.0"
 
     # Version of each Client API SDK
-    [VERSION_CLIENT_JAVA]="6.9.0"
-    [VERSION_CLIENT_JS]="6.9.0"
+    [VERSION_CLIENT_JAVA]="6.10.0"
+    [VERSION_CLIENT_JS]="6.10.0"
 
     # Version of the JavaScript utils module
-    [VERSION_UTILS_JS]="6.9.0"
+    [VERSION_UTILS_JS]="6.10.0"
 
     # Version of each platform's tutorials
-    [VERSION_TUTORIAL_JAVA]="6.9.0"
-    [VERSION_TUTORIAL_JS]="6.9.0"
-    [VERSION_TUTORIAL_NODE]="6.9.0"
+    [VERSION_TUTORIAL_JAVA]="6.10.0"
+    [VERSION_TUTORIAL_JS]="6.10.0"
+    [VERSION_TUTORIAL_NODE]="6.10.0"
 )
