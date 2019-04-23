f8e88e908ae0e43e06780d5df215d15269e2ddee
Juan Navarro <juan.navarro@gmx.es>
2019-04-23 14:20:38 +0200

    kurento-buildpackage: Trace all commands (bash -x)

--- kurento-buildpackage.sh
+++ kurento-buildpackage.sh
@@ -148,6 +148,9 @@ BASEPATH="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"  # Absolute canonical path
 # shellcheck source=bash.conf.sh
 source "$BASEPATH/bash.conf.sh" || exit 1
 
+# Trace all commands
+set -o xtrace
+
 
 
 # Check permissions
