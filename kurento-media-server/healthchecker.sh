f7901213f7a01dd7a5496719c4a9c5c91fb04ce9
Juan Navarro <juan.navarro@gmx.es>
2019-05-23 18:08:21 +0200

    kurento-media-server/healthchecker: Rewrite for readability

--- kurento-media-server/healthchecker.sh
+++ kurento-media-server/healthchecker.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-[[ "$(curl -w '%{http_code}' -N -H "Connection: Upgrade" -H "Upgrade: websocket" -H "Host: 127.0.0.1:8888" -H "Origin: 127.0.0.1" http://127.0.0.1:8888/kurento)" == 500 ]] && exit 0 || exit 1
+RC="$(curl \
+    --silent \
+    --no-buffer \
+    --write-out '%{http_code}' \
+    --header "Connection: Upgrade" \
+    --header "Upgrade: websocket" \
+    --header "Host: 127.0.0.1:8888" \
+    --header "Origin: 127.0.0.1" \
+    http://127.0.0.1:8888/kurento)"
+
+if [[ "$RC" == "500" ]]; then
+    exit 0
+else
+    exit 1
+fi
