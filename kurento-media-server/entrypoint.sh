07720f2a5d6242c7599fb443f299391e08845723
Juan Navarro <juan.navarro@gmx.es>
2019-05-23 18:46:42 +0200

    kurento-media-server/entrypoint.sh: Allow both TURN and STUN
    Previously the STUN settings would truncate the file, deleting any TURN
    configuration that might have been set before.
    
    This fix was originally contributed by Axel Etcheverry (@euskadi31) in
    Kurento/kurento-docker#10. Thanks!
    
    The whole script has also been reviewed to use modern Bash style and
    pass ShellCheck tests.

--- kurento-media-server/entrypoint.sh
+++ kurento-media-server/entrypoint.sh
@@ -6,17 +6,20 @@ set -o errexit -o errtrace -o pipefail -o nounset
 # Trace all commands
 set -o xtrace
 
-if [ -n "$KMS_TURN_URL" ]; then
-  echo "turnURL=$KMS_TURN_URL" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+# Prepare the Kurento's WebRTC STUN/TURN settings file
+true >/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+
+if [[ -n "$KMS_TURN_URL" ]]; then
+    echo "turnURL=$KMS_TURN_URL" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
-if [ -n "$KMS_STUN_IP" -a -n "$KMS_STUN_PORT" ]; then
-  # Generate WebRtcEndpoint configuration
-  echo "stunServerAddress=$KMS_STUN_IP" > /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-  echo "stunServerPort=$KMS_STUN_PORT" >> /etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+if [[ -n "$KMS_STUN_IP" ]] && [[ -n "$KMS_STUN_PORT" ]]; then
+    echo "stunServerAddress=$KMS_STUN_IP" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+    echo "stunServerPort=$KMS_STUN_PORT"  >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
-# Remove ipv6 local loop until ipv6 is supported
-cat /etc/hosts | sed '/::1/d' | tee /etc/hosts > /dev/null
+# Remove the IPv6 loopback until IPv6 is well supported
+# Note: `sed -i /etc/hosts` won't work inside a Docker container
+cat /etc/hosts | sed '/::1/d' | tee /etc/hosts >/dev/null
 
 exec /usr/bin/kurento-media-server "$@"
