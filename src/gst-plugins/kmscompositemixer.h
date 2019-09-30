6dfb468b22b104db887e4f9de664d6953655476a
Juan Navarro <juan.navarro@gmx.es>
2019-09-30 19:37:21 +0200

    Use constexpr sizeof() instead of magic numbers for string lengths

--- src/gst-plugins/kmscompositemixer.h
+++ src/gst-plugins/kmscompositemixer.h
@@ -21,12 +21,10 @@
 
 #define AUDIO_SINK_PAD_PREFIX  "sink_"
 #define AUDIO_SRC_PAD_PREFIX  "src_"
-
+#define LENGTH_AUDIO_SINK_PAD_PREFIX (sizeof(AUDIO_SINK_PAD_PREFIX) - 1)
+#define LENGTH_AUDIO_SRC_PAD_PREFIX (sizeof(AUDIO_SRC_PAD_PREFIX) - 1)
 #define AUDIO_SINK_PAD AUDIO_SINK_PAD_PREFIX "%u"
 #define AUDIO_SRC_PAD AUDIO_SRC_PAD_PREFIX "%u"
-
-#define LENGTH_AUDIO_SINK_PAD_PREFIX 5  /* sizeof("sink_") */
-#define LENGTH_AUDIO_SRC_PAD_PREFIX 4   /* sizeof("src_") */
 
 G_BEGIN_DECLS
 #define KMS_TYPE_COMPOSITE_MIXER kms_composite_mixer_get_type()
