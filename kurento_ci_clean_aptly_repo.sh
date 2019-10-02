d5adc628bb9581f8a94fa9811ee43e30a69f32b4
Fede Diaz <nordri@gmail.com>
2019-10-02 16:42:49 +0200

    Joining jenkins and instance script in one. Reducing parameter number to just distro.

--- kurento_ci_clean_aptly_repo.sh
+++ kurento_ci_clean_aptly_repo.sh
@@ -19,20 +19,6 @@
 #/   Name of the Ubuntu distribution for which the repo will be purge.
 #/   E.g.: "xenial", "bionic"
 #/
-#/ --repo-name <RepoName>
-#/
-#/   Name of the repository that will be purge.
-#/   E.g.: "kurento-openvidu-xenial-dev", "kurento-openvidu-bionic-dev"
-#/
-#/ --publish-name <PublishName>
-#/
-#/   Name of the Aptly publishing endpoint that should be used.
-#/   E.g.: "s3:ubuntu:dev"
-#/
-#/   The repository URL will be like:
-#/
-#/       http://ubuntu.openvidu.io/<PublishName> <DistroName> kms6
-#/
 
 
 
@@ -67,26 +53,6 @@ while [[ $# -gt 0 ]]; do
                 exit 1
             fi
             ;;
-        --repo-name)
-            if [[ -n "${2-}" ]]; then
-                CFG_REPO_NAME="$2"
-                shift
-            else
-                echo "ERROR: --repo-name expects <RepoName>"
-                echo "Run with '--help' to read usage details"
-                exit 1
-            fi
-            ;;
-        --publish-name)
-            if [[ -n "${2-}" ]]; then
-                CFG_PUBLISH_NAME="$2"
-                shift
-            else
-                echo "ERROR: --publish-name expects <PublishName>"
-                echo "Run with '--help' to read usage details"
-                exit 1
-            fi
-            ;;
         *)
             echo "ERROR: Unknown argument '${1-}'"
             echo "Run with '--help' to read usage details"
@@ -96,6 +62,11 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+
+
+# Feed the variables
+CFG_REPO_NAME="kurento-openvidu-${CFG_DISTRO_NAME}-dev"
+CFG_PUBLISH_NAME="dev"
 
 
 # Apply config restrictions
@@ -122,38 +93,79 @@ echo "CFG_PUBLISH_NAME=$CFG_PUBLISH_NAME"
 
 
 
+# Build the script which will be run in proxy
+cat >run.sh<<EOF
 # Step 1: Check if repo exists
 # ----------------------------
 
-REPO_EXISTS="$(aptly repo list | grep --count "$CFG_REPO_NAME")" || true
-if [[ "$REPO_EXISTS" == "0" ]]; then
+REPO_EXISTS=\$(aptly repo list | grep --count "$CFG_REPO_NAME") || true
+if [[ "\$REPO_EXISTS" == "0" ]]; then
     echo "Repo: $CFG_REPO_NAME does not exist"
     exit 0
 fi
 
-
-
 # Step 2: Unpublish the repo
 # --------------------------
 
-aptly publish drop "$CFG_DISTRO_NAME" "$PUBLISH_ENDPOINT"
-
-
+PUBLISH_ENDPOINT="s3:ubuntu:${CFG_PUBLISH_NAME}"
+aptly publish drop "$CFG_DISTRO_NAME" "\$PUBLISH_ENDPOINT"
 
 # Step 3: Create a list with the content of the repository
 # --------------------------------------------------------
 
-PKG_LIST=$(mktemp -t kurento-purge-XXX --suffix .sh)
+PKG_LIST=\$(mktemp -t kurento-purge-XXX --suffix .sh)
 aptly repo show -with-packages "$CFG_REPO_NAME" \
-  | awk '{ print "aptly repo remove $CFG_REPO_NAME " $1 }' > ${PKG_LIST}
-
-
+  | awk '{ print "aptly repo remove $CFG_REPO_NAME " \$1 }' > \${PKG_LIST}
 
 # Step 4: Purge the repository
 # ----------------------------
 
-/bin/bash ${PKG_LIST} || true
+/bin/bash \${PKG_LIST} || true
+EOF
 
 
+
+# Run commands in a clean container
+# ---------------------------------
+
+# Prepare SSH key to access Kurento Proxy machine
+cat "$KEY_PUB" >secret.pem
+chmod 0400 secret.pem
+
+docker run --rm -i \
+    --mount type=bind,src="$PWD",dst=/workdir -w /workdir \
+    --mount type=bind,src="$KURENTO_SCRIPTS_HOME",dst=/adm-scripts \
+    buildpack-deps:xenial-scm /bin/bash <<EOF
+
+# Bash options for strict error checking
+set -o errexit -o errtrace -o pipefail -o nounset
+
+# Trace all commands
+set -o xtrace
+
+# Exit trap, used to clean up
+on_exit() {
+	# TODO: Maybe a more descriptive message?
+    echo "There was an error running the script."
+}
+trap on_exit EXIT
+
+scp -o StrictHostKeyChecking=no -i secret.pem \
+    run.sh \
+    ubuntu@proxy.openvidu.io:
+
+ssh -n -o StrictHostKeyChecking=no -i ./secret.pem \
+    ubuntu@proxy.openvidu.io '\
+        ./run.sh'
+
+ssh -n -o StrictHostKeyChecking=no -i ./secret.pem \
+    ubuntu@proxy.openvidu.io '\
+        rm run.sh'
+EOF
+
+
+
+# Delete SSH key
+rm secret.pem
 
 echo "Done!"
