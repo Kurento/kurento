ba9eb6357df5de1adc2243256d86c93b374f0bc7
Juan Navarro <juan.navarro@gmx.es>
2019-10-14 17:24:58 +0200

    CMake/CommonBuildFlags: Move build to C++14 standard

--- CMake/CommonBuildFlags.cmake
+++ CMake/CommonBuildFlags.cmake
@@ -59,7 +59,7 @@ function(common_buildflags_set)
   # General flags, used for all build configurations
   # `dpkg-buildflags` sets "-g -O2"
   set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS}   -std=gnu11   -Wall -pthread ${DPKG_CFLAGS}"   PARENT_SCOPE)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++11 -Wall -pthread ${DPKG_CXXFLAGS}" PARENT_SCOPE)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++14 -Wall -pthread ${DPKG_CXXFLAGS}" PARENT_SCOPE)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${DPKG_LDFLAGS}" PARENT_SCOPE)
   set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} ${DPKG_LDFLAGS}" PARENT_SCOPE)
   set(CMAKE_EXE_LINKER_FLAGS    "${CMAKE_EXE_LINKER_FLAGS}    ${DPKG_LDFLAGS}" PARENT_SCOPE)
