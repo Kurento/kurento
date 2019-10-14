f3c6f80d65d84239593c824a12e2fe23a1c24292
Juan Navarro <juan.navarro@gmx.es>
2019-10-14 18:59:13 +0200

    kms-build-run: Improve AddressSanitizer options (ASAN_OPTIONS)

--- bin/kms-build-run.sh
+++ bin/kms-build-run.sh
@@ -342,7 +342,11 @@ elif [[ "$CFG_ADDRESS_SANITIZER" == "true" ]]; then
 
     RUN_VARS+=(
         "LD_PRELOAD='$LIBSAN'"
-        "ASAN_OPTIONS='suppressions=${PWD}/bin/sanitizers/asan.supp detect_leaks=1 strict_string_checks=1 detect_invalid_pointer_pairs=2 check_initialization_order=1 strict_init_order=1'"
+        # Use ASAN_OPTIONS recommended for aggressive diagnostics:
+        # https://github.com/google/sanitizers/wiki/AddressSanitizer#faq
+        # NOTE: "detect_stack_use_after_return=1" breaks Kurento execution (more study needed to see why)
+        # NOTE: GST_PLUGIN_DEFINE() causes ODR violations so this check must be disabled
+        "ASAN_OPTIONS='suppressions=${PWD}/bin/sanitizers/asan.supp detect_odr_violation=0 detect_leaks=1 detect_invalid_pointer_pairs=2 strict_string_checks=1 detect_stack_use_after_return=0 check_initialization_order=1 strict_init_order=1'"
     )
 
 elif [[ "$CFG_THREAD_SANITIZER" == "true" ]]; then
