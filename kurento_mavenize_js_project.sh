Kurento/kurento

commit d261658509f3a74f7555c2a534928f4517041bd9

mavenize_js_project: Prevent script execution from command-line runs
kurento_get_version.sh will run this command:
mvn --batch-mode --non-recursive exec:exec \
-Dexec.executable=echo \
-Dexec.args=\${project.version}

We don't want that the script maven_script.sh gets executed by that
command, so we separate configuration of the exec-maven-plugin into two
different <execution>s:
- default-cli is the ID that command-line executions get.
- default-package is the ID of the execution performed by the 'mvn
package' command in kurento_maven_deploy.sh

kurento_mavenize_js_project.sh
@@ -153,18 +153,24 @@ cat >pom.xml <<EOF
         <groupId>org.codehaus.mojo</groupId>
         <artifactId>exec-maven-plugin</artifactId>
         <version>1.6.0</version>
+        <!-- Have different executions for explicit CLI calls (e.g. to get
+        the project version) and implicit calls (e.g. 'mvn package') -->
         <executions>
           <execution>
-            <id>stage-sources</id>
-            <phase>process-sources</phase>
+            <id>default-cli</id>
+            <phase/>
+          </execution>
+          <execution>
+            <id>default-package</id>
+            <phase>package</phase>
             <goals>
               <goal>exec</goal>
             </goals>
+            <configuration>
+              <executable>maven_script.sh</executable>
+            </configuration>
           </execution>
         </executions>
-        <configuration>
-          <executable>maven_script.sh</executable>
-        </configuration>
       </plugin>
       <plugin>
         <artifactId>maven-assembly-plugin</artifactId>
@@ -214,28 +220,28 @@ EOF
 cat >$ASSEMBLY_FILE <<-EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <assembly xmlns="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2"
-		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-		xsi:schemaLocation="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2 http://maven.apache.org/xsd/assembly-1.1.2.xsd">
-	<id>dist</id>
-	<formats>
-		<format>zip</format>
-	</formats>
-	<includeBaseDirectory>false</includeBaseDirectory>
-	<fileSets>
-		<fileSet>
-			<includes>
-				<include>README.md</include>
-				<include>LICENSE</include>
-			</includes>
-		</fileSet>
-		<fileSet>
-			<directory>dist</directory>
-			<outputDirectory>/js</outputDirectory>
-			<includes>
-				<include>*.js</include>
-				<include>*.map</include>
-			</includes>
-		</fileSet>
-	</fileSets>
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://maven.apache.org/plugins/maven-assembly-plugin/assembly/1.1.2 http://maven.apache.org/xsd/assembly-1.1.2.xsd">
+  <id>dist</id>
+  <formats>
+    <format>zip</format>
+  </formats>
+  <includeBaseDirectory>false</includeBaseDirectory>
+  <fileSets>
+    <fileSet>
+      <includes>
+        <include>README.md</include>
+        <include>LICENSE</include>
+      </includes>
+    </fileSet>
+    <fileSet>
+      <directory>dist</directory>
+      <outputDirectory>/js</outputDirectory>
+      <includes>
+        <include>*.js</include>
+        <include>*.map</include>
+      </includes>
+    </fileSet>
+  </fileSets>
 </assembly>
 EOF

commit 0dfbf79766dd7ce7dc714a8d85c1d958493d4ae9

mavenize_js_project: Fix broken packaging of Maven JS resouces
Commit Kurento/adm-scripts@d261658 broke the packaging of JS resources,
because it changed the generation phase from "process-sources" to
"package", which means that maven_script.sh would run too late in the
Maven lifecycle.

Using the more apt "generate-resources" phase solves the issue:
maven_script.sh is called, and _then_ the "package" phase finds all
files and packages them.

kurento_mavenize_js_project.sh
@@ -156,23 +156,25 @@ cat >pom.xml <<EOF
   </organization>
 
   <!-- Project configuration -->
-
   <build>
     <plugins>
       <plugin>
         <groupId>org.codehaus.mojo</groupId>
         <artifactId>exec-maven-plugin</artifactId>
         <version>1.6.0</version>
-        <!-- Have different executions for explicit CLI calls (e.g. to get
-        the project version) and implicit calls (e.g. 'mvn package') -->
         <executions>
+          <!-- Explicit CLI calls (e.g. from kurento_get_version.sh to get
+          the project version) are defined separately so maven_script.sh
+          doesn't get called. -->
           <execution>
             <id>default-cli</id>
             <phase/>
           </execution>
+          <!-- Use the "generate-resources" phase to call maven_script.sh
+          that generates all JS files and puts them into resources/ -->
           <execution>
             <id>default-package</id>
-            <phase>package</phase>
+            <phase>generate-resources</phase>
             <goals>
               <goal>exec</goal>
             </goals>