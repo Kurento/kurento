d5808334270682c57dcd706ef482f0cd6698a636
Fede Diaz <nordri@gmail.com>
2019-11-13 09:27:57 +0100

    Revert "externalAddresses support. You can assign KMS public IP by passing KMS_PUBLIC_IP envvar to docker run command."
    This reverts commit 4d7d1b50af5ec44f9c7fa05fbc4ddab6f633d7dd.

--- kurento-media-server/entrypoint.sh
+++ kurento-media-server/entrypoint.sh
@@ -9,21 +9,13 @@ set -o xtrace
 # Prepare the Kurento's WebRTC STUN/TURN settings file
 true >/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 
-if [[ -n "${KMS_TURN_URL}" ]]; then
-    echo "turnURL=${KMS_TURN_URL}" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+if [[ -n "$KMS_TURN_URL" ]]; then
+    echo "turnURL=$KMS_TURN_URL" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
-if [[ -n "${KMS_STUN_IP}" ]] && [[ -n "${KMS_STUN_PORT}" ]]; then
-    echo "stunServerAddress=${KMS_STUN_IP}" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-    echo "stunServerPort=${KMS_STUN_PORT}"  >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-fi
-
-if [[ -n "${KMS_PUBLIC_IP}" ]]; then
-	if [[ "${KMS_PUBLIC_IP}" == "AUTO" ]]; then
-		echo "externalAddresses=$(curl ifconfig.co)" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-	else
-		echo "externalAddresses=${KMS_PUBLIC_IP}" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
-	fi
+if [[ -n "$KMS_STUN_IP" ]] && [[ -n "$KMS_STUN_PORT" ]]; then
+    echo "stunServerAddress=$KMS_STUN_IP" >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
+    echo "stunServerPort=$KMS_STUN_PORT"  >>/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini
 fi
 
 # Remove the IPv6 loopback until IPv6 is well supported
