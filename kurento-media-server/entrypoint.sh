9a750b9deeeb0e126a6935d7cff5d728b2c4bf58
Juan Navarro <juan.navarro@gmx.es>
2020-04-16 18:44:22 +0200

    kurento-media-server: add KMS_UID to run with different User ID
    This allows running KMS with a different User ID than the default one
    (which is 0 == root, by default in Docker containers based on Ubuntu).

--- kurento-media-server/entrypoint.sh
+++ kurento-media-server/entrypoint.sh
@@ -8,9 +8,25 @@ set -o errexit -o errtrace -o pipefail -o nounset
 # Trace all commands
 set -o xtrace
 
+# Trap functions
+function on_error() {
+    echo "[Docker entrypoint] ERROR ($?)"
+    exit 1
+}
+trap on_error ERR
+
 # Settings
 BASE_RTP_FILE="/etc/kurento/modules/kurento/BaseRtpEndpoint.conf.ini"
 WEBRTC_FILE="/etc/kurento/modules/kurento/WebRtcEndpoint.conf.ini"
+
+# Check root permissions -- Overriding the Docker container run user (e.g. with
+# `docker run --user=1234`) is not supported, because this entrypoint script
+# needs to edit root-owned files under "/etc".
+# Instead, run with `docker run -e KMS_UID=1234`.
+[[ "$(id -u)" -eq 0 ]] || {
+    echo "[Docker entrypoint] ERROR: Please run container as root user"
+    exit 1
+}
 
 # Aux function: set value to a given parameter
 function set_parameter() {
@@ -22,8 +38,8 @@ function set_parameter() {
     local COMMENT=";"  # Kurento .ini files use ';' for comment lines
     local REGEX="^${COMMENT}?\s*${PARAM}=.*"
 
-    if grep --extended-regexp --quiet "$REGEX" "$FILE"; then
-        sed --regexp-extended --in-place "s/${REGEX}/${PARAM}=${VALUE}/" "$FILE"
+    if grep --extended-regexp -q "$REGEX" "$FILE"; then
+        sed --regexp-extended -i "s/${REGEX}/${PARAM}=${VALUE}/" "$FILE"
     else
         echo "${PARAM}=${VALUE}" >>"$FILE"
     fi
@@ -62,7 +78,7 @@ if [[ -n "${KMS_TURN_URL:-}" ]]; then
     set_parameter "$WEBRTC_FILE" "turnURL" "$KMS_TURN_URL"
 fi
 
-# Remove the IPv6 loopback until IPv6 is well supported.
+# Remove the IPv6 loopback until IPv6 is well supported in KMS.
 # Notes:
 # - `cat /etc/hosts | sed | tee` because `sed -i /etc/hosts` won't work inside a
 #   Docker container.
@@ -77,5 +93,29 @@ if [[ -z "${GST_DEBUG:-}" ]]; then
     export GST_DEBUG="3,Kurento*:4,kms*:4,sdp*:4,webrtc*:4,*rtpendpoint:4,rtp*handler:4,rtpsynchronizer:4,agnosticbin:4"
 fi
 
-# Run Kurento Media Server
-exec /usr/bin/kurento-media-server "$@"
+# Run Kurento Media Server, changing to requested user (if any)
+RUN_UID="$(id -u)"
+if [[ -n "${KMS_UID:-}" && "$KMS_UID" != "$RUN_UID" ]]; then
+    echo "[Docker entrypoint] Start Kurento Media Server, UID: $KMS_UID"
+
+    groupmod \
+        --gid "$KMS_UID" \
+        kurento
+
+    usermod \
+        --uid "$KMS_UID" \
+        --gid "$KMS_UID" \
+        kurento
+
+    # Run KMS as a child PID; normally, Docker best practices dictate that
+    # the main process should run as PID 1. However this is OK as `runuser`
+    # monitors and wraps the child process, passing through all input/output
+    # and any incoming signal.
+    # See: https://unix.stackexchange.com/questions/269254/why-does-util-linux-runuser-su-fork/269330#269330
+    exec runuser --user kurento --group kurento -- /usr/bin/kurento-media-server "$*"
+else
+    echo "[Docker entrypoint] Start Kurento Media Server, UID: $RUN_UID"
+
+    # Run KMS as PID 1 (replace the current process)
+    exec /usr/bin/kurento-media-server "$*"
+fi
