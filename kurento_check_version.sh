e67c8095615c2a480d35d76635d8f4e4269089b7
Juan Navarro <juan.navarro@gmx.es>
2021-01-29 15:00:14 +0100

    kurento_check_version minor: use common config & logs

--- kurento_check_version.sh
+++ kurento_check_version.sh
@@ -1,50 +1,64 @@
-#!/bin/bash -x
+#!/usr/bin/env bash
 
-echo "##################### EXECUTE: kurento_check_version #####################"
+
+
+# Shell setup
+# -----------
+
+BASEPATH="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"  # Absolute canonical path
+# shellcheck source=bash.conf.sh
+source "$BASEPATH/bash.conf.sh" || exit 1
+
+log "==================== BEGIN ===================="
+
+# Trace all commands
+set -o xtrace
+
+
 
 # Force creating a Git tag when a Release version is detected.
 # This is default 'false' because sometimes we need to do several actual
 # changes before being able to mark a commit as an official Release.
-if [ -n "$1" ]; then
+if [ -n "${1:-}" ]; then
   CREATE_TAG="$1"
 else
-  if [ -z "$CREATE_TAG" ]; then
+  if [ -z "${CREATE_TAG:-}" ]; then
     CREATE_TAG="false"
   fi
 fi
 
-[ -n "$CHECK_SUBMODULES" ] || CHECK_SUBMODULES="yes"
+[ -n "${CHECK_SUBMODULES:-}" ] || CHECK_SUBMODULES="yes"
 
 # Path information
 BASEPATH="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"  # Absolute canonical path
 PATH="${BASEPATH}:${PATH}"
 
-echo "[kurento_check_version] Create tag is '$CREATE_TAG'"
+log "Create tag is '$CREATE_TAG'"
 
 PROJECT_VERSION="$(kurento_get_version.sh)" || {
-  echo "[kurento_check_version] ERROR: Command failed: kurento_get_version"
+  log "ERROR: Command failed: kurento_get_version"
   exit 1
 }
-echo "[kurento_check_version] PROJECT_VERSION: $PROJECT_VERSION"
+log "PROJECT_VERSION: $PROJECT_VERSION"
 
 if [ "${PROJECT_VERSION}x" = "x" ]; then
-  echo "[kurento_check_version] ERROR: Could not find project version"
+  log "ERROR: Could not find project version"
   exit 1
 fi
 
 if [[ ${PROJECT_VERSION} == *-SNAPSHOT ]]; then
-  echo "[kurento_check_version] Exit: Version is SNAPSHOT: ${PROJECT_VERSION}"
+  log "Exit: Version is SNAPSHOT: ${PROJECT_VERSION}"
   exit 0
 fi
 
 if [[ ${PROJECT_VERSION} == *-dev ]]; then
-  echo "[kurento_check_version] Exit: Version is DEV"
+  log "Exit: Version is DEV"
   exit 0
 fi
 
 if [[ $(echo "$PROJECT_VERSION" | grep -o '\.' | wc -l) -gt 2 ]]
 then
-  echo "[kurento_check_version] Exit: Found more than two dots, should be a configure.ac dev version"
+  log "Exit: Found more than two dots, should be a configure.ac dev version"
   exit 0
 fi
 
@@ -56,7 +70,7 @@ if [[ "${CHECK_SUBMODULES}" == "yes" ]]; then
     fi
   "
   if [ $? -eq 1 ]; then
-    echo "[kurento_check_version] ERROR: Not all the projects are in a tag"
+    log "ERROR: Not all the projects are in a tag"
     exit 1
   fi
 fi
@@ -66,26 +80,30 @@ if [ -f debian/changelog ]; then
   #ver=$(head -1 debian/changelog | sed -e "s@.* (\(.*\)) .*@\1@")
   CHG_VER="$(dpkg-parsechangelog --show-field Version)"
   if [[ "${CHG_VER%%-*}" != "${PROJECT_VERSION}" ]]; then
-    echo "[kurento_check_version] WARNING Version in changelog is different to current version"
+    log "WARNING Version in changelog is different to current version"
     #exit 1
   fi
 fi
 
 # Check that release version conforms to semver
 kurento_check_semver.sh "${PROJECT_VERSION}" || {
-  echo "[kurento_check_version] ERROR: Command failed: kurento_check_semver"
+  log "ERROR: Command failed: kurento_check_semver"
   exit 1
 }
 
 if [[ "$CREATE_TAG" == "true" ]]; then
   TAG_MSG="Tag version $PROJECT_VERSION"
   TAG_NAME="$PROJECT_VERSION"
-  echo "[kurento_check_version] Create git tag: '$TAG_NAME'"
+  log "Create git tag: '$TAG_NAME'"
   if git tag -a -m "$TAG_MSG" "$TAG_NAME"; then
-    echo "[kurento_check_version] Tag created, push to remote"
+    log "Tag created, push to remote"
     git push origin "$TAG_NAME"
   else
-    echo "[kurento_check_version] ERROR: Command failed: git tag $TAG_NAME"
+    log "ERROR: Command failed: git tag $TAG_NAME"
     exit 1
   fi
 fi
+
+
+
+log "==================== END ===================="
--- kurento_get_version.sh
+++ kurento_get_version.sh
@@ -51,11 +51,10 @@ elif [ -f pom.xml ]
 then
   echo "Getting version from pom.xml" >&2
   MAVEN_CMD='mvn --batch-mode --non-recursive exec:exec -Dexec.executable=echo -Dexec.args=\${project.version}'
-  [ -n "$MAVEN_SETTINGS" ] && MAVEN_CMD="$MAVEN_CMD --settings $MAVEN_SETTINGS"
-  MAVEN_CMD_QUIET="$MAVEN_CMD --quiet 2>/dev/null"
-  PROJECT_VERSION="$(eval "$MAVEN_CMD_QUIET")" || {
+  [ -n "${MAVEN_SETTINGS:-}" ] && MAVEN_CMD="$MAVEN_CMD --settings $MAVEN_SETTINGS"
+  PROJECT_VERSION="$(eval "$MAVEN_CMD --quiet 2>/dev/null")" || {
     echo "[kurento_get_version] ERROR: Command failed: mvn echo project.version" >&2
-    echo "[kurento_get_version] Run again to get log messages ..." >&2
+    echo "[kurento_get_version] Running again to print the whole output:" >&2
     eval "$MAVEN_CMD" >&2  # This is just to print all output from Maven, eases debugging
     exit 1
   }
