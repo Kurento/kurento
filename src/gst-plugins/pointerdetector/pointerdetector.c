2b422784dcd5078f47217cfdfd2bde2001be0b86
Saul Pablo Labajo Izquierdo <slabajo@naevatec.com>
2021-07-12 13:42:23 +0200

    switch to gstreamer from bionic

--- src/gst-plugins/pointerdetector/pointerdetector.c
+++ src/gst-plugins/pointerdetector/pointerdetector.c
@@ -30,6 +30,6 @@ init (GstPlugin * plugin)
 
 GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
-    kmspointerdetector,
+    pointerdetector,
     "Kurento pointer detector",
     init, VERSION, GST_LICENSE_UNKNOWN, "Kurento", "http://kurento.com/")
