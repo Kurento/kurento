67b616f706532de5e3f1281bcb1b09b4dd742344
Juan Navarro <juan.navarro@gmx.es>
2022-04-13 18:23:23 +0200

    cmake: Fix usage of local builds and avoid system files altogether
    With these changes, when using kms-omni-build it is now really possible
    to build and use all of kurento-module-creator and kms-cmake-utils, from
    the local build folder, without the scripts reaching to system folders
    when trying to find dependencies.

--- cmake/KurentoUtils/KurentoUtilsConfig.cmake
+++ cmake/KurentoUtils/KurentoUtilsConfig.cmake
@@ -1,4 +1,6 @@
 # CMake Config Mode search file. Adds the main Kurento dir to CMAKE_MODULE_PATH.
 # https://cmake.org/cmake/help/latest/command/find_package.html#config-mode-search-procedure
 
-list(INSERT CMAKE_MODULE_PATH 0 "${CMAKE_CURRENT_LIST_DIR}/../Kurento")
+get_filename_component(KURENTO_CMAKE_DIR "${CMAKE_CURRENT_LIST_DIR}/../Kurento" ABSOLUTE)
+list(INSERT CMAKE_MODULE_PATH 0 "${KURENTO_CMAKE_DIR}")
+list(REMOVE_DUPLICATES CMAKE_MODULE_PATH)
