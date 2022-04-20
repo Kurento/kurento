3146dfde228bc8e8f6448402f74a728c6c0a8bff
Juan Navarro <juan.navarro@gmx.es>
2022-04-20 14:11:27 +0200

    Add new CI Job script kurento_ci_job_deploy_java
    This comes to replace the custom code that existed in all CI Java jobs,
    and replaces the need to call kurento_merge_java_project.sh

---
+++ kurento_ci_job_deploy_java.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+
+#/ CI job - Deploy Java artifacts with Maven.
+#/
+#/ This script is meant to be called from the "Execute shell" section of all
+#/ Jenkins jobs which want to deploy Java artifacts.
+#/
+#/
+#/ Arguments
+#/ ---------
+#/
+#/ None.
+#/
+#/
+#/ Variables
+#/ ---------
+#/
+#/ This script expects some environment variables to be exported.
+#/
+#/ * Variable(s) from job parameters (with "This project is parameterized"):
+#/
+#/ JOB_GIT_NAME
+#/
+#/   Git branch or tag that should be checked out, if it exists.
+#/
+#/
+#/ * Variable(s) from job Multi-Configuration ("Matrix") Project axis:
+#/
+#/ JOB_DISTRO
+#/
+#/   Name of the Ubuntu distribution where this job is run.
+#/   E.g.: "xenial", "bionic".
+#/
+#/
+#/ * Variable(s) from job Custom Tools (with "Install custom tools"):
+#/
+#/ KURENTO_SCRIPTS_HOME
+#/
+#/   Jenkins path to 'adm-scripts', containing all Kurento CI scripts.
+
+
+
+# Shell setup
+# -----------
+
+BASEPATH="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"  # Absolute canonical path
+# shellcheck source=bash.conf.sh
+source "$BASEPATH/bash.conf.sh" || exit 1
+
+log "==================== BEGIN ===================="
+
+# Trace all commands
+set -o xtrace
+
+
+
+# Job setup
+# ---------
+
+# Don't build from experimental branches. Otherwise we'd need to have some
+# mechanism to publish experimental module builds, which we don't have for
+# Java and JavaScript modules.
+#
+# Maybe in the future we might have something like experimental Maven or NPM
+# repositories, then we'd want to build experimental branches for them. But
+# for now, just skip and avoid polluting the default builds repositories.
+GIT_DEFAULT="$(kurento_git_default_branch.sh)"
+if [[ "$JOB_GIT_NAME" != "$GIT_DEFAULT" ]]; then
+    log "Skip building from experimental branch '$JOB_GIT_NAME'"
+    exit 0
+fi
+
+# Check out the requested branch
+"${KURENTO_SCRIPTS_HOME}/kurento_git_checkout_name.sh" \
+    --name "$JOB_GIT_NAME" --fallback "$JOB_DISTRO"
+
+
+
+# Build
+# -----
+
+RUN_COMMANDS=(
+    # Install any .jar files that might have been passed to this job
+    # (with "Copy artifacts from another project")
+    "find . -iname '*.jar' -print0 | xargs --no-run-if-empty -0 -P0 -I{} mvn --batch-mode install:install-file -Dfile='{}'"
+
+    # Compile, package, and deploy the current project.
+    "kurento_maven_deploy.sh"
+
+    # Only create a tag if the deployment process was successful
+    # Allow errors because the tag might already exist (like if the release
+    # is being done again after solving some deployment issue).
+    "kurento_check_version.sh true || { log 'WARNING: Command failed: kurento_check_version (tagging enabled)'; }"
+)
+
+export CONTAINER_IMAGE="kurento/kurento-ci-buildtools:xenial"
+"${KURENTO_SCRIPTS_HOME}/kurento_ci_container_job_setup.sh" "${RUN_COMMANDS[@]}"
+
+
+
+log "==================== END ===================="
