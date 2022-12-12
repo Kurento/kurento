21f8c36dcc31d5676b922a7e171937194212d6e8
Juan Navarro <juan.navarro@gmx.es>
2022-12-12 17:31:04 +0100

    Website hostname change: kurento.org -> kurento.openvidu.io

--- src/gst-plugins/platedetector/platedetector.c
+++ src/gst-plugins/platedetector/platedetector.c
@@ -32,4 +32,4 @@ GST_PLUGIN_DEFINE (GST_VERSION_MAJOR,
     GST_VERSION_MINOR,
     kmsplatedetector,
     "Kurento plate detector",
-    init, VERSION, GST_LICENSE_UNKNOWN, "Kurento", "http://kurento.com/")
+    init, VERSION, GST_LICENSE_UNKNOWN, "Kurento", "https://kurento.openvidu.io/")
