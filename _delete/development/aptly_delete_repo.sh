d53ff05b15287f1248f5a4c2eb0f2d757dae36a8
Juan Navarro <juan.navarro@gmx.es>
2022-12-23 17:00:23 +0100

    Delete '_delete' folder with all the old unmaintained scripts

--- _delete/development/aptly_delete_repo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-# Strict error checking
-set -o errexit -o errtrace -o pipefail -o nounset
-
-echo "##################### EXECUTE: aptly_delete_repo #####################"
-
-aptly publish drop ${DISTRIBUTION} s3:ubuntu:${DISTRIBUTION}
-aptly repo drop kurento-experimental-${DISTRIBUTION}
