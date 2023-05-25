579ea6d3a6de3aa1339a929a879bfc5a23a9bd6b
Saul Pablo Labajo Izquierdo <slabajo@naevatec.com>
2023-05-25 08:30:37 +0000

    feature/vp9_initial_support

--- server/module-core/src/gst-plugins/commons/kmsutils.c
+++ server/module-core/src/gst-plugins/commons/kmsutils.c
@@ -276,6 +276,9 @@ kms_utils_get_caps_codec_name_from_sdp (const gchar * codec_name)
   if (g_ascii_strcasecmp (VP8_ENCONDING_NAME, codec_name) == 0) {
     return "VP8";
   }
+  if (g_ascii_strcasecmp (VP9_ENCONDING_NAME, codec_name) == 0) {
+    return "VP9";
+  }
 
   return codec_name;
 }
