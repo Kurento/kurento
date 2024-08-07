ce36a669f8ea7a24932f6b92e276c884d7261af7
Saul Pablo Labajo Izquierdo <slabajo@naevatec.com>
2024-08-07 12:37:11 +0000

    update ci scripts to kurento noble

--- ci-scripts/kurento_ci_job_generate_module.sh
+++ ci-scripts/kurento_ci_job_generate_module.sh
@@ -92,7 +92,7 @@ docker run -t --detach \
     --rm --name "$CONTAINER_NAME" \
     --mount type=bind,src="$CI_SCRIPTS_PATH",dst=/ci-scripts \
     --mount type=bind,src="$PWD",dst=/workdir \
-    kurento/kurento-ci-buildtools:focal
+    kurento/kurento-ci-buildtools:noble
 
 # Stop (which also removes, due to `--rm`) the container upon script exit.
 trap_add "docker stop --time 3 $CONTAINER_NAME" EXIT
