760340ce1e7f4bf28308e731d75a8fd7b40ba89f
Saul Pablo Labajo Izquierdo <slabajo@naevatec.com>
2024-09-18 22:26:33 +0200

    fix javadoc

--- clients/java/client/src/main/java/org/kurento/client/AbstractBuilder.java
+++ clients/java/client/src/main/java/org/kurento/client/AbstractBuilder.java
@@ -78,7 +78,7 @@ public class AbstractBuilder<T> {
 
   /**
    * Builds an object asynchronously using the builder design pattern.
-   * <p/>
+   * <p>
    * The continuation will have {@link Continuation#onSuccess} called when the object is ready, or
    * {@link Continuation#onError} if an error occurs
    *
