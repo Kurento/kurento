5a4bdd1a1ef19a8f4421f37361b4fe1c8cfb0259
Saul Pablo Labajo Izquierdo <slabajo@naevatec.com>
2025-01-13 18:03:34 +0000

    additional fix

--- server/module-core/src/gst-plugins/commons/kmsenctreebin.c
+++ server/module-core/src/gst-plugins/commons/kmsenctreebin.c
@@ -366,6 +366,7 @@ kms_enc_tree_bin_set_target_bitrate (KmsEncTreeBin *self)
     case X264:
       property_name = h264_property_name;
       kbps_div = 1;
+      min_bitrate = 1;
       break;
     case OPENH264:
       property_name = h264_property_name;
